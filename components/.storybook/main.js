987b4d5a43180402b99961ad7c425f4e65fbea8c
Ela Yudhanira <ela.yudhanira@scania.com>
2020-11-11 15:01:28 +0100

    .storybook config is added

---
+++ components/.storybook/main.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "stories": [
+    "../src/**/**/*.stories.js"
+  ],
+  "addons": [
+    "@storybook/addon-docs",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-notes/register",
+    "@storybook/addon-controls"
+  ]
+}
