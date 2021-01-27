4a63875fcdad21810d17025d740c6cbedab0681d
Ela Yudhanira <ela.yudhanira@scania.com>
2021-01-27 08:59:58 +0100

    Fix all errors in core packages

--- theme/core/grid/compile.js
+++ theme/core/grid/compile.js
@@ -4,18 +4,22 @@ const glob = require('glob');
 const path = require('path');
 const del = require('del');
 const outputFolder = 'dist';
-const bundleScss = require('bundle-scss');
+const { Bundler } = require('scss-bundle');
 
 init();
-
-const dest= `${outputFolder}/scss/grid.scss`;
-const mask= "./*.scss";
 
 async function init() {
   await clean();
   await createFolders();
   await glob.sync('_grid.scss').forEach(generateCss);
-  bundleScss(mask, dest);
+  (async () => {
+    // Absolute project directory path.
+    const projectDirectory = path.resolve(__dirname, "./");
+    const bundler = new Bundler(undefined, projectDirectory);
+    // Relative file path to project directory path.
+    const result = await bundler.bundle("./_grid.scss");
+    fs.writeFileSync(`${outputFolder}/scss/grid.scss`, result.bundledContent)
+  })();
   console.log(`${outputFolder}/ folder contains all files`)
 }
 
