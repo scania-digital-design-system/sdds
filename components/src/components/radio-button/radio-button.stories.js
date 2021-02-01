a429df6fee16f58d17520f790d70ea3626450b6d
Ela Yudhanira <ela.yudhanira@scania.com>
2021-02-01 15:36:57 +0100

    Added radio-button style
    - Add styling for radio button
    - Add interaction
    - Explore animation on click (on focus)

---
+++ components/src/components/radio-button/radio-button.stories.js
@@ -0,0 +1,39 @@
+export default {
+  title: 'Component/Radio Button'
+};
+
+const Template = () => {
+  return `
+    <c-theme name="scania"></c-theme>
+    <div class="sdds-radio-button-group">
+      <div class="sdds-radio-item">
+        <input class="sdds-form-input" type="radio" name="rb-example" id="rb-option-1">
+        <label class="sdds-form-label" for="rb-option-1">
+          Label Text 1
+        </label>
+      </div>
+      <div class="sdds-radio-item">
+        <input class="sdds-form-input" type="radio" name="rb-example" id="rb-option-2">
+        <label class="sdds-form-label" for="rb-option-2">
+          Label Text 2
+        </label>
+      </div>
+      <div class="sdds-radio-item">
+        <input class="sdds-form-input" type="radio" name="rb-example" id="rb-option-3" checked="checked" disabled>
+        <label class="sdds-form-label" for="rb-option-3">
+          Label Text 3
+        </label>
+      </div>
+      <div class="sdds-radio-item">
+        <input class="sdds-form-input" type="radio" name="rb-example" id="rb-option-4" disabled>
+        <label class="sdds-form-label" for="rb-option-4">
+          Label Text 4
+        </label>
+      </div>
+    </div>
+    `
+};
+
+export const Basic = Template.bind({});
+
+Basic.args = {}
