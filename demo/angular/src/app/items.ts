9242756a9a0d9b6ce27aa1be16848f524b7511cf
Ela Yudhanira <ela.yudhanira@scania.com>
2021-02-09 11:12:01 +0100

    Fix navigiation on global style
    - Update migration readme to import global-style before sdds
    - Update example on angular demo

--- demo/angular/src/app/items.ts
+++ demo/angular/src/app/items.ts
@@ -10,7 +10,7 @@ export const items = [
     type:'primary',
     subMenu : [
       { text: 'List', url : '/list', type: 'primary' },
-      { text: 'Grid', url : '/grid', type: 'primary' },
+      { text: 'Table', url : '/table', type: 'primary' },
       { text: 'Form', url : '/form', type: 'primary' },
       { text: 'Modal', url : '/modal', type: 'primary' }
     ]
