1065551030e129ad5407355a95ee99b51c44a52d
Ela Yudhanira <ela.yudhanira@scania.com>
2021-02-23 10:14:33 +0100

    Compile woff files in typography core package
    - Use ttf2woff to generate webfont
    - Generate font.css for webfont
    - Import font.css in the main typography css

--- theme/core/typography/compile.js
+++ theme/core/typography/compile.js
@@ -4,13 +4,14 @@ const glob = require('glob');
 const path = require('path');
 const del = require('del');
 const outputFolder = 'dist';
-
+const ttf2woff = require('ttf2woff');
 
 init();
 
 async function init() {
   await clean();
   await createFolders();
+  await glob.sync('../assets/fonts/**/*.ttf').forEach(initFont);
   await glob.sync('*.scss').forEach(generateCss);
   console.log(`${outputFolder}/ folder contains all files`)
 }
@@ -27,6 +28,77 @@ function createFolders() {
   fs.mkdirSync(outputFolder);
   fs.mkdirSync(`${outputFolder}/css`);
   fs.mkdirSync(`${outputFolder}/scss`);
+  fs.mkdirSync(`${outputFolder}/fonts/latin`, { recursive: true });
+  fs.mkdirSync(`${outputFolder}/fonts/cyrillic`, { recursive: true });
+}
+
+function initFont(font) {
+  const options = {};
+  const filename = path.parse(font).name;
+  const baseFolder = path.basename(path.dirname(font));
+  let input;
+
+  generateFontCss(font);
+
+  try {
+    input = fs.readFileSync(path.resolve(font));
+  } catch (e) {
+    console.error("Can't open input file (%s)", input);
+    process.exit(1);
+  }
+  
+  const ttf = new Uint8Array(input);
+  
+  const woff = Buffer.from ?
+    Buffer.from(ttf2woff(ttf, options).buffer)
+    :
+    new Buffer(ttf2woff(ttf, options).buffer);
+
+  fs.writeFileSync(`${outputFolder}/fonts/${baseFolder}/${filename}.woff`, woff);   
+}
+
+// Generate font css
+
+function generateFontCss(file) {
+  const props = path.parse(file);
+  const type = props.dir.split('/').pop();
+  const name = props.name
+    .replace(/CY|-|Regular/g, '')
+    .split(/(?=[A-Z])/).join(' ');
+  const unicode = type === 'cyrillic' ? `
+  unicode-range: U+0400-04FF;` : '';
+
+  let content;
+
+  if(props.name.indexOf('Bold') > -1) {
+    content = `
+  font-family: "${name.replace(' Bold', '')}";
+  font-weight: bold;${
+    unicode
+  }`;
+  } else if(props.name.indexOf('Italic') > -1) {
+    content = `
+  font-family: "${name.replace(' Italic', '')}";
+  font-style: italic;${
+    unicode
+  }`;
+  } else {
+    content = `
+  font-family: "${name}";${
+    unicode
+  }`;
+  }
+
+  const data = generateFontFace(file, content);
+
+  fs.writeFileSync(`${outputFolder}/fonts/fonts.css`, data, { flag: 'a' });
+}
+function generateFontFace(file, props) {
+  const filename = file.replace(/...core\/assets\/fonts\/|.ttf/g, '');
+  return `@font-face {${
+    props
+  }
+  src: url("${filename}.woff") format("woff")\n}\n`;
 }
 
 // Create a scss folder will all scss files in current folder
