77706d4085b40b7693b72d15f384486aadce150b
Jonathan <jonathanengman@hotmail.com>
2021-03-05 16:14:46 +0100

    Javascript update with some design changes refering to github comments

--- components/src/components/tooltips/tooltip.stories.js
+++ components/src/components/tooltips/tooltip.stories.js
@@ -14,33 +14,66 @@ export default {
 
 const ComponentTooltip = ({tooltipBorder, text=''}) => {
 
-  return `
-  <style>
-  .tooltip-wrap {visibility: hidden;}
-  .target-wrap:hover .tooltip-wrap {
-    visibility: visible;
-  }
-  .target-wrap:hover {
-    cursor: pointer;
-  }
-  .target-wrap {
-    margin: 10rem 10rem;
-    display: inline-block;
-    background: #16417F;
-    color: white;
-    padding: 2rem;
-    border-radius: 1rem;
-  }
-  </style>
+  class SDDSTooltip extends HTMLElement {
+    constructor() {
+  
+      super();
+  
+      this._tooltip = document.querySelector('#ttp');
+      if (!this._tooltip)
+        return;
 
+      this._show = this._show.bind(this._tooltip);
+      this._hide = this._hide.bind(this._tooltip);
+    }
+    
+    connectedCallback() {
+      
+      this._hide();
+  
+      this._tooltip = document.querySelector('#tooltip-1');
+      if (!this._tooltip)
+        return;
+  
+      this._target = document.querySelector('#button-1');
+      if (!this._target)
+        return;
+  
+      this._target.addEventListener('focus', this._show);
+      this._target.addEventListener('blur', this._hide);
+      this._target.addEventListener('mouseenter', this._show);
+      this._target.addEventListener('mouseleave', this._hide);
+    }
+  
+    disconnectedCallback() {
+  
+      if (!this._target)
+        return;
+  
+        this._target.removeEventListener('focus', this._show);
+        this._target.removeEventListener('blur', this._hide);
+        this._target.removeEventListener('mouseenter', this._show);
+        this._target.removeEventListener('mouseleave', this._hide);
+        this._target = null;
+    }
+  
+    _show() {
+      this.hidden = false;
+    }
+  
+    _hide() {
+      this.hidden = true;
+    }
+  }
+
+  window.customElements.get('sdds-tooltip', SDDSTooltip);
+
+  return `
   <c-theme name="scania" global="true"></c-theme>
 
-  <div class="target-wrap">
-  Hover me!
-  <span class="tooltip-wrap">
-  <sdds-tooltip border="${tooltipBorder}" text='${text}' data-tooltip="${text}" ></sdds-tooltip>
-  </span>
-  </div>
+  <sdds-tooltip id="tooltip-1" border="${tooltipBorder}" text='${text}' selector="button-1" ></sdds-tooltip>
+
+  <sdds-button id="button-1" type="primary" text="Button"></sdds-button>
   `
 };
 
