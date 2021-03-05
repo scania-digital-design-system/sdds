3b2a30368b2192a70a4a87d1cce67c434c89b313
Ela Yudhanira <ela.yudhanira@scania.com>
2021-03-05 16:35:48 +0100

    Added textarea styling
    - Extends styling from textfield
    
    How to check :
    - Check design in Figma
    - Check if textarea functions properly

---
+++ components/src/components/textarea/textarea.stories.js
@@ -0,0 +1,151 @@
+import readme from './readme.md';
+
+export default {
+  title: 'Component/Textarea',
+  parameters: {
+    notes: readme,
+  },
+  argTypes: {
+    placeholder: {
+      name: 'Placeholder',
+      description: 'Placeholder text',
+      control: {
+        type: 'text'
+      }
+    },
+    disabled: {
+      description: 'Set textfield to disabled state',
+      name: 'Disabled',
+      control: {
+        type: 'boolean'
+      }
+    },
+    label: {
+      description: 'Label text for specific textfield',
+      name: 'Label text',
+      control: {
+        type: 'text'
+      }
+    },
+    labelPosition: {
+      description: 'Label can be placed inside the textfield',
+      name: 'Label position',
+      control: {
+        type: 'select',
+        options: [
+          'no-label',
+          'inside',
+          'outside'
+        ]
+      }
+    },
+    helper: {
+      name: 'Helper text',
+      description: 'Add helper text for the textfield',
+      control: {
+        type: 'text'
+      }
+    },
+    textcounter: {
+      name: 'Text counter',
+      description: 'Set a maximum value how long the text can be',
+      control: {
+        type:'number'
+      }
+    },
+    state: {
+      name: 'State',
+      description:'Switch between success or error state',
+      control: {
+        type: 'select',
+        options: [
+          'none',
+          'success',
+          'error'
+        ]
+      }
+    }
+  },
+  args: {
+    placeholder: 'Placeholder',
+    disabled: false,
+    state: 'default',
+    label: '',
+    labelPosition: 'no-label',
+    helper: '',
+  }
+};
+
+const style =`<style>
+
+.demo {
+  margin-top: 20px;
+  margin-left:20px;
+  width: 208px
+}
+</style>`;
+
+const textfieldTemplate = ({placeholder,disabled,label,labelPosition,state,helper,textcounter}) => {
+
+  return `
+  ${style}
+  <c-theme></c-theme>
+  <div class="demo">
+    <sdds-textarea
+      state="${state}"
+      maxlength="${textcounter}"
+      label="${label}"
+      helper="${helper}"
+      label-position="${labelPosition}"
+      ${disabled ? 'disabled' : ''}
+      placeholder="${placeholder}">
+    </sdds-textarea>
+  </div>
+  `
+};
+
+export const Basic = textfieldTemplate.bind({});
+
+Basic.args = {}
+
+export const Disabled = textfieldTemplate.bind({});
+
+Disabled.args = {
+  disabled: true,
+  placeholderText: 'Disabled'
+}
+
+export const labelInside = textfieldTemplate.bind({});
+
+labelInside.args = {
+  label: 'Label text',
+  labelPosition: 'inside'
+};
+
+export const labelOutside = textfieldTemplate.bind({});
+
+labelOutside.args = {
+  label: 'Label text',
+  labelPosition: 'outside'
+};
+
+export const helper = textfieldTemplate.bind({});
+
+helper.args = {
+  helper: 'Helper text',
+  label: 'Label text'
+};
+
+export const state = textfieldTemplate.bind({});
+
+state.args = {
+  state: 'error',
+  helper: 'Helper message',
+  label: 'Label text'
+};
+
+export const textCounter = textfieldTemplate.bind({});
+
+textCounter.args = {
+  textcounter: 10
+};
---
+++ components/src/components/textarea/textarea.tsx
@@ -0,0 +1,111 @@
+import {
+  Component, h, Listen, Prop, State
+} from '@stencil/core';
+
+@Component({
+  tag: 'sdds-textarea',
+  styleUrl: 'textarea.scss',
+  shadow: true,
+})
+
+export class Textarea{
+  /** Textinput for focus state */
+  textEl?: HTMLTextAreaElement;
+
+  /** Label text */
+  @Prop() label: string;
+
+  /** Helper text */
+  @Prop() helper: string;
+
+  /** Textarea cols attribute */
+  @Prop() cols: number;
+
+  /** Textarea rows attribute */
+  @Prop() rows: number;
+  
+  /** Label position: `no-label` (default), `inside`, `outside` */
+  @Prop() labelPosition = 'no-label';
+
+  /** Placeholder text */
+  @Prop() placeholder: string = "";
+
+  /** Value of the input text */
+  @Prop() value = "";
+
+  /** Set input in disabled state */
+  @Prop() disabled: boolean = false;
+
+  /** Error state of input */
+  @Prop() state: string;
+
+  /** Max length of input */
+  @Prop() maxlength = 0;
+
+  /** Listen to the focus state of the input */
+  @State() focusInput;
+
+  //Listener if input enters focus state
+  @Listen('focus')
+  handleFocusIn() {
+    this.focusInput = true;
+  }
+
+  //Listener if input leaves focus state
+  @Listen('focusout')
+  handleFocusOut() {
+    this.focusInput = false;
+  }
+
+  //Data input event in value prop
+  handleInputChange(e): void {
+    this.value = e.target.value;
+  }
+
+  //** Set the input as focus when clicking the whole textfield with suffix/prefix */
+  handleFocusClick(): void {
+    this.textEl.focus();
+  }
+
+  render() {
+    return (
+      <div class={`
+        sdds-textarea-container
+        ${this.labelPosition === 'inside' ? 'sdds-textarea-label-inside' : ''}
+        ${this.focusInput ? 'sdds-textarea-focus' : ''}
+        ${this.disabled ? 'sdds-textarea-disabled': ''}
+        ${this.state == 'error' || this.state == 'success' ? `sdds-textarea-${this.state}` : ''}
+        `}
+        onClick={() => this.handleFocusClick()}
+      >
+        {this.label.length > 0 && 
+          <span class={`sdds-textarea-label`}>
+            {this.label}
+          </span>
+        }
+        <div class="sdds-textarea-wrapper">
+          <textarea
+            class={`sdds-textarea-input`}
+            ref={inputEl => this.textEl = inputEl as HTMLTextAreaElement}
+            disabled={this.disabled}
+            placeholder={this.placeholder}
+            value={this.value}
+            maxlength={this.maxlength}
+            cols={this.cols}
+            rows={this.rows}
+            onInput={(e) => this.handleInputChange(e)}
+          ></textarea>
+        </div>
+        {this.helper.length > 0 && 
+          <span class={`sdds-textarea-helper`}>
+            {this.helper}
+          </span>
+        }
+        {this.maxlength > 0 && 
+          <div class={`sdds-textarea-textcounter`}>
+            {this.value.length} <span class="sdds-textfield-textcounter-divider"> / </span> {this.maxlength}
+          </div> 
+        }
+      </div>
+    )}
+}
