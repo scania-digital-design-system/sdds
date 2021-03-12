b260c602a048c9e0c1b99a956f0f5a9f4e371ec5
Jonathan <jonathanengman@hotmail.com>
2021-03-12 14:48:26 +0100

    Added animation for the tooltip.

--- components/src/components/tooltips/tooltip.tsx
+++ components/src/components/tooltips/tooltip.tsx
@@ -11,52 +11,39 @@ export class Tooltip {
   @Prop() text = "";
   @Prop() border: string;
   @Prop() selector = "";
-  @Prop() hidden: boolean = true;
+  @Prop() show: boolean = false;
   @State() target: any;
 
   tooltip!: HTMLInputElement;
   
   componentDidLoad() {
     this.target = document.querySelector(this.selector);
-  
-    this.target.addEventListener('mouseenter', () => {
-      this.hidden = false;
-      // createPopper.setOptions({
-      //   modifiers: [{ name: 'eventListeners', enabled: true }],
-      // });
-    });
-
-    this.target.addEventListener('mouseleave', () => {
-      this.hidden = true;
-      // createPopper.setOptions({
-      //   modifiers: [{ name: 'eventListeners', enabled: false }],
-      // });
-    });
 
     const _this = this;
 
     createPopper(this.target, this.tooltip, {
-      placement: 'auto',
+      placement: 'bottom-start',
       modifiers: [
         {
           name: 'positionCalc',
           enabled: true,
           phase: 'main',
           fn({ state }) {
-            if (state.placement === 'top' || state.placement === 'bottom') {
-              _this.border = 'default';
 
+            if (state.placement === 'bottom-start') {
+              _this.border = 'top-left';
+
+            } else if (state.placement === 'bottom-end') {
+              _this.border = 'top-right';
+              
             } else if (state.placement === 'top-start' || state.placement === 'left') {
               _this.border = 'bottom-right';
 
             } else if (state.placement === 'right' || state.placement === 'top-end') {
               _this.border = 'bottom-left';
 
-            } else if (state.placement === 'bottom-end') {
-              _this.border = 'top-left';
-
-            } else if (state.placement === 'bottom-start') {
-              _this.border = 'top-right';
+            } else if (state.placement === 'top' || state.placement === 'bottom') {
+              _this.border = 'default';
             }
           }
         },
@@ -68,13 +55,29 @@ export class Tooltip {
         },
       ],
     });
-  }
-  
 
+    this.target.addEventListener('mouseenter', () => {
+      this.show = true;
+      //TODO fix improve performance in the future, destroy poppers when it's not visible.
+      // popperInstance.setOptions({
+      //   modifiers: [{ name: 'eventListeners', enabled: true }],
+      // });
+      // popperInstance.update();
+    });
+
+    this.target.addEventListener('mouseleave', () => {
+      this.show = false;
+      // popperInstance.setOptions({
+      //   modifiers: [{ name: 'eventListeners', enabled: false }],
+      // });
+    });
+  };
+  
  render() {
   return (
-    <span ref={(el) => this.tooltip = el as HTMLInputElement} class={`${this.hidden ? 'sdds-tooltip-hidden' : `sdds-tooltip sdds-tooltip-${this.border}`}`}>
-      {this.text}
+    <span ref={(el) => this.tooltip = el as HTMLInputElement} 
+    class={`sdds-tooltip sdds-tooltip-${this.border} ${this.show ? 'sdds-tooltip-show' : ''}`}>
+    {this.text}
     </span>
   )
 }
