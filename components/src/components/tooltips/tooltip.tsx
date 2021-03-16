a90c29f2ec8b3749f4a70ea73df0d02916dedad5
Jonathan <jonathanengman@hotmail.com>
2021-03-16 15:27:15 +0100

    Fix

--- components/src/components/tooltips/tooltip.tsx
+++ components/src/components/tooltips/tooltip.tsx
@@ -15,7 +15,7 @@ export class Tooltip {
   @Prop() show: boolean = false;
   @Prop() placement: Placement = 'bottom';
   @State() target: any;
- 
+  
   tooltip!: HTMLInputElement;
   
   componentDidLoad() {
