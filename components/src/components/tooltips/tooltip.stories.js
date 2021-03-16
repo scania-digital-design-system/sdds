b08709966523f541475d69fa89325411b957f5f5
Jonathan <jonathanengman@hotmail.com>
2021-03-16 09:03:01 +0100

    Controls for the position now avaiable with some new arrow additions

--- components/src/components/tooltips/tooltip.stories.js
+++ components/src/components/tooltips/tooltip.stories.js
@@ -1,5 +1,15 @@
 export default {
   title: 'Component/Tooltip',
+  argTypes: {
+    tooltipPosition: {
+      control: {
+        type: 'select',
+        options:['bottom-start', 'bottom', 'bottom-end', 'top-start', 'top', 'top-end', 'left-start', 'left', 'left-end', 'right-start', 'right', 'right-end']
+      },
+      defaultValue: 'bottom',
+      description: 'Position of the tooltip'
+    },
+  }
 };
 
 const ComponentTooltip = ({...Basic}) => {
@@ -12,11 +22,11 @@ const ComponentTooltip = ({...Basic}) => {
   </style>
   <c-theme name="scania" global="true"></c-theme>
   
-  <div class="sdds-container" style="margin-top:10rem;">
+  <div class="sdds-container" style="margin-top:40rem;margin-left:40rem;">
     <div class="sdds-row">
       <div class="sdds-col-xxlg-5 sdds-col-xlg-5 sdds-col-lg-5 sdds-col-md-5">
 
-        <sdds-tooltip selector="#button-1" text='${Basic.text}' ></sdds-tooltip>
+        <sdds-tooltip placement="${Basic.tooltipPosition}" selector="#button-1" text='${Basic.text}' ></sdds-tooltip>
         <sdds-button id="button-1" type="primary" text="Button"></sdds-button>
         
       </div>
@@ -27,5 +37,5 @@ const ComponentTooltip = ({...Basic}) => {
 
 export const Basic = ComponentTooltip.bind({});
 Basic.args = {
-  text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  text: 'Lorem ipsum dolor sit amet',
 }
