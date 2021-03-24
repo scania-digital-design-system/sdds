96e07b623cd6cd84d349460f61be5b703706abb1
Jonathan <jonathanengman@hotmail.com>
2021-03-24 21:25:29 +0100

    Updates angular app

--- demo/angular/src/main.ts
+++ demo/angular/src/main.ts
@@ -1,8 +1,13 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
+import { addTheme, defineCustomElements } from '@scania/components';
+import { theme as scania } from '@scania/theme-light';
 import { environment } from './environments/environment';
+
+defineCustomElements();
+
+addTheme(scania);
 
 if (environment.production) {
   enableProdMode();
