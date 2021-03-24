e04c9b09c7cdb06315f7aeb05408fec16e62d2c7
Jonathan <jonathanengman@hotmail.com>
2021-03-24 15:22:27 +0100

    Fresh Angular app init

--- demo/angular/src/app/app-routing.module.ts
+++ demo/angular/src/app/app-routing.module.ts
@@ -1,24 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { HomeComponent } from './home/home.component';
-import { NotfoundComponent } from './notfound/notfound.component';
-import { InfoModule } from './info/info.module';
-
-export const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: '404', component: NotfoundComponent },
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: '**', redirectTo: '/404' }
-
-
-];
+const routes: Routes = [];
 
 @NgModule({
-  imports: [ 
-    RouterModule.forRoot(routes), 
-    InfoModule 
-  ],
-  exports : [ RouterModule ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- demo/angular/src/main.ts
+++ demo/angular/src/main.ts
@@ -4,15 +4,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-import { defineCustomElements, addTheme } from '@scania/components';
-import { theme as scania } from '@scania/theme-light';
-
 if (environment.production) {
   enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
-
-defineCustomElements();
-addTheme(scania);
+  .catch(err => console.error(err));
