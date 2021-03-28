ec94f6e16e715abed2f13c48b6d8cb26116be344
Jonathan <jonathanengman@hotmail.com>
2021-03-28 16:01:17 +0200

    Updated the component name from content --> main

--- demo/angular/src/app/app.module.ts
+++ demo/angular/src/app/app.module.ts
@@ -3,14 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ContentComponent } from './components/content/content.component';
 import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './components/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContentComponent,
     HeaderComponent,
+    MainComponent,
   ],
   imports: [
     BrowserModule,
