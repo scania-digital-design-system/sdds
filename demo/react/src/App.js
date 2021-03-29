83f9f04b291f13cdfa1238e253b2c8d3677a4fe7
Jonathan <jonathanengman@hotmail.com>
2021-03-29 22:42:19 +0200

    WIP: Html adaption to React

---
+++ demo/react/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './components/header/Header';
+import Main from './components/main/Main';
+
+function App() {
+  return (
+    <div className="App">
+      <sdds-theme></sdds-theme>
+      <Header />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
