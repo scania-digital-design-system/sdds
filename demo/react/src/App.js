ddd8d8177b421d28048e87271886d04ba2046991
Jonathan <jonathanengman@hotmail.com>
2021-03-31 17:02:03 +0200

    workaround main

--- demo/react/src/App.js
+++ demo/react/src/App.js
@@ -1,12 +1,12 @@
-// import Header from './components/header/Header';
-import Main from './components/main/Main';
+import Header from './components/header/Header';
+// import Main from './components/main/Main';
 
 function App() {
   return (
     <div className="App">
       <sdds-theme></sdds-theme>
-      {/* <Header /> */}
-      <Main />
+      <Header />
+      {/* <Main /> */}
     </div>
   );
 }
