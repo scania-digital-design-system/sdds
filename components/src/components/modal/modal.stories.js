0cf56fd2a923a4e09f53c8f3f3008d2dcd1da8b8
Marcus M <marcus.mena.pacheco@scania.com>
2021-04-15 16:55:13 +0200

    Modal component v1
    - Add modal
    - Add grid package in components
    - Watcher for the show state for overflow:hidden
    - Added sizing
    - Indentation fixes

--- components/src/components/modal/modal.stories.js
+++ components/src/components/modal/modal.stories.js
@@ -1,32 +1,49 @@
 export default {
-  title: 'component/Modal'
+  title: 'component/Modal',
+  args: {
+    preventBackdrop: false,
+    size: 'md'
+  },
+  argTypes: {
+    size: {
+      control: {
+        type: 'select',
+        options:['xs','sm','md','lg']
+      }
+    }
+  }
 };
-const ModalTemplate = ({preventBackdrop}) => {
+
+const ModalTemplate = ({...Modal}) => {
   return `
+
   <sdds-theme></sdds-theme>
-  <sdds-modal selector="#modal1" ${preventBackdrop == true? 'prevent':''} >
+  <button onclick="console.log('Open modal 1')" id="modal1" class="sdds-btn sdds-btn-primary">Open modal 1</button>
+  <sdds-modal size="${Modal.size}" selector="#modal1" ${Modal.preventBackdrop == true? 'prevent':''} >
     <h5 slot="sdds-modal-headline">Headline 1</h5>
-    <div slot="sdds-modal-body">Modal 1</div>
+      <p slot="sdds-modal-body">
+        Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Maecenas tempus, tellus eget condimentum rhoncus.
+      </p>
+      <button slot="sdds-modal-actions" modal-dismiss onclick="console.log('delete')" class="sdds-btn sdds-btn-danger sdds-btn-md">Delete</button>
+      <button slot="sdds-modal-actions" modal-dismiss onclick="console.log('cancel')" class="sdds-btn sdds-btn-secondary sdds-btn-md">Cancel</button>
   </sdds-modal>
 
-  <button id="modal1" class="sdds-btn sdds-btn-primary">Open modal</button>
-  <sdds-modal selector="#modal2" ${preventBackdrop == true? 'prevent':''} >
+  <sdds-modal size="${Modal.size}" selector="#modal2" ${Modal.preventBackdrop == true? 'prevent':''} >
     <h5 slot="sdds-modal-headline">Headline 2</h5>
-    <p slot="sdds-modal-body">
+    <div slot="sdds-modal-body">
       Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Maecenas tempus, tellus eget condimentum rhoncus.
-    </p>
-  <div slot="sdds-modal-actions">
-    <button class="sdds-btn sdds-btn-primary sdds-btn-md" style="margin-right:16px">Save</button>
-    <button onclick="() => { console.log('save')}" class="sdds-btn sdds-btn-secondary sdds-btn-md">Cancel</button>
-  </div>
-</sdds-modal>
+    </div>
+    <button slot="sdds-modal-actions" onclick="console.log('save')" class="sdds-btn sdds-btn-primary sdds-btn-md">Save</button>
+    <button slot="sdds-modal-actions" modal-dismiss onclick="console.log('cancel')" class="sdds-btn sdds-btn-secondary sdds-btn-md">Cancel</button>
+  </sdds-modal>
 
-<button onclick="console.log('testing')" id="modal2" class="sdds-btn sdds-btn-primary">Open modal</button>
+  <button onclick="console.log('Open modal 2')" id="modal2" class="sdds-btn sdds-btn-primary">Open modal 2</button>
+
   `
 };
 
 export const Modal = ModalTemplate.bind();
 
 Modal.args = {
-  preventBackdrop: false
+ size: 'md'
 }
