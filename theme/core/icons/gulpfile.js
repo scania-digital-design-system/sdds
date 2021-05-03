87aab21b422c2b4b72e15253e194b9358f801463
Ela Yudhanira <ela.yudhanira@scania.com>
2021-05-03 10:49:19 +0200

    Added template for iconfont generation
    - generate clean icons inside temp folder
    - need to remove temp folder

--- theme/core/icons/gulpfile.js
+++ theme/core/icons/gulpfile.js
@@ -1,13 +1,19 @@
-const { series } = require('gulp');
+const { series, task, src, dest } = require('gulp');
 const { parse } = require('svgson');
 const {optimize} = require('svgo');
 const path = require('path');
 const glob = require('glob');
 const fs = require('fs-extra');
 const del = require('del');
+const iconfontCss = require('gulp-iconfont-css');
+const iconfont = require('gulp-iconfont');
 
 const outputFolder = 'dist';
-const iconFolder = './svg/*.svg';
+const iconFolder = './src/svg/*.svg';
+const tempFolder = 'temp';
+
+const runTimestamp = Math.round(Date.now()/1000);
+const fontName = 'sdds-icons';
 
 const svgoConfig = {
   plugins: [
@@ -66,7 +72,11 @@ function clean() {
 }
 
 function initFolders(cb) {
-  fs.mkdirp(outputFolder);
+  // fs.mkdirp(outputFolder);
+  // fs.mkdirp('svg/temp/');
+  [outputFolder, tempFolder].map(folder => {
+    fs.mkdirSync(folder, { recursive: true });
+  });
 
   cb();
 }
@@ -75,21 +85,21 @@ function initFolders(cb) {
 // It can also be used within the `series()` composition.
 async function build(cb) {
   // body omitted
-
-  await generateIcons(iconFolder);
+  await generateIcons();
   
   cb();
 }
 
-async function generateIcons(iconsFolder){
+async function generateIcons(){
   const icon = {};
 
-  for(const file of glob.sync(iconsFolder)) {
+  for(const file of glob.sync(iconFolder)) {
     
     const props = path.parse(file);
     
     const content = fs.readFileSync(file);
     const response = await optimize(content, { path:file, ...svgoConfig });
+    
     const parsedSvg = await parse(response.data);
 
     icon.name = props.name.split('-').pop(); // for svg files named scania-abc
@@ -107,21 +117,38 @@ async function generateIcons(iconsFolder){
 
     export default icon;
     `
-
+    fs.writeFileSync(`${tempFolder}/${icon.name}.svg`, response.data);
     fs.writeFileSync(`${outputFolder}/${icon.name}.js`, svgToJSON);  
   };
   
 }
 
-exports.build = build;
-exports.default = series(clean, initFolders, build);
+function createIconfont() {
+  return src([`${tempFolder}/*.svg`])
+    .pipe(iconfontCss({
+      fontName: fontName,
+      path: './src/_icons.css', // template path
+      targetPath: `css/all.css`,
+      fontPath: '../',
+      cssClass:'sdds-icon'
+    }))
+    .pipe(iconfont({
+      fontName: fontName, // required
+      prependUnicode: true, // recommended option
+      formats: ['ttf', 'eot', 'woff'], // default, 'woff2' and 'svg' are available
+      timestamp: runTimestamp, // recommended to get consistent builds when watching files
+    }))
+      // .on('glyphs', function(glyphs, options) {
+      //   // CSS templating, e.g.
+      //   console.log(glyphs, options);
+      // })
+    .pipe(dest(`${outputFolder}/fonts/`));
+}
 
+function cleanTemp(){
+  return del(tempFolder);
+}
 
-// var icon = {
-//   "name": "truck",
-//   "width": "256",
-//   "height": "256",
-//   "definition": "M163 36V18H93v18H72v202h112V36h-21zm-10 10h21v182H82V46h21V28h50v18zM116.498 183.94L132.177 134H107l33.588-62-15.767 50H150z"
-// };
-
-// export default icon;
+exports.build = build; // build without iconfont
+exports.iconfont = createIconfont;
+exports.default = series(clean, initFolders, build, createIconfont);
