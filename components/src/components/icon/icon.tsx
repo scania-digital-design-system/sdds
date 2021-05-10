2c72e4516cedd8b166f56d44f9e4fa8ebbb30187
Ela Yudhanira <ela.yudhanira@scania.com>
2021-05-10 15:28:17 +0200

    Cleanup small packages
    - Fix typography packages to refer to CDN
    - Fix logotype to refer to CDN
    - Fix transform attribute in icons

--- components/src/components/icon/icon.tsx
+++ components/src/components/icon/icon.tsx
@@ -18,8 +18,9 @@ export class Icon {
       // dynamic import icon from @scania/icons
       this.icon = await import(`@scania/icons/dist/${this.name}`);
     } catch(err) {
-      console.warn(`Icon ${this.name} does not exist in SDDS package. Make sure you have installed @scania/icons package`);
-      this.name = 'truck' // If icon not found, display a truck icon
+      console.warn(err);
+      console.warn(`Make sure you have installed @scania/icons package`);
+      this.name = 'scania-truck' // If icon not found, display a truck icon
     }
    
   }
@@ -28,9 +29,27 @@ export class Icon {
     this.loadIcon();
   }
 
+  generateViewBox(){
+    // viewbox = minX minY width height
+    let viewbox = '0 0 0 0';
+    if(this.icon !== undefined){
+      // If icons has transform: translate attributes, add it to the viewbox.
+      viewbox = `${this.icon.default.hasOwnProperty('transform') ? this.getTransformViewBox() : '0 0'} ${this.icon.default.width} ${this.icon.default.height}`
+    }
+    return viewbox;
+  }
+
+  getTransformViewBox(){
+    const minX = parseFloat(this.icon.default.transform[0]) * -1;
+    const minY = parseFloat(this.icon.default.transform[1]) * -1;
+    return minX + ' ' + minY;
+  }
+
   render() {
     return [
-      <svg xmlns='http://www.w3.org/2000/svg' viewBox={`0 0 ${this.icon !== undefined ? this.icon.default.width : '0'} ${this.icon!== undefined ? this.icon.default.height : '0'}`}>
+      <svg xmlns='http://www.w3.org/2000/svg'
+      viewBox={this.generateViewBox()}
+      >
         <path fill='currentColor' d={this.icon ? this.icon.default.definition : ''} />
       </svg>,
     ];
