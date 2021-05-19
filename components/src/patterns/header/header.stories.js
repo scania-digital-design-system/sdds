c396be81b2a26edcadb49e5a625d09a4281d1edf
Ela Yudhanira <ela.yudhanira@scania.com>
2021-05-19 15:16:12 +0200

    Added initial styles for header

--- components/src/patterns/header/header.stories.js
+++ components/src/patterns/header/header.stories.js
@@ -25,9 +25,35 @@ const Template = ({siteName, shortName, siteUrl, items}) => {
   return `
   <sdds-theme name="scania"></sdds-theme>
   
-  <c-header site-name='${siteName}' short-name='${shortName}' site-url='${siteUrl}'>
-  ${items}
-  </c-header>
+  <nav class="sdds-navbar">
+    <button class="sdds-navbar-icon-button sdds-navbar-side-menu-toggler">
+      <span class="sdds-icon-toggler"></span>
+    </button>
+    <div class="sdds-navbar-application-brand">${siteName}</div>
+    <div class="sdds-navbar__overlay"></div>
+
+    <div class="sdds-navbar-top-menu sdds-navbar-menu">
+      <ul class="sdds-navbar-menu-nav">
+        <li class="sdds-navbar-menu-item">
+          <i class="sdds-icon scania-truck"></i>
+          <a class="sdds-navbar-menu-nav-link" href="#"> Item 1</a>
+          
+        </li>
+        <li class="sdds-navbar-menu-item">
+          <a class="sdds-navbar-menu-nav-link" href="https://scania.com/se/sv/home/experience-scania.html" target="_blank"> Item 2 </a>
+          
+        </li>
+      </ul>
+    </div>
+
+    <div class="sdds-navbar-right-menu">
+      <a class="sdds-navbar-menu-nav-link" href="#">EN</a>
+    </div>
+    <div class="sdds-navbar-brand">
+      <a href="https://scania.com" class="sdds-navbar-brand__image"></a>
+    </div>
+
+  </nav>
   `
 };
 
