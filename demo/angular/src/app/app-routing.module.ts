c9dd63042fa48119e02860eda1016a3e9bc3644b
Ela Yudhanira <ela.yudhanira@scania.com>
2021-06-03 10:36:54 +0200

    Add routing to angular demo

--- demo/angular/src/app/app-routing.module.ts
+++ demo/angular/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormComponent } from './components/form/form.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path:'form', component: FormComponent },
+  { path:'home', component: HomeComponent,  pathMatch: 'full' }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
