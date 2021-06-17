8640838066e5933c6d01a52e2469370b02136f3a
Kewser <kewser.jemal.said@scania.com>
2021-06-17 11:15:37 +0200

    Added initial implementation of badges

---
+++ components/src/components/badges/badges.stories.js
@@ -0,0 +1,63 @@
+export default {
+  title: 'Component/Badges',
+  argTypes: {
+    value: {
+      name: 'Value',
+      description: 'Set a value to show on the badge',
+      control: {
+        type:'number'
+      }
+    },  
+  },
+  args: {
+   value:''
+  }
+}
+const basicTemplate = ({value}) => {
+  //convert to string  
+  let valueString= value != null ? value.toString(): "";
+    return `
+    ${style}
+      <sdds-theme></sdds-theme>  
+      <sdds-badges value=${valueString}>       
+      </sdds-badges> 
+
+    `
+  };
+export const Basic = basicTemplate.bind({});
+  Basic.args = {};
+
+const style =`<style>
+                    .demo {
+                      margin:20px;
+                      height: 50px;
+                      width: 50px;
+                      position: relative;
+                      background-color: #C4C4C4;
+                    }
+                    .demo-badges{
+                      position: absolute;
+                      left: 32px;
+                      top: -5px;
+                    }
+              </style>`;
+const badgesTemplate = ({value}) => {
+  //convert to string  
+  let valueString= value != null ? value.toString(): "";
+    return `
+    ${style}
+      <sdds-theme></sdds-theme>
+      <div class="demo">
+      <sdds-badges class = "demo-badges" value=${valueString}>       
+      </sdds-badges> 
+      </div>
+    `
+  };
+export const Rounded = badgesTemplate.bind({});
+  Rounded.args = {
+    value:2
+  }
+export const Pill = badgesTemplate.bind({});
+  Pill.args = {
+    value: 100
+  }
---
+++ components/src/components/badges/readme.md
@@ -0,0 +1,17 @@
+# sdds-badges
+
+
+
+<!-- Auto Generated Below -->
+
+
+## Properties
+
+| Property | Attribute | Description | Type     | Default |
+| -------- | --------- | ----------- | -------- | ------- |
+| `value`  | `value`   |             | `string` | `""`    |
+
+
+----------------------------------------------
+
+*Built with [StencilJS](https://stenciljs.com/)*
