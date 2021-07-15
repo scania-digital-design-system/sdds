fda69d5dc94bf72033e8d72273c9836bbe948717
Ela Yudhanira <ela.yudhanira@scania.com>
2021-07-15 11:43:34 +0200

    Update demo and fix styling issue

--- components/src/components/theme/fullpage.stories.js
+++ components/src/components/theme/fullpage.stories.js
@@ -72,60 +72,60 @@ const Template = ({ }) => {
         <div class="sdds-row">
           <div class="sdds-col">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
 
-          <div class="sdds-col">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+          <div class="sdds-col">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>          
+        </div>
 
-          <div class="sdds-footer sdds-col-max-12">
-            <div class="sdds-footer-top sdds-container-fluid">
-              <div class="sdds-row">
-                <div class="sdds-footer-top-col sdds-col-max sdds-col-xxlg sdds-col-xlg sdds-col-lg sdds-col-md-12 sdds-col-sm-12 sdds-col-xs-12">
-                  <div class="sdds-footer-title opened">
-                    <span>Title 1</span>
-                    <span class="sdds-footer-top-icon">
-                      <svg width='12' height='7' viewBox='0 0 12 7' fill='none' xmlns='http://www.w3.org/2000/svg'>
-                        <path d='M1 1L6 6L11 1' stroke='currentColor' stroke-width='1.25' stroke-linecap='round' stroke-linejoin='round' />
-                      </svg>
-                    </span>
-                  </div>
-                  <ul class="sdds-footer-main-links opened">
-                    <li><a href="#">Legal link</a></li>
-                    <li><a href="#">Legal link</a></li>
-                    <li><a href="#">Legal link</a></li>
-                    <li><a href="#">Legal link</a></li>
-                  </ul>
+        <div class="sdds-footer sdds-col-max-12">
+          <div class="sdds-footer-top sdds-container-fluid">
+            <div class="sdds-row">
+              <div class="sdds-footer-top-col sdds-col-max sdds-col-xxlg sdds-col-xlg sdds-col-lg sdds-col-md-12 sdds-col-sm-12 sdds-col-xs-12">
+                <div class="sdds-footer-title opened">
+                  <span>Title 1</span>
+                  <span class="sdds-footer-top-icon">
+                    <svg width='12' height='7' viewBox='0 0 12 7' fill='none' xmlns='http://www.w3.org/2000/svg'>
+                      <path d='M1 1L6 6L11 1' stroke='currentColor' stroke-width='1.25' stroke-linecap='round' stroke-linejoin='round' />
+                    </svg>
+                  </span>
                 </div>
+                <ul class="sdds-footer-main-links opened">
+                  <li><a href="#">Legal link</a></li>
+                  <li><a href="#">Legal link</a></li>
+                  <li><a href="#">Legal link</a></li>
+                  <li><a href="#">Legal link</a></li>
+                </ul>
+              </div>
 
-                <div class="sdds-col-max sdds-col-xxlg sdds-col-xlg sdds-col-lg sdds-col-md-12 sdds-col-sm-12 sdds-col-xs-12">
-                  <div class="sdds-footer-title">
-                    <span>Title 2</span>
-                    <span class="sdds-footer-top-icon">
-                      <svg width='12' height='7' viewBox='0 0 12 7' fill='none' xmlns='http://www.w3.org/2000/svg'>
-                        <path d='M1 1L6 6L11 1' stroke='currentColor' stroke-width='1.25' stroke-linecap='round' stroke-linejoin='round' />
-                      </svg>
-                    </span>
-                  </div>
-                  <ul class="sdds-footer-main-links">
-                    <li><a href="#">Legal link</a></li>
-                    <li><a href="#">Legal link</a></li>
-                    <li><a href="#">Legal link</a></li>
-                    <li><a href="#">Legal link</a></li>
-                  </ul>
+              <div class="sdds-col-max sdds-col-xxlg sdds-col-xlg sdds-col-lg sdds-col-md-12 sdds-col-sm-12 sdds-col-xs-12">
+                <div class="sdds-footer-title">
+                  <span>Title 2</span>
+                  <span class="sdds-footer-top-icon">
+                    <svg width='12' height='7' viewBox='0 0 12 7' fill='none' xmlns='http://www.w3.org/2000/svg'>
+                      <path d='M1 1L6 6L11 1' stroke='currentColor' stroke-width='1.25' stroke-linecap='round' stroke-linejoin='round' />
+                    </svg>
+                  </span>
                 </div>
+                <ul class="sdds-footer-main-links">
+                  <li><a href="#">Legal link</a></li>
+                  <li><a href="#">Legal link</a></li>
+                  <li><a href="#">Legal link</a></li>
+                  <li><a href="#">Legal link</a></li>
+                </ul>
               </div>
             </div>
-            <div class="sdds-footer-main">
-              <ul class="sdds-footer-main-links">
-                <li><a href="#">Legal link</a></li>
-                <li><a href="#">Legal link</a></li>
-                <li><a href="#">Legal link</a></li>
-              </ul>
-              <ul class="sdds-footer-social-links">
-                <li><a href="#">Social 1</a></li>
-                <li><a href="#">Social 1</a></li>
-                <li><a href="#">Social 1</a></li>
-              </ul>
-              <div class="sdds-footer-main-brand">
-                <p>Copyright &copy; 2021 Scania</p>
-              </div>
+          </div>
+          <div class="sdds-footer-main">
+            <ul class="sdds-footer-main-links">
+              <li><a href="#">Legal link</a></li>
+              <li><a href="#">Legal link</a></li>
+              <li><a href="#">Legal link</a></li>
+            </ul>
+            <ul class="sdds-footer-social-links">
+              <li><a href="#">Social 1</a></li>
+              <li><a href="#">Social 1</a></li>
+              <li><a href="#">Social 1</a></li>
+            </ul>
+            <div class="sdds-footer-main-brand">
+              <p>Copyright &copy; 2021 Scania</p>
             </div>
           </div>
         </div>
