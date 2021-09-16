e198f9cdac89346767961b1f13e5a2b3c46ef9e5
Kewser <kewser.jemal.said@scania.com>
2021-09-16 10:40:17 +0200

    Added default implementation of data-table

---
+++ components/src/components/table/table.stories.js
@@ -0,0 +1,77 @@
+export default {
+  title: 'Component/Table'
+};
+const style = `<style>
+.table-demo {
+
+  margin: 30px;
+}
+</style>`;
+const Template = ({ type, divider }) => {
+  return `
+  ${style}
+      <sdds-theme></sdds-theme>
+      <div class="table-demo">
+      <table class="sdds-table sdds-table-${type} sdds-table-${divider}">
+      <caption>Table header</caption>       
+      <thead>
+                        <tr class="sdds-table-header">
+                          <th>Header</th>
+                          <th>Header</th>
+                          <th>Header</th>
+                          <th>Header</th>
+                          </tr>
+                 
+              
+               
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Text</td>
+                    <td>Text</td>
+                    <td>Text</td>
+                    <td>Text</td>
+                </tr>
+                <tr>
+                    <td>Text</td>
+                    <td>Text</td>
+                    <td>Text</td>
+                    <td>Text</td>
+                </tr>
+                <tr>
+                <td>Text</td>
+                <td>Text</td>
+                <td>Text</td>
+                <td>Text</td>
+                </tr>
+                <tr>
+                <td>Text</td>
+                <td>Text</td>
+                <td>Text</td>
+                <td>Text</td>
+                </tr>
+            </tbody>
+      </table>
+      </div>
+    `;
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  type: ''
+};
+export const Compact = Template.bind({});
+Compact.args = {
+  type: 'compact'
+};
+
+export const DefaultDividers = Template.bind({});
+DefaultDividers.args = {
+  type: '',
+  divider: 'divider'
+};
+export const CompactDividers = Template.bind({});
+CompactDividers.args = {
+  type: 'compact',
+  divider: 'divider'
+};
