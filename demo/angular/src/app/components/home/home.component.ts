5c33b1082974ed14f69354dbf17bc6b59c00b154
Johnny <nikola.johnny.mirkovic@scania.com>
2021-10-18 14:04:58 +0200

    fix for css not loading from Main

--- demo/angular/src/app/components/home/home.component.ts
+++ demo/angular/src/app/components/home/home.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html'
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent  {
-
-  constructor() { }
+export class HomeComponent {
+  constructor() {}
 }
