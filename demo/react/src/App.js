7011274040ccc01724284fc87784b34dab54ef08
gaonita <gaon.yg@gmail.com>
2021-11-04 15:27:14 +0100

    react demo with component 1.14.0

--- demo/react/src/App.js
+++ demo/react/src/App.js
@@ -1,40 +1,39 @@
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
-import Header from "./components/header/Header";
-import Form from "./components/form/Form";
-import Home from "./components/home/Home";
-import Footer from "./components/footer/Footer";
-import SideMenu from "./components/SideMenu/SideMenu";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Header from './components/header/Header';
+import Form from './components/form/Form';
+import Home from './components/home/Home';
+import Footer from './components/footer/Footer';
+import SideMenu from './components/SideMenu/SideMenu';
 
 function App() {
-    return (
-        <div id="root" className="App">
-            <Router>
-                <sdds-theme/>
-                <div className="sdds-navbar-overlay expanded"/>
+  return (
+    <div id="root" className="App">
+      <Router>
+        <sdds-theme />
+        <div className="sdds-navbar-overlay expanded" />
 
-                <Header/>
+        <Header />
 
-                <div className="sdds-push sdds-demo-container">
+        <div className="sdds-push sdds-demo-container">
+          <SideMenu />
 
-                    <SideMenu/>
-
-                    <div className={"sdds-content-push"}>
-                        <div className="sdds-container-fluid content-wrapper">
-                            <Switch>
-                                <Route exact path="/">
-                                    <Home/>
-                                </Route>
-                                <Route path="/form">
-                                    <Form/>
-                                </Route>
-                            </Switch>
-                        </div>
-                        <Footer/>
-                    </div>
-                </div>
-            </Router>
+          <div className={'sdds-content-push'}>
+            <div className="sdds-container-fluid content-wrapper">
+              <Switch>
+                <Route exact path="/">
+                  <Home />
+                </Route>
+                <Route path="/form">
+                  <Form />
+                </Route>
+              </Switch>
+            </div>
+            <Footer />
+          </div>
         </div>
-    );
+      </Router>
+    </div>
+  );
 }
 
 export default App;
--- demo/react/src/components/sideMenu/components/sideMenuItem/SideMenuItem.js
+++ demo/react/src/components/sideMenu/components/sideMenuItem/SideMenuItem.js
@@ -1,80 +1,79 @@
-import React, {useState} from 'react';
-import IconButton from "../IconButton/IconButton";
+import React, { useState } from 'react';
+import IconButton from '../IconButton/IconButton';
 
-const SideMenuItem = ({itemName, collapse, itemList}) => {
-    const [dropdownOpened, setDropdownOpened] = useState(false)
-    const [showPopOver, setShowPopOver] = useState(false)
-    const [posX, setPosX] = useState(null)
-    const [posY, setPosY] = useState(0)
+const SideMenuItem = ({ itemName, collapse, itemList }) => {
+  const [dropdownOpened, setDropdownOpened] = useState(false);
+  const [showPopOver, setShowPopOver] = useState(false);
+  const [posX, setPosX] = useState(null);
+  const [posY, setPosY] = useState(0);
 
-    const dropdownClick = () => {
-        setDropdownOpened(!dropdownOpened)
-    }
+  const dropdownClick = () => {
+    setDropdownOpened(!dropdownOpened);
+  };
 
-    function onHoverMenu(event) {
-        setShowPopOver(true)
-        const elem = event.target;
-        setPosX(elem.getBoundingClientRect().left + elem.offsetWidth + 2)
-        setPosY(elem.getBoundingClientRect().top);
-    }
+  function onHoverMenu(event) {
+    setShowPopOver(true);
+    const elem = event.target;
+    setPosX(elem.getBoundingClientRect().left + elem.offsetWidth + 2);
+    setPosY(elem.getBoundingClientRect().top);
+  }
 
-    return (
-        <div className={`sdds-navbar-menu-item-dropdown sdds-navbar-menu-hover ${dropdownOpened ? "opened" : null}`}
-             onClick={() => dropdownClick()}
-             onMouseEnter={(e) => onHoverMenu(e)}
-             onMouseLeave={() => setShowPopOver(false)}>
+  return (
+    <div
+      className={`sdds-navbar-menu-item-dropdown sdds-navbar-menu-hover ${
+        dropdownOpened ? 'opened' : null
+      }`}
+      onClick={() => dropdownClick()}
+      onMouseEnter={(e) => onHoverMenu(e)}
+      onMouseLeave={() => setShowPopOver(false)}
+    >
+      <div className="sdds-navbar-menu-item-dropdown-parent">
+        <IconButton />
+        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+        <a className="sdds-navbar-menu-item-link" href="#">
+          <span className="sdds-menu-item-dropdown-text">{itemName}</span>
+          {itemList && <span className="sdds-icon-arrow" />}
+        </a>
+      </div>
 
-            <div className="sdds-navbar-menu-item-dropdown-parent">
-                <IconButton/>
-                {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-                <a className="sdds-navbar-menu-item-link" href="#">
-                    <span className="sdds-menu-item-dropdown-text">{itemName}</span>
-                    {itemList && <span className="sdds-icon-arrow"/>}
-                </a>
-            </div>
+      {dropdownOpened && (
+        <ul className={`sdds-navbar-menu__dropdown-menu`}>
+          {itemList.map((e) => (
+            <li className="sdds-navbar-menu__dropdown-item">
+              {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+              <a href="#">{e.name}</a>
+            </li>
+          ))}
+        </ul>
+      )}
 
-            {
-                dropdownOpened &&
-                <ul className={`sdds-navbar-menu__dropdown-menu`}>
-                    {
-                        itemList.map((e) =>
-                            <li className="sdds-navbar-menu__dropdown-item">
-                                {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-                                <a href="#">{e.name}</a>
-                            </li>
-                        )
-                    }
-                </ul>
-            }
-
-            {
-                collapse && showPopOver &&
-                <div className={"sdds-navbar-menu-popover"}
-                     style={{
-                         display: 'block',
-                         position: 'fixed',
-                         left: `${posX}px`,
-                         top: `${posY}px`,
-                         zIndex: 100,
-                     }}>
-                    <div className="sdds-navbar-menu-item-dropdown">
-                        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-                        <a className="sdds-navbar-menu-item-link" href="">
-                            <span className="sdds-menu-item-dropdown-text">{itemName}</span>
-                        </a>
-                    </div>
-                    {
-                        itemList &&
-                        itemList.map((e) =>
-                            <li className="sdds-navbar-menu-item-dropdown hover sdds-navbar-menu-item-dropdown-parent">
-                                <a href="">{e.name}</a>
-                            </li>
-                        )
-                    }
-                </div>
-            }
+      {collapse && showPopOver && (
+        <div
+          className={'sdds-navbar-menu-popover'}
+          style={{
+            display: 'block',
+            position: 'fixed',
+            left: `${posX}px`,
+            top: `${posY}px`,
+            zIndex: 100,
+          }}
+        >
+          <div className="sdds-navbar-menu-item-dropdown">
+            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+            <a className="sdds-navbar-menu-item-link" href="">
+              <span className="sdds-menu-item-dropdown-text">{itemName}</span>
+            </a>
+          </div>
+          {itemList &&
+            itemList.map((e) => (
+              <li className="sdds-navbar-menu-item-dropdown hover sdds-navbar-menu-item-dropdown-parent">
+                <a href="">{e.name}</a>
+              </li>
+            ))}
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default SideMenuItem;
