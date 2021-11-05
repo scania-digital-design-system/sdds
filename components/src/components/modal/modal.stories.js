04e7be3d5afd74170b76d2614bc7a680600f2cd2
Johnny <nikola.johnny.mirkovic@scania.com>
2021-11-05 16:27:00 +0100

    using data- custom attribute for modal dismiss

--- components/src/components/modal/modal.stories.js
+++ components/src/components/modal/modal.stories.js
@@ -27,8 +27,8 @@ const ModalTemplate = ({ ...Modal }) => {
       <p slot="sdds-modal-body">
         Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Maecenas tempus, tellus eget condimentum rhoncus.
       </p>
-      <button slot="sdds-modal-actions" modal-dismiss onclick="console.log('delete')" class="sdds-btn sdds-btn-danger sdds-btn-md">Delete</button>
-      <button slot="sdds-modal-actions" modal-dismiss onclick="console.log('cancel')" class="sdds-btn sdds-btn-secondary sdds-btn-md">Cancel</button>
+      <button slot="sdds-modal-actions" data-dismiss-modal onclick="console.log('delete')" class="sdds-btn sdds-btn-danger sdds-btn-md">Delete</button>
+      <button slot="sdds-modal-actions" data-dismiss-modal onclick="console.log('cancel')" class="sdds-btn sdds-btn-secondary sdds-btn-md">Cancel</button>
   </sdds-modal>
   
   <br/>
@@ -42,7 +42,7 @@ const ModalTemplate = ({ ...Modal }) => {
       Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Maecenas tempus, tellus eget condimentum rhoncus.
     </div>
     <button slot="sdds-modal-actions" onclick="console.log('save')" class="sdds-btn sdds-btn-primary sdds-btn-md">Save</button>
-    <button slot="sdds-modal-actions" modal-dismiss onclick="console.log('cancel')" class="sdds-btn sdds-btn-secondary sdds-btn-md">Cancel</button>
+    <button slot="sdds-modal-actions" data-dismiss-modal onclick="console.log('cancel')" class="sdds-btn sdds-btn-secondary sdds-btn-md">Cancel</button>
   </sdds-modal>
 
   <button onclick="console.log('Open modal 2')" id="modal2" class="sdds-btn sdds-btn-primary">Open modal 2</button>
