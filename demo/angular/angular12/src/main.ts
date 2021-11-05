27a3f64bb014d6c6fd21b332ca1603835db0b675
Marcus M <marcus.mena.pacheco@scania.com>
2021-11-05 10:42:11 +0100

    Fixed angular 12 example looking same
    - removed index.d.ts from gitignore for angular12 example

--- demo/angular/angular12/src/main.ts
+++ demo/angular/angular12/src/main.ts
@@ -1,21 +1,23 @@
-
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-//Import SDDS
-import { defineCustomElements, addTheme } from '@scania/components';
-import { theme } from '@scania/theme-light';
-
+import { addTheme, defineCustomElements } from '@scania/components';
+import { theme as scania } from '@scania/theme-light';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+console.warn(
+  'This is a demo application, please visit https://digitaldesign.scania.com for more information regarding SDDS implementation'
+);
 
-  defineCustomElements();
-  addTheme(theme);
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
+
+defineCustomElements();
+addTheme(scania);
