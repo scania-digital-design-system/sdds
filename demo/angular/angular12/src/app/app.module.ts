cdcf0b77c55be2574bba445b17c01d1bf77f4420
Johnny <nikola.johnny.mirkovic@scania.com>
2021-11-08 16:44:16 +0100

    Prettier in demo/angular

--- demo/angular/angular12/src/app/app.module.ts
+++ demo/angular/angular12/src/app/app.module.ts
@@ -1,4 +1,3 @@
-
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -26,6 +25,6 @@ import { CustomDropdown } from './components/form/dropdown/dropdown.component';
   imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class AppModule { }
+export class AppModule {}
