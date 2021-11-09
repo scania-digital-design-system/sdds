f04731efe750cac30dc80f0c8648a0067c2db693
gaonita <gaon.yg@gmail.com>
2021-11-09 17:09:38 +0100

    popover fix

--- demo/react/src/components/sideMenu/SideMenu.js
+++ demo/react/src/components/sideMenu/SideMenu.js
@@ -1,47 +1,45 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import SideMenuItem from './components/sideMenuItem/SideMenuItem';
 import IconButton from './components/IconButton/IconButton';
 import SideMenuDropdownItem from './components/sideMenuDropdownItem/sideMenuDropdownItem';
 
 function SideMenu() {
-  const [collapse, setCollapse] = useState(false);
-  const clickCollapse = () => setCollapse(!collapse);
+    const [collapse, setCollapse] = useState(false);
+    const clickCollapse = () => setCollapse(!collapse);
 
-  return (
-    <div
-      className={`sdds-sidebar expanded ${
-        collapse ? 'sdds-sidebar-collapse' : ''
-      }`}
-      style={{ height: '100%' }}
-    >
-      <div className={`sdds-navbar-side-menu expanded`}>
-        <ul className="sdds-navbar-menu-list">
-          <SideMenuItem itemName={'Item 1'} collapse={collapse} />
-          <SideMenuDropdownItem itemName={'Item 1'} collapse={collapse} />
-          <SideMenuDropdownItem
-            itemName={'Item 2'}
-            collapse={collapse}
-            itemList={[
-              { name: 'Sub-item 1' },
-              { name: 'Sub-item 2' },
-              { name: 'Sub-item 3' },
-            ]}
-          />
-        </ul>
-
+    return (
         <div
-          className="sdds-navbar-menu-item sdds-navbar-menu-item-bottom sdds-navbar-menu-hide-on-mobile"
-          onClick={() => clickCollapse()}
-        >
-          <IconButton />
-          <a id="collapse-btn" className="sdds-navbar-menu-item-link" href="#">
-            Collapse
-          </a>
+            className={`sdds-sidebar expanded ${collapse ? 'sdds-sidebar-collapse' : ''}`}>
+            <div className={`sdds-navbar-side-menu expanded`}>
+                <ul className="sdds-navbar-menu-list">
+                    <SideMenuItem itemName={'Home'} link={'/'} collapse={collapse}/>
+                    <SideMenuItem itemName={'Form'} link={'/form'} collapse={collapse}/>
+                    <SideMenuDropdownItem itemName={'Dropdown 1'}
+                                          collapse={collapse}
+                                          itemList={[
+                                              {name: 'Sub-item 1'},
+                                              {name: 'Sub-item 2'},
+                                              {name: 'Sub-item 3'},
+                                          ]}
+                    />
+                    <SideMenuDropdownItem itemName={'Dropdown 2'} collapse={collapse}
+                                          itemList={[
+                                              {name: 'Sub-item 1'},
+                                              {name: 'Sub-item 2'},
+                                              {name: 'Sub-item 3'},
+                                          ]}
+                    />
+                </ul>
+                <div className="sdds-navbar-menu-item sdds-navbar-menu-item-bottom sdds-navbar-menu-hide-on-mobile"
+                     onClick={() => clickCollapse()}>
+                    <IconButton/>
+                    <a className="sdds-navbar-menu-item-link">
+                        Collapse
+                    </a>
+                </div>
+            </div>
         </div>
-      </div>
-      {/*<div id="popover" className="sdds-navbar-menu-popover" style={{display: 'none', position: 'fixed'}}/>*/}
-    </div>
-  );
+    );
 }
 
 export default SideMenu;
--- demo/react/src/components/sideMenu/components/sideMenuItem/SideMenuItem.js
+++ demo/react/src/components/sideMenu/components/sideMenuItem/SideMenuItem.js
@@ -1,58 +1,40 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import IconButton from '../IconButton/IconButton';
 
-const SideMenuItem = ({ itemName, collapse }) => {
-  const [showPopOver, setShowPopOver] = useState(false);
-  const [posX, setPosX] = useState(null);
-  const [posY, setPosY] = useState(0);
+const SideMenuItem = ({itemName, link, collapse}) => {
+    const [showPopOver, setShowPopOver] = useState(false);
+    const [posX, setPosX] = useState(null);
+    const [posY, setPosY] = useState(0);
 
-  function onHoverMenu(event) {
-    setShowPopOver(!showPopOver);
-    const elem = event.target;
-    setPosX(elem.getBoundingClientRect().left + elem.offsetWidth + 2);
-    setPosY(elem.getBoundingClientRect().top);
-  }
+    function onHoverMenu(event) {
+        setShowPopOver(!showPopOver);
+        const elem = event.target;
+        setPosX(elem.getBoundingClientRect().left + elem.offsetWidth + 2);
+        setPosY(elem.getBoundingClientRect().top);
+    }
 
-  return (
-    <>
-      {collapse && showPopOver && (
-        <div
-          className={'sdds-navbar-menu-popover'}
-          style={{
-            display: 'block',
-            position: 'fixed',
-            left: `${posX}px`,
-            top: `${posY}px`,
-            zIndex: 100,
-          }}
-        >
-          <li
-            className="sdds-navbar-menu-item"
-            style={{
-              display: 'flex',
-            }}
-          >
-            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-            <a className="sdds-navbar-menu-item-link" href="#">
-              {itemName}
-            </a>
-          </li>
-        </div>
-      )}
-      <li
-        className={`sdds-navbar-menu-item`}
-        onClick={() => console.log('side menu item without dropdown')}
-        onMouseEnter={(e) => onHoverMenu(e)}
-        onMouseLeave={() => setShowPopOver(!showPopOver)}
-      >
-        <IconButton />
-        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-        <a className="sdds-navbar-menu-item-link" href="">
-          {itemName}
-        </a>
-      </li>
-    </>
-  );
+    return (
+        <>
+            <div className={`sdds-navbar-menu-item`}
+                 onMouseEnter={(e) => onHoverMenu(e)}
+                 onMouseLeave={() => setShowPopOver(!showPopOver)}>
+                <IconButton/>
+                <a className="sdds-navbar-menu-item-link" href={link}>
+                    {itemName}
+                </a>
+            </div>
+            {collapse && showPopOver &&
+            <div className={'sdds-navbar-menu-popover'}
+                 style={{left: `${posX}px`, top: `${posY}px`, fontWeight: 'bold'}}>
+                <div className="sidebar-menu-popover">
+                    <a className="sdds-navbar-menu-item-link" href={link}>
+                        {itemName}
+                    </a>
+                </div>
+            </div>
+            }
+        </>
+    );
 };
 
 export default SideMenuItem;
