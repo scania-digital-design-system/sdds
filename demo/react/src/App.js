5f1e0a8aff4445e6f23e7783c2351111fd5f49c7
gaonita <gaon.yg@gmail.com>
2021-11-11 10:44:35 +0100

    collapsed item link fix

--- demo/react/src/App.js
+++ demo/react/src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './components/header/Header';
 import Form from './components/form/Form';
 import Home from './components/home/Home';
@@ -6,31 +6,31 @@ import Footer from './components/footer/Footer';
 import SideMenu from './components/SideMenu/SideMenu';
 
 function App() {
-    return (
-        <div id="root" className="App">
-            <Router>
-                <sdds-theme/>
-                <div className="sdds-navbar-overlay expanded"/>
-                <Header/>
-                <div className="sdds-push sdds-demo-container">
-                    <SideMenu/>
-                    <div className={'sdds-content-push'}>
-                        <div className="sdds-container-fluid content-wrapper">
-                            <Switch>
-                                <Route exact path="/">
-                                    <Home/>
-                                </Route>
-                                <Route path="/form">
-                                    <Form/>
-                                </Route>
-                            </Switch>
-                        </div>
-                        <Footer/>
-                    </div>
-                </div>
-            </Router>
+  return (
+    <div id="root" className="App">
+      <Router>
+        <sdds-theme />
+        <div className="sdds-navbar-overlay expanded" />
+        <Header />
+        <div className="sdds-push sdds-demo-container">
+          <SideMenu />
+          <div className={'sdds-content-push'}>
+            <div className="sdds-container-fluid content-wrapper">
+              <Switch>
+                <Route exact path="/">
+                  <Home />
+                </Route>
+                <Route path="/form">
+                  <Form />
+                </Route>
+              </Switch>
+            </div>
+            <Footer />
+          </div>
         </div>
-    );
+      </Router>
+    </div>
+  );
 }
 
 export default App;
--- demo/react/src/components/header/Header.js
+++ demo/react/src/components/header/Header.js
@@ -1,15 +1,15 @@
 import './Header.css';
 
 const Header = () => {
-    return (
-        <nav className="sdds-navbar">
-            <button className="sdds-navbar-icon-button sdds-navbar-side-menu-drawer expanded">
-                <span className="sdds-icon-drawer"/>
-            </button>
-            <div className="sdds-navbar-application-brand">SDDS DEMO REACT</div>
-            <div className="sdds-navbar-scania-brand"/>
-        </nav>
-    );
+  return (
+    <nav className="sdds-navbar">
+      <button className="sdds-navbar-icon-button sdds-navbar-side-menu-drawer expanded">
+        <span className="sdds-icon-drawer" />
+      </button>
+      <div className="sdds-navbar-application-brand">SDDS DEMO REACT</div>
+      <div className="sdds-navbar-scania-brand" />
+    </nav>
+  );
 };
 
 export default Header;
