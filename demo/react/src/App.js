938e535772101f4b00d137ee29055a2bdd61ac10
gaonita <gaon.yg@gmail.com>
2021-11-16 09:12:15 +0100

    Improvement/side menu in React demo (#232)
    * side menu react demo with component v 1.14.0

--- demo/react/src/App.js
+++ demo/react/src/App.js
@@ -3,26 +3,33 @@ import Header from './components/header/Header';
 import Form from './components/form/Form';
 import Home from './components/home/Home';
 import Footer from './components/footer/Footer';
+import SideMenu from './components/sideMenu/SideMenu';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <sdds-theme></sdds-theme>
+    <div id="root" className="App">
+      <Router>
+        <sdds-theme />
+        <div className="sdds-navbar-overlay expanded" />
         <Header />
-        <div className="sdds-container content-wrapper">
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/form">
-              <Form />
-            </Route>
-          </Switch>
+        <div className="sdds-push sdds-demo-container">
+          <SideMenu />
+          <div className={'sdds-content-push'}>
+            <div className="sdds-container-fluid content-wrapper">
+              <Switch>
+                <Route exact path="/">
+                  <Home />
+                </Route>
+                <Route path="/form">
+                  <Form />
+                </Route>
+              </Switch>
+            </div>
+            <Footer />
+          </div>
         </div>
-        <Footer />
-      </div>
-    </Router>
+      </Router>
+    </div>
   );
 }
 
---
+++ demo/react/src/components/sideMenu/components/IconButton/IconButton.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const IconButton = () => {
+  return (
+    <span className="sdds-navbar-icon-button">
+      <svg
+        width="20"
+        height="20"
+        viewBox="0 0 20 20"
+        fill="#e2e2e4"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <rect y="0.334473" width="20" height="20" />
+      </svg>
+    </span>
+  );
+};
+
+export default IconButton;
