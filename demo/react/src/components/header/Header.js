a2c7282ee7a4b47af261a9c329eaabb340131b1c
gaonita <gaon.yg@gmail.com>
2021-12-20 15:12:26 +0100

    Expand/hide side menu on mobile in React demo (#256)
    Expand/hide side menu on mobile in React demo

--- demo/react/src/components/header/Header.js
+++ demo/react/src/components/header/Header.js
@@ -1,9 +1,14 @@
 import './Header.css';
 
-const Header = () => {
+const Header = ({ onDrawerClick, expand }) => {
   return (
     <nav className="sdds-navbar">
-      <button className="sdds-navbar-icon-button sdds-navbar-side-menu-drawer expanded">
+      <button
+        className={`sdds-navbar-icon-button sdds-navbar-side-menu-drawer ${
+          expand ? 'expanded' : null
+        }`}
+        onClick={onDrawerClick}
+      >
         <span className="sdds-icon-drawer" />
       </button>
       <div className="sdds-navbar-application-brand">SDDS DEMO REACT</div>
