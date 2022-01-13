65607ea60a48d58044d8d2e4cc30675c27d9e6f0
Marcus M <marcus.mena.pacheco@scania.com>
2022-01-13 16:00:39 +0100

    fix(sdds-theme): fixing but related with text and background classes
    Added import missing to be able to use sdds-text and background classes

---
+++ components/src/components/theme/utilites.stories.js
@@ -0,0 +1,53 @@
+export default {
+  title: 'utilities/Colours',
+};
+
+const TextColorTemplate = ({ color, text }) => {
+  return `
+  <sdds-theme></sdds-theme>
+  <h1 class="sdds-text-${color}">${text}</h1>
+  <p class="sdds-text-${color}">${text}</p>
+  `;
+};
+
+export const textColor = TextColorTemplate.bind({});
+textColor.args = {
+  color: 'grey-900',
+  text: 'A text block',
+};
+
+const BackgroundColorTemplate = ({ height, width, backgroundColor }) => {
+  return `
+  <sdds-theme></sdds-theme>
+    <div class="sdds-background-${backgroundColor}" style="height:${height}px; width:${width}px; display:block"> <h1>background-color:${backgroundColor}</h1></div>
+  `;
+};
+
+export const backgroundColor = BackgroundColorTemplate.bind({});
+
+backgroundColor.argTypes = {
+  height: {
+    name: 'Height',
+    control: {
+      type: 'range',
+      min: 10,
+      max: 1000,
+      step: 10,
+    },
+  },
+  width: {
+    name: 'Width',
+    control: {
+      type: 'range',
+      min: 10,
+      max: 1000,
+      step: 10,
+    },
+  },
+};
+
+backgroundColor.args = {
+  backgroundColor: 'blue-100',
+  height: 500,
+  width: 500,
+};
