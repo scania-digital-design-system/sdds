f7686daf0b62cfab4d8dc4da8562f8d7f501abb4
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2022-01-31 12:40:04 +0100

    fix(storybook): amplify error

--- components/.storybook/main.js
+++ components/.storybook/main.js
@@ -2,7 +2,7 @@ module.exports = {
   stories: ['../src/**/**/*.stories.js'],
   addons: [
     '@storybook/addon-docs',
-    'storybook-addon-themes',
+    'storybook-addon-themes/register',
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-notes/register',
