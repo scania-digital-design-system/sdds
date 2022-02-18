c9d951c92117cdc7eac3e364706d2a5ab151b7e9
Rengfelt Annika <annika.rengfelt@scania.com>
2022-02-18 09:56:31 +0100

    refactor(tooltip): component layout and removing excess demo code

--- components/src/components/tooltips/tooltip.stories.js
+++ components/src/components/tooltips/tooltip.stories.js
@@ -3,7 +3,7 @@ export default {
   argTypes: {
     tooltipPosition: {
       control: {
-        type: 'select',
+        type: 'radio',
         options: [
           'bottom-start',
           'bottom',
@@ -27,25 +27,30 @@ export default {
 
 const ComponentTooltip = ({ ...Basic }) => `
   <sdds-theme></sdds-theme>
+    <sdds-tooltip 
+      placement="${Basic.tooltipPosition}"
+      selector="#button-1" 
+      text="${Basic.text}" 
+      mouse-over-tooltip="${Basic.mouseOverTooltip}">             
+    </sdds-tooltip>
+    <sdds-button type="primary" text="Hover me!" id="button-1"></sdds-button>
 
-  <div class="sdds-container" style="margin-top:40rem;margin-left:40rem;">
-    <div class="sdds-row">
-      <div class="sdds-col-xxlg-5 sdds-col-xlg-5 sdds-col-lg-5 sdds-col-md-5">
-        <sdds-tooltip placement="${Basic.tooltipPosition}" selector="#button-1" text="${Basic.text}" offset-distance="${Basic.offsetDistance}" offset-skidding="${Basic.offsetSkidding}" mouse-over-tooltip="${Basic.mouseOverTooltip}">             
-            <p>Paragraph tag inside of Tooltip with <b>bold</b> and <i>italic</i> tags too.</p>  
-        </sdds-tooltip>
-        <sdds-button type="primary" text="Button" id="button-1"></sdds-button>
-        <sdds-tooltip placement="${Basic.tooltipPosition}" selector="#button-2" text="${Basic.text}" offset-distance="${Basic.offsetDistance}" offset-skidding="${Basic.offsetSkidding}" mouse-over-tooltip="${Basic.mouseOverTooltip}" /></sdds-tooltip>
-        <sdds-button type="primary" text="Button" id="button-2"></sdds-button>
-      </div>
-    </div>
-  </div>
+    <sdds-tooltip 
+      placement="${Basic.tooltipPosition}" 
+      selector="#button-2" 
+      text="${Basic.text}" 
+      mouse-over-tooltip="${Basic.mouseOverTooltip}"/>
+      <p>Paragraph tag inside of Tooltip with <b>bold</b> and <i>italic</i> tags too.</p>
+    </sdds-tooltip>
+    <sdds-button type="secondary" text="Hover me!" id="button-2"></sdds-button>
+
   `;
 
 export const Basic = ComponentTooltip.bind({});
 Basic.args = {
   text: 'Text that will be displayed in tooltip',
-  offsetDistance: 20,
-  offsetSkidding: 30,
   mouseOverTooltip: false,
+};
+Basic.parameters = {
+  layout: 'centered',
 };
