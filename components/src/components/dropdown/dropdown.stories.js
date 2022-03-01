1218152c631d3f0af3292fc49754fb6bb1567e7b
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2022-03-01 15:42:52 +0100

    fix(dropdown): code review issue 1

--- components/src/components/dropdown/dropdown.stories.js
+++ components/src/components/dropdown/dropdown.stories.js
@@ -85,7 +85,13 @@ export default {
       name: 'Extra dropdown options',
       type: 'string',
       defaultValue:
-        '<sdds-dropdown-option value="option-4"> Copy this for more options... </sdds-dropdown-option>',
+        '<sdds-dropdown-option value="option-4"> Copy this for more options... </sdds-dropdown-option>' +
+        '<sdds-dropdown-option value="alpha-values-1">A B C D E F G H I J K L M N O P Q R S T U V W X Y Z Å Ä Ö</sdds-dropdown-option>\n' +
+        '<sdds-dropdown-option value="alpha-values-2">a b c d e f g h i j k l m n o p q r s t u v w x y z å ä ö</sdds-dropdown-option>\n' +
+        '<sdds-dropdown-option value="num-values">0 1 2 3 4 5 6 7 8 9</sdds-dropdown-option>\n' +
+        '<sdds-dropdown-option value="special-chars-1">! " # $ % & \' ( ) * + , - . /</sdds-dropdown-option>\n' +
+        '<sdds-dropdown-option value="special-chars-2">: ; < = > ? @</sdds-dropdown-option>\n' +
+        '<sdds-dropdown-option value="special-chars-3">[ \\ ] ^ _ ` { | } ~</sdds-dropdown-option>',
     },
   },
 };
@@ -114,7 +120,7 @@ const Template = ({
           state="${state}"
           type="${type}"
           default-option="${defaultOption}">
-          <sdds-dropdown-option value="option-1">Stockhol & Stockholm</sdds-dropdown-option>
+          <sdds-dropdown-option value="option-1">Stockholm & Stockholm</sdds-dropdown-option>
           <sdds-dropdown-option value="option-2">Hello 2</sdds-dropdown-option>
           <sdds-dropdown-option value="option-3">Option 3</sdds-dropdown-option>          
           ${extraDropdownOptions}
