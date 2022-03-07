e5dc9c1622f59e494c34a46a8696a5877f6c0eb1
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2022-03-07 15:56:54 +0100

    fix(table): setup base web-component file, improve css

--- components/src/components/table/table.tsx
+++ components/src/components/table/table.tsx
@@ -9,6 +9,44 @@ export class Table {
   @Prop() tableName: string;
 
   render() {
-    return <div>Hello table</div>;
+    return (
+      <table class="sdds-table sdds-table">
+        <caption>Table caption</caption>
+        <thead>
+          <tr>
+            <th>Header</th>
+            <th>Header</th>
+            <th>Header</th>
+            <th>Header</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Text</td>
+            <td>Text</td>
+            <td>Text</td>
+            <td>Text</td>
+          </tr>
+          <tr>
+            <td>Text</td>
+            <td>Text</td>
+            <td>Text</td>
+            <td>Text</td>
+          </tr>
+          <tr>
+            <td>Text</td>
+            <td>Text</td>
+            <td>Text</td>
+            <td>Text</td>
+          </tr>
+          <tr>
+            <td>Text</td>
+            <td>Text</td>
+            <td>Text</td>
+            <td>Text</td>
+          </tr>
+        </tbody>
+      </table>
+    );
   }
 }
