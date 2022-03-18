7312e8cb1515517dd830e21a2184d891322801e0
kjec2f <85104432+kjec2f@users.noreply.github.com>
2022-03-18 13:11:14 +0100

    feat(slider): pre-alhpa version (#204)
    * Splitting slider
    
    * Splitted to three component
    
    * Fixed some styling
    
    * fix: tiny fixes
    
    Co-authored-by: theJohnnyMe <nikola.johnny.mirkovic@scania.com>

--- components/src/components/slider/slider.tsx
+++ components/src/components/slider/slider.tsx
@@ -69,18 +69,6 @@ export class Slider {
     this.watchValueTwo();
   }
 
-  private handleOnClickPlus = () => {
-    parseInt(this.rangeStyle['--val']) < parseInt(this.rangeStyle['--max'])
-      ? this.updateValue(this.leftRangeInputEl.valueAsNumber + 1)
-      : '';
-  };
-
-  private handleOnClickMinus = () => {
-    parseInt(this.rangeStyle['--val']) > parseInt(this.rangeStyle['--min'])
-      ? this.updateValue(this.leftRangeInputEl.valueAsNumber - 1)
-      : '';
-  };
-
   private handleOnChangeLeft = (event) => {
     this.onInputTextChange(event.target.value, this.leftInputTextRef, true);
   };
@@ -174,7 +162,7 @@ export class Slider {
           max={`${this.rangeStyle['--max']}`}
           ref={(el) => (this.leftRangeInputEl = el as HTMLInputElement)}
           type="range"
-        ></input>
+        />
       </div>
     );
   }
@@ -183,33 +171,7 @@ export class Slider {
     return (
       <div>
         <div style={this.rangeStyle} class="container">
-          <div
-            class="sliderspan"
-            ref={(el) => (this.sliderspan = el as HTMLInputElement)}
-          >
-            <span
-              class="spantext"
-              ref={(el) => (this.spantext = el as HTMLInputElement)}
-            >
-              {this.rangeStyle['--val']}
-            </span>
-            <span class="spantrianlge"></span>
-          </div>
-          <button onClick={this.handleOnClickMinus}>
-            {' '}
-            <span>-</span>{' '}
-          </button>
-          <input
-            min={`${this.rangeStyle['--min']}`}
-            max={`${this.rangeStyle['--max']}`}
-            value={`${this.rangeStyle['--val']}`}
-            ref={(el) => (this.leftRangeInputEl = el as HTMLInputElement)}
-            type="range"
-          ></input>
-          <button onClick={this.handleOnClickPlus}>
-            {' '}
-            <span>+</span>{' '}
-          </button>
+          <sdds-continousvalue-slider />
         </div>
       </div>
     );
@@ -226,7 +188,7 @@ export class Slider {
           ref={(el) => (this.leftInputTextRef = el as HTMLInputElement)}
           class="input-text"
           onKeyUp={(ev) => this.handleOnChangeLeft(ev)}
-        ></input>
+        />
         <div
           class="range-slider"
           style={{
@@ -242,7 +204,7 @@ export class Slider {
             class="sliders"
             style={this.rangeStyle}
             ref={(el) => (this.leftRangeInputEl = el as HTMLInputElement)}
-          ></input>
+          />
           <input
             min={`${this.secondRangeStyle['--min']}`}
             max={`${this.secondRangeStyle['--max']}`}
@@ -251,7 +213,7 @@ export class Slider {
             class="sliders sliders-right"
             ref={(el) => (this.rightRangeInputEl = el as HTMLInputElement)}
             style={this.secondRangeStyle}
-          ></input>
+          />
         </div>
         <input
           min={`${this.secondRangeStyle['--min']}`}
@@ -261,7 +223,7 @@ export class Slider {
           ref={(el) => (this.rightInputTextRef = el as HTMLInputElement)}
           class="input-text"
           onKeyUp={(ev) => this.handleOnChangeRight(ev)}
-        ></input>
+        />
       </div>
     );
   }
