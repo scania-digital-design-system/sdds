cda55f1adb48ae83579e5d35894b993311c5125e
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2022-03-29 15:43:56 +0200

    fix(table-header-element): icons change and animation

--- components/src/components/table/table-header-element.tsx
+++ components/src/components/table/table-header-element.tsx
@@ -41,6 +41,10 @@ export class TableHeaderElement {
     console.log(`Header has got this info: ${keyValue}`);
     if (keyValue !== this.columnKey) {
       this.sortedByMyKey = false;
+      // To sync with CSS transition timing
+      setTimeout(() => {
+        this.sortingDirection = '';
+      }, 200);
     }
   }
 
@@ -66,31 +70,52 @@ export class TableHeaderElement {
         >
           <span class="sdds-table__header-button-text">{this.columnTitle}</span>
 
-          <svg
-            class="sdds-table__header-button-icon"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 12 15"
-          >
-            <path
-              fill-rule="evenodd"
-              clip-rule="evenodd"
-              d="M8.45 13.67V4.62a.5.5 0 0 1 1 0v9.05h-1Z"
-              fill="currentColor"
-            />
-            <path
-              fill-rule="evenodd"
-              clip-rule="evenodd"
-              d="M6.07 10.28a.5.5 0 0 1 .7.08l2.1 2.66a.1.1 0 0 0 .15 0l2.09-2.66a.5.5 0 1 1 .78.62l-2.08 2.66a1.1 1.1 0 0 1-1.73 0l-2.1-2.66a.5.5 0 0 1 .1-.7ZM3.55.4v9.04a.5.5 0 1 1-1 0V.39h1Z"
-              fill="currentColor"
-            />
-            <path
-              fill-rule="evenodd"
-              clip-rule="evenodd"
-              d="M5.93 3.78a.5.5 0 0 1-.7-.08l-2.1-2.66a.1.1 0 0 0-.15 0L.89 3.7a.5.5 0 0 1-.78-.62L2.19.42a1.1 1.1 0 0 1 1.73 0l2.1 2.66a.5.5 0 0 1-.1.7Z"
-              fill="currentColor"
-            />
-          </svg>
+          {this.sortingDirection === '' && (
+            <svg
+              class="sdds-table__header-button-icon"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 12 15"
+            >
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M8.45 13.67V4.62a.5.5 0 0 1 1 0v9.05h-1Z"
+                fill="currentColor"
+              />
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M6.07 10.28a.5.5 0 0 1 .7.08l2.1 2.66a.1.1 0 0 0 .15 0l2.09-2.66a.5.5 0 1 1 .78.62l-2.08 2.66a1.1 1.1 0 0 1-1.73 0l-2.1-2.66a.5.5 0 0 1 .1-.7ZM3.55.4v9.04a.5.5 0 1 1-1 0V.39h1Z"
+                fill="currentColor"
+              />
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M5.93 3.78a.5.5 0 0 1-.7-.08l-2.1-2.66a.1.1 0 0 0-.15 0L.89 3.7a.5.5 0 0 1-.78-.62L2.19.42a1.1 1.1 0 0 1 1.73 0l2.1 2.66a.5.5 0 0 1-.1.7Z"
+                fill="currentColor"
+              />
+            </svg>
+          )}
+          {this.sortingDirection !== '' && (
+            <svg
+              class={`sdds-table__header-button-icon ${
+                this.sortingDirection === 'desc'
+                  ? 'sdds-table__header-button-icon--rotate'
+                  : ''
+              }`}
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 32 32"
+            >
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M17 2.974a1 1 0 0 0-2 0v24.3l-9.312-9.312a1 1 0 0 0-1.414 1.414l9.887 9.888a2.6 2.6 0 0 0 3.677 0l9.888-9.888a1 1 0 0 0-1.414-1.414L17 27.274v-24.3Z"
+                fill="currentColor"
+              />
+            </svg>
+          )}
         </button>
       );
     }
