4bb16f0ef1246d72981a75fb45f33c1e99adc691
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2022-04-19 09:31:54 +0200

    fix(data-table): post release improvements (#355)

--- components/src/components/table/table.stories.js
+++ components/src/components/table/table.stories.js
@@ -2,40 +2,40 @@ export default {
   title: 'Component/Table',
 };
 const Template = ({ type, divider }) => `
-    <table class="sdds-table sdds-table-${type} sdds-table-${divider}">
+    <table class="sdds-table sdds-table--${type} sdds-table--${divider}">
     <caption class="sdds-table__title">Table title</caption>       
     <thead class="sdds-table__header">
       <tr class="sdds-table__row">
-        <th class="sdds-table__header-cell">Header</th>
-        <th class="sdds-table__header-cell">Header</th>
-        <th class="sdds-table__header-cell">Header</th>
-        <th class="sdds-table__header-cell">Header</th>
+        <th class="sdds-table__header-cell sdds-u-pr2 sdds-u-pl2">Header</th>
+        <th class="sdds-table__header-cell sdds-u-pr2 sdds-u-pl2">Header</th>
+        <th class="sdds-table__header-cell sdds-u-pr2 sdds-u-pl2">Header</th>
+        <th class="sdds-table__header-cell sdds-u-pr2 sdds-u-pl2">Header</th>
       </tr>
     </thead>
      <tbody class="sdds-table__body">
       <tr class="sdds-table__row">
-        <td class="sdds-table__body-cell">Text</td>
-        <td class="sdds-table__body-cell">Text</td>
-        <td class="sdds-table__body-cell">Text</td>
-        <td class="sdds-table__body-cell">Text</td>
+        <td class="sdds-table__body-cell  sdds-u-pr2 sdds-u-pl2">Text</td>
+        <td class="sdds-table__body-cell  sdds-u-pr2 sdds-u-pl2">Text</td>
+        <td class="sdds-table__body-cell  sdds-u-pr2 sdds-u-pl2">Text</td>
+       <td class="sdds-table__body-cell  sdds-u-pr2 sdds-u-pl2">Text</td>
       </tr>
       <tr class="sdds-table__row">
-        <td class="sdds-table__body-cell">Text</td>
-        <td class="sdds-table__body-cell">Text</td>
-        <td class="sdds-table__body-cell">Text</td>
-        <td class="sdds-table__body-cell">Text</td>
+        <td class="sdds-table__body-cell  sdds-u-pr2 sdds-u-pl2">Text</td>
+        <td class="sdds-table__body-cell  sdds-u-pr2 sdds-u-pl2">Text</td>
+        <td class="sdds-table__body-cell  sdds-u-pr2 sdds-u-pl2">Text</td>
+       <td class="sdds-table__body-cell  sdds-u-pr2 sdds-u-pl2">Text</td>
       </tr>
       <tr class="sdds-table__row">
-        <td class="sdds-table__body-cell">Text</td>
-        <td class="sdds-table__body-cell">Text</td>
-        <td class="sdds-table__body-cell">Text</td>
-        <td class="sdds-table__body-cell">Text</td>
+        <td class="sdds-table__body-cell  sdds-u-pr2 sdds-u-pl2">Text</td>
+        <td class="sdds-table__body-cell  sdds-u-pr2 sdds-u-pl2">Text</td>
+        <td class="sdds-table__body-cell  sdds-u-pr2 sdds-u-pl2">Text</td>
+       <td class="sdds-table__body-cell  sdds-u-pr2 sdds-u-pl2">Text</td>
       </tr>
       <tr class="sdds-table__row">
-        <td class="sdds-table__body-cell">Text</td>
-        <td class="sdds-table__body-cell">Text</td>
-        <td class="sdds-table__body-cell">Text</td>
-        <td class="sdds-table__body-cell">Text</td>
+        <td class="sdds-table__body-cell  sdds-u-pr2 sdds-u-pl2">Text</td>
+        <td class="sdds-table__body-cell  sdds-u-pr2 sdds-u-pl2">Text</td>
+        <td class="sdds-table__body-cell  sdds-u-pr2 sdds-u-pl2">Text</td>
+       <td class="sdds-table__body-cell  sdds-u-pr2 sdds-u-pl2">Text</td>
       </tr>
     </tbody>
   </table>
@@ -61,41 +61,41 @@ CompactDividers.args = {
   divider: 'divider',
 };
 const Table = ({ type, divider }) => ` 
-    <table class="sdds-table sdds-table-${type} sdds-table-${divider}">   
-      <thead>
-        <tr>
-          <th>Header</th>
-          <th>Header</th>
-          <th>Header</th>
-          <th>Header</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Text</td>
-          <td>Text</td>
-          <td>Text</td>
-          <td>Text</td>
-        </tr>
-        <tr>
-          <td>Text</td>
-          <td>Text</td>
-          <td>Text</td>
-          <td>Text</td>
-        </tr>
-        <tr>
-          <td>Text</td>
-          <td>Text</td>
-          <td>Text</td>
-          <td>Text</td>
-        </tr>
-        <tr>
-          <td>Text</td>
-          <td>Text</td>
-          <td>Text</td>
-          <td>Text</td>
-        </tr>
-      </tbody>
+    <table class="sdds-table sdds-table--${type} sdds-table--${divider}">   
+   <thead class="sdds-table__header">
+      <tr class="sdds-table__row">
+        <th class="sdds-table__header-cell sdds-u-pr2 sdds-u-pl2">Header</th>
+        <th class="sdds-table__header-cell sdds-u-pr2 sdds-u-pl2">Header</th>
+        <th class="sdds-table__header-cell sdds-u-pr2 sdds-u-pl2">Header</th>
+        <th class="sdds-table__header-cell sdds-u-pr2 sdds-u-pl2">Header</th>
+      </tr>
+    </thead>
+     <tbody class="sdds-table__body">
+      <tr class="sdds-table__row">
+        <td class="sdds-table__body-cell  sdds-u-pr2 sdds-u-pl2">Text</td>
+        <td class="sdds-table__body-cell  sdds-u-pr2 sdds-u-pl2">Text</td>
+        <td class="sdds-table__body-cell  sdds-u-pr2 sdds-u-pl2">Text</td>
+       <td class="sdds-table__body-cell  sdds-u-pr2 sdds-u-pl2">Text</td>
+      </tr>
+      <tr class="sdds-table__row">
+        <td class="sdds-table__body-cell  sdds-u-pr2 sdds-u-pl2">Text</td>
+        <td class="sdds-table__body-cell  sdds-u-pr2 sdds-u-pl2">Text</td>
+        <td class="sdds-table__body-cell  sdds-u-pr2 sdds-u-pl2">Text</td>
+       <td class="sdds-table__body-cell  sdds-u-pr2 sdds-u-pl2">Text</td>
+      </tr>
+      <tr class="sdds-table__row">
+        <td class="sdds-table__body-cell  sdds-u-pr2 sdds-u-pl2">Text</td>
+        <td class="sdds-table__body-cell  sdds-u-pr2 sdds-u-pl2">Text</td>
+        <td class="sdds-table__body-cell  sdds-u-pr2 sdds-u-pl2">Text</td>
+       <td class="sdds-table__body-cell  sdds-u-pr2 sdds-u-pl2">Text</td>
+      </tr>
+      <tr class="sdds-table__row">
+        <td class="sdds-table__body-cell  sdds-u-pr2 sdds-u-pl2">Text</td>
+        <td class="sdds-table__body-cell  sdds-u-pr2 sdds-u-pl2">Text</td>
+        <td class="sdds-table__body-cell  sdds-u-pr2 sdds-u-pl2">Text</td>
+       <td class="sdds-table__body-cell  sdds-u-pr2 sdds-u-pl2">Text</td>
+      </tr>
+    </tbody>
     </table>
     `;
 export const TableHeaderOnly = Table.bind({});
