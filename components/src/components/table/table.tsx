42ccb124a19ca039110d6fba46684b6b485329be
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2022-05-20 11:15:13 +0200

    feat: searchbar style

--- components/src/components/table/table.tsx
+++ components/src/components/table/table.tsx
@@ -284,10 +284,19 @@ export class Table {
     // grab the value of search and turn to small caps
     const searchTerm = event.currentTarget.value.toLowerCase();
 
+    const sddsTableSearchBar = event.currentTarget.parentElement;
+
+    if (searchTerm.length > 0) {
+      sddsTableSearchBar.classList.add('sdds-table__searchbar--active');
+    } else {
+      sddsTableSearchBar.classList.remove('sdds-table__searchbar--active');
+    }
+
     // grab all rows in body
-    const dataRows = event.currentTarget.parentElement.parentElement
-      .getElementsByClassName('sdds-table__body')[0]
-      .querySelectorAll('.sdds-table__row');
+    const dataRows =
+      event.currentTarget.parentElement.parentElement.parentElement.parentElement
+        .getElementsByClassName('sdds-table__body')[0]
+        .querySelectorAll('.sdds-table__row');
 
     // turn it into array
     const dataRowsToArray = [...dataRows];
@@ -352,14 +361,32 @@ export class Table {
           ref={(table) => (this.tableSelector = table)}
         >
           {this.tableTitle && (
-            <caption class="sdds-table__title">
-              {this.tableTitle}
-              <input
-                class="sdds-table__searchbar"
-                type="text"
-                onKeyUp={(event) => this.searchFunction(event)}
-              />
-            </caption>
+            <div class="sdds-table__upper-bar">
+              <div class="sdds-table__upper-bar-flex">
+                <caption class="sdds-table__title">{this.tableTitle}</caption>
+                <div class="sdds-table__searchbar">
+                  <input
+                    class="sdds-table__searchbar-input"
+                    type="text"
+                    onKeyUp={(event) => this.searchFunction(event)}
+                  />
+                  <span class="sdds-table__searchbar-icon">
+                    <svg
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 32 32"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        clip-rule="evenodd"
+                        d="M12.942 1.985c-6.051 0-10.957 4.905-10.957 10.957 0 6.051 4.906 10.957 10.957 10.957 2.666 0 5.109-.952 7.008-2.534l8.332 8.331a1 1 0 1 0 1.414-1.414l-8.331-8.331a10.912 10.912 0 0 0 2.534-7.01c0-6.05-4.905-10.956-10.957-10.956ZM3.985 12.942a8.957 8.957 0 1 1 17.914 0 8.957 8.957 0 0 1-17.914 0Z"
+                        fill="currentColor"
+                      />
+                    </svg>
+                  </span>
+                </div>
+              </div>
+            </div>
           )}
           <thead class="sdds-table__header">
             <tr class="sdds-table__header-row">
