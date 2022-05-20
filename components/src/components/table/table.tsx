4c897e5e8b42b377f6b288808033a1a350b37a60
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2022-05-20 16:58:52 +0200

    fix: text-field animation, prop for filtering, story file update

--- components/src/components/table/table.tsx
+++ components/src/components/table/table.tsx
@@ -32,6 +32,8 @@ export class Table {
 
   @Prop({ reflect: true }) multiSelect: boolean = false;
 
+  @Prop({ reflect: true }) filtering: boolean = true;
+
   @Prop() bodyData: any = `[
       {
           "truck": "L-series",
@@ -360,31 +362,33 @@ export class Table {
           }}
           ref={(table) => (this.tableSelector = table)}
         >
-          {this.tableTitle && (
+          {(this.tableTitle || this.filtering) && (
             <div class="sdds-table__upper-bar">
               <div class="sdds-table__upper-bar-flex">
                 <caption class="sdds-table__title">{this.tableTitle}</caption>
-                <div class="sdds-table__searchbar">
-                  <input
-                    class="sdds-table__searchbar-input"
-                    type="text"
-                    onKeyUp={(event) => this.searchFunction(event)}
-                  />
-                  <span class="sdds-table__searchbar-icon">
-                    <svg
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 32 32"
-                    >
-                      <path
-                        fill-rule="evenodd"
-                        clip-rule="evenodd"
-                        d="M12.942 1.985c-6.051 0-10.957 4.905-10.957 10.957 0 6.051 4.906 10.957 10.957 10.957 2.666 0 5.109-.952 7.008-2.534l8.332 8.331a1 1 0 1 0 1.414-1.414l-8.331-8.331a10.912 10.912 0 0 0 2.534-7.01c0-6.05-4.905-10.956-10.957-10.956ZM3.985 12.942a8.957 8.957 0 1 1 17.914 0 8.957 8.957 0 0 1-17.914 0Z"
-                        fill="currentColor"
-                      />
-                    </svg>
-                  </span>
-                </div>
+                {this.filtering && (
+                  <div class="sdds-table__searchbar">
+                    <input
+                      class="sdds-table__searchbar-input"
+                      type="text"
+                      onKeyUp={(event) => this.searchFunction(event)}
+                    />
+                    <span class="sdds-table__searchbar-icon">
+                      <svg
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 32 32"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          clip-rule="evenodd"
+                          d="M12.942 1.985c-6.051 0-10.957 4.905-10.957 10.957 0 6.051 4.906 10.957 10.957 10.957 2.666 0 5.109-.952 7.008-2.534l8.332 8.331a1 1 0 1 0 1.414-1.414l-8.331-8.331a10.912 10.912 0 0 0 2.534-7.01c0-6.05-4.905-10.956-10.957-10.956ZM3.985 12.942a8.957 8.957 0 1 1 17.914 0 8.957 8.957 0 0 1-17.914 0Z"
+                          fill="currentColor"
+                        />
+                      </svg>
+                    </span>
+                  </div>
+                )}
               </div>
             </div>
           )}
