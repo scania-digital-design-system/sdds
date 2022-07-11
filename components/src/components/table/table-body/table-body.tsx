bd61dfe7fdd15ff99b8095b21da6888571379f0f
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2022-07-11 14:58:48 +0200

    fix: moving table body in separate folder

--- components/src/components/table/table-body/table-body.tsx
+++ components/src/components/table/table-body/table-body.tsx
@@ -2,13 +2,13 @@ import { Component, h, Host } from '@stencil/core';
 
 @Component({
   tag: 'sdds-table-body',
-  styleUrl: 'table.scss',
+  styleUrl: 'table-body.scss',
   shadow: true,
 })
 export class TableBody {
   render() {
     return (
-      <Host class="sdds-table__body">
+      <Host>
         <slot></slot>
       </Host>
     );
