f865741e2ec6eb14c570c8dfe2f3783467488cf9
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2022-07-11 11:06:49 +0200

    feat: table header as separate component

--- components/src/components/table/table-header/table-header.tsx
+++ components/src/components/table/table-header/table-header.tsx
@@ -2,13 +2,13 @@ import { Component, h, Host } from '@stencil/core';
 
 @Component({
   tag: 'sdds-table-header',
-  styleUrl: 'table.scss',
+  styleUrl: 'table-header.scss',
   shadow: true,
 })
 export class TableHeader {
   render() {
     return (
-      <Host class="sdds-table__header">
+      <Host>
         <slot></slot>
       </Host>
     );
