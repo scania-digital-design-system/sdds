fa27fd536dbbdcabd444d3e3dec22003c54ffaf8
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2022-07-19 10:33:42 +0200

    fix: improvement for column counting

--- components/src/components/data-table/table-component-test.stories.js
+++ components/src/components/data-table/table-component-test.stories.js
@@ -3,28 +3,23 @@ export default {
 };
 const Template = () => `
    <sdds-table id="johnny-test-id" enable-multiselect>
-          <sdds-table-toolbar enable-filtering table-title="Table title">
-            <button slot="sdds-table__actionbar">Test</button>
-          </sdds-table-toolbar>        
+          
           <sdds-table-header>
               <sdds-header-cell column-key='truck' column-title='Truck type' ></sdds-header-cell>
               <sdds-header-cell column-key='driver' column-title='Driver name' sortable ></sdds-header-cell>
               <sdds-header-cell column-key='country' column-title='Country' sortable ></sdds-header-cell>
               <sdds-header-cell column-key='mileage' column-title='Mileage' sortable text-align='right' ></sdds-header-cell> 
           </sdds-table-header>       
-          <sdds-table-body>       
-              <sdds-table-body-row>
-                  <sdds-body-cell cell-value="Test value 1" cell-key="truck"></sdds-body-cell>
-                  <sdds-body-cell cell-value="Test value 2" cell-key="driver"></sdds-body-cell>
-                  <sdds-body-cell cell-value="Test value 3" cell-key="country"></sdds-body-cell>
-                  <sdds-body-cell cell-value="Test value 4" cell-key="mileage"></sdds-body-cell>            
-              </sdds-table-body-row>
+          <sdds-table-body>  
               <sdds-table-body-row>
                   <sdds-body-cell cell-value="Test value 5" cell-key="truck"></sdds-body-cell>
                   <sdds-body-cell cell-value="Test value 6" cell-key="driver"></sdds-body-cell>
                   <sdds-body-cell cell-value="Test value 7" cell-key="country"></sdds-body-cell>
-                  <sdds-body-cell cell-value="Test value 8" cell-key="mileage"></sdds-body-cell>            
-              </sdds-table-body-row>              
+                  <sdds-body-cell cell-value="Test value 8" cell-key="mileage"></sdds-body-cell>  
+                  <div slot="extended">
+                  <p></p>
+</div>          
+              </sdds-table-body-row>       
           </sdds-table-body>
         <sdds-table-footer enable-pagination rows-per-page="2"></sdds-table-footer>
   </sdds-table>  
