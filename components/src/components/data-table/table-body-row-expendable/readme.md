4e86df57ab5f0d393cb4c0376d1900b7dd525b50
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2022-07-20 11:56:44 +0200

    feat: init work on expendable table row

---
+++ components/src/components/data-table/table-body-row-expendable/readme.md
@@ -0,0 +1,10 @@
+# sdds-table-body-row-expended
+
+
+
+<!-- Auto Generated Below -->
+
+
+----------------------------------------------
+
+*Built with [StencilJS](https://stenciljs.com/)*
---
+++ components/src/components/data-table/table-body-row-expendable/table-body-row-expendable.tsx
@@ -0,0 +1,59 @@
+import { Component, h, Host, State } from '@stencil/core';
+
+@Component({
+  tag: 'sdds-table-body-row-expendable',
+  styleUrl: 'table-body-row-expendable.scss',
+  shadow: true,
+})
+export class TableBodyRowExpandable {
+  @State() isExtended: boolean = false;
+
+  onChangeHandler(event) {
+    this.isExtended = event.currentTarget.checked;
+  }
+
+  render() {
+    return (
+      <Host>
+        <tr class="sdds-table__row">
+          <td class="sdds-table__cell">
+            <div class="sdds-table__input-container">
+              <label>
+                <input
+                  type="checkbox"
+                  onChange={(event) => this.onChangeHandler(event)}
+                />
+                <span>
+                  <svg
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 32 32"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      d="M4.273 9.783a1 1 0 0 1 1.415 0l9.888 9.888a.6.6 0 0 0 .848 0l9.888-9.888a1 1 0 1 1 1.415 1.414l-9.889 9.889a2.6 2.6 0 0 1-3.677 0l-9.888-9.889a1 1 0 0 1 0-1.414Z"
+                      fill="currentColor"
+                    />
+                  </svg>
+                </span>
+              </label>
+            </div>
+          </td>
+          <slot />
+        </tr>
+
+        <tr
+          class={{
+            'sdds-table__row-extend': true,
+            'sdds-table__row-extend--opened': this.isExtended,
+          }}
+        >
+          <td class="sdds-table__cell-extend" colSpan={5}>
+            <slot name="extend" />
+          </td>
+        </tr>
+      </Host>
+    );
+  }
+}
