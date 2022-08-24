432f9ba68b5496bbbefcebae307e32fbcd982aac
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2022-08-24 13:56:29 +0200

    fix: stories separation

--- components/src/components/data-table/table-component-test.stories.js
+++ components/src/components/data-table/table-component-test.stories.js
@@ -1,136 +1,61 @@
 export default {
-  title: 'Component/Data Table',
+  title: 'Component/Data Table/Web Component',
 };
-const Template = () => `
 
-  <h3>Expandable rows</h3>
-  <sdds-table id="expendable-rows-table" enable-expandable-rows>
-      <sdds-table-header>         
-          <sdds-header-cell column-key='truck' column-title='Truck type'></sdds-header-cell>
-          <sdds-header-cell column-key='driver' column-title='Driver name'></sdds-header-cell>
-          <sdds-header-cell column-key='country' column-title='Country'></sdds-header-cell>
-          <sdds-header-cell column-key='mileage' column-title='Mileage'  text-align='right'></sdds-header-cell> 
-      </sdds-table-header>                 
-      <sdds-table-body>           
-        <sdds-table-body-row-expandable>  
-          <sdds-body-cell cell-value="Test value 1" cell-key="truck"></sdds-body-cell>
-          <sdds-body-cell cell-value="Test value 2" cell-key="driver"></sdds-body-cell>
-          <sdds-body-cell cell-value="Test value 3" cell-key="country"></sdds-body-cell>
-          <sdds-body-cell cell-value="Test value 4" cell-key="mileage"></sdds-body-cell> 
-          <div slot="expand-row">Hello world 1</div>
-        </sdds-table-body-row-expandable>            
-         <sdds-table-body-row-expandable>  
-          <sdds-body-cell cell-value="Test value 5" cell-key="truck"></sdds-body-cell>
-          <sdds-body-cell cell-value="Test value 6" cell-key="driver"></sdds-body-cell>
-          <sdds-body-cell cell-value="Test value 7" cell-key="country"></sdds-body-cell>
-          <sdds-body-cell cell-value="Test value 8" cell-key="mileage"></sdds-body-cell> 
-          <div slot="expand-row">Hello to you too</div>
-        </sdds-table-body-row-expandable>            
-        <sdds-table-body-row-expandable>  
-          <sdds-body-cell cell-value="Test value 9" cell-key="truck"></sdds-body-cell>
-          <sdds-body-cell cell-value="Test value 10" cell-key="driver"></sdds-body-cell>
-          <sdds-body-cell cell-value="Test value 11" cell-key="country"></sdds-body-cell>
-          <sdds-body-cell cell-value="Test value 12" cell-key="mileage"></sdds-body-cell> 
-          <div slot="expand-row"><sdds-button type="primary" text="Call to action"></sdds-button></div>
-        </sdds-table-body-row-expandable>            
-      </sdds-table-body>
-  </sdds-table> 
-  
-  <br>
-  <br>
-  
-  <h3>Disabled filtering, pagination and sorting - left to the user to listen to events</h3>
-   <sdds-table id="disabled-functionality-table">
-          <sdds-table-toolbar table-title="Disabled functionalities table" enable-filtering></sdds-table-toolbar>
-          <sdds-table-header>
-              <sdds-header-cell column-key='truck' column-title='Truck type'></sdds-header-cell>
-              <sdds-header-cell column-key='driver' column-title='Driver name' sortable></sdds-header-cell>
-              <sdds-header-cell column-key='country' column-title='Country' sortable></sdds-header-cell>
-              <sdds-header-cell column-key='mileage' column-title='Mileage' sortable text-align='right'></sdds-header-cell> 
-          </sdds-table-header>       
-          <sdds-table-body disable-pagination-function disable-filtering-function disable-sorting-function enable-dummy-data>  
-              <sdds-table-body-row>
-                  <sdds-body-cell cell-value="Test value 5" cell-key="truck"></sdds-body-cell>
-                  <sdds-body-cell cell-value="Test value 6" cell-key="driver"></sdds-body-cell>
-                  <sdds-body-cell cell-value="Test value 7" cell-key="country"></sdds-body-cell>
-                  <sdds-body-cell cell-value="Test value 8" cell-key="mileage"></sdds-body-cell>           
-              </sdds-table-body-row>       
-          </sdds-table-body>
-        <sdds-table-footer enable-client-pagination client-max-pages="10"></sdds-table-footer>
-  </sdds-table>  
-  
-  <br>
-  <br>
+const BasicTemplate = () => `
   <h3>Basic table</h3>
   
   <sdds-table id="basic-table">
-              
-          <sdds-table-header>
-              <sdds-header-cell column-key='truck' column-title='Truck type'></sdds-header-cell>
-              <sdds-header-cell column-key='driver' column-title='Driver name'></sdds-header-cell>
-              <sdds-header-cell column-key='country' column-title='Country'></sdds-header-cell>
-              <sdds-header-cell column-key='mileage' column-title='Mileage' text-align='right'></sdds-header-cell> 
-          </sdds-table-header>       
-          <sdds-table-body>       
-              <sdds-table-body-row>
-                  <sdds-body-cell cell-value="Test value 1" cell-key="truck"></sdds-body-cell>
-                  <sdds-body-cell cell-value="Test value 2" cell-key="driver"></sdds-body-cell>
-                  <sdds-body-cell cell-value="Test value 3" cell-key="country"></sdds-body-cell>
-                  <sdds-body-cell cell-value="Test value 4" cell-key="mileage"></sdds-body-cell>            
-              </sdds-table-body-row>
-              <sdds-table-body-row>
-                  <sdds-body-cell cell-value="Test value 5" cell-key="truck"></sdds-body-cell>
-                  <sdds-body-cell cell-value="Test value 6" cell-key="driver"></sdds-body-cell>
-                  <sdds-body-cell cell-value="Test value 7" cell-key="country"></sdds-body-cell>
-                  <sdds-body-cell cell-value="Test value 8" cell-key="mileage"></sdds-body-cell>            
-              </sdds-table-body-row>   
-              <sdds-table-body-row>
-                  <sdds-body-cell cell-value="Test value 1" cell-key="truck"></sdds-body-cell>
-                  <sdds-body-cell cell-value="Test value 2" cell-key="driver"></sdds-body-cell>
-                  <sdds-body-cell cell-value="Test value 3" cell-key="country"></sdds-body-cell>
-                  <sdds-body-cell cell-value="Test value 4" cell-key="mileage"></sdds-body-cell>            
-              </sdds-table-body-row>
-              <sdds-table-body-row>
-                  <sdds-body-cell cell-value="Test value 5" cell-key="truck"></sdds-body-cell>
-                  <sdds-body-cell cell-value="Test value 6" cell-key="driver"></sdds-body-cell>
-                  <sdds-body-cell cell-value="Test value 7" cell-key="country"></sdds-body-cell>
-                  <sdds-body-cell cell-value="Test value 8" cell-key="mileage"></sdds-body-cell>            
-              </sdds-table-body-row> 
-              <sdds-table-body-row>
-                  <sdds-body-cell cell-value="Test value 1" cell-key="truck"></sdds-body-cell>
-                  <sdds-body-cell cell-value="Test value 2" cell-key="driver"></sdds-body-cell>
-                  <sdds-body-cell cell-value="Test value 3" cell-key="country"></sdds-body-cell>
-                  <sdds-body-cell cell-value="Test value 4" cell-key="mileage"></sdds-body-cell>            
-              </sdds-table-body-row>
-              <sdds-table-body-row>
-                  <sdds-body-cell cell-value="Test value 5" cell-key="truck"></sdds-body-cell>
-                  <sdds-body-cell cell-value="Test value 6" cell-key="driver"></sdds-body-cell>
-                  <sdds-body-cell cell-value="Test value 7" cell-key="country"></sdds-body-cell>
-                  <sdds-body-cell cell-value="Test value 8" cell-key="mileage"></sdds-body-cell>            
-              </sdds-table-body-row>            
-          </sdds-table-body>
-  </sdds-table>  
-  
-  
-  <br>
-  <br>
-  
-  <h3>Filtering and sorting example</h3>
-   <sdds-table id="filtering-table">
-          <sdds-table-toolbar table-title="Filter & Sort" enable-filtering></sdds-table-toolbar>
-          <sdds-table-header>
-              <sdds-header-cell column-key='truck' column-title='Truck type'></sdds-header-cell>
-              <sdds-header-cell column-key='driver' column-title='Driver name' sortable></sdds-header-cell>
-              <sdds-header-cell column-key='country' column-title='Country' sortable></sdds-header-cell>
-              <sdds-header-cell column-key='mileage' column-title='Mileage' sortable text-align='right'></sdds-header-cell> 
-          </sdds-table-header>       
-          <sdds-table-body enable-dummy-data>                      
-          </sdds-table-body>
-  </sdds-table>  
-  
-  <br>
-  <br>
-  
+      <sdds-table-header>
+          <sdds-header-cell column-key='truck' column-title='Truck type'></sdds-header-cell>
+          <sdds-header-cell column-key='driver' column-title='Driver name'></sdds-header-cell>
+          <sdds-header-cell column-key='country' column-title='Country'></sdds-header-cell>
+          <sdds-header-cell column-key='mileage' column-title='Mileage' text-align='right'></sdds-header-cell> 
+      </sdds-table-header>       
+      <sdds-table-body>       
+          <sdds-table-body-row>
+              <sdds-body-cell cell-value="Test value 1" cell-key="truck"></sdds-body-cell>
+              <sdds-body-cell cell-value="Test value 2" cell-key="driver"></sdds-body-cell>
+              <sdds-body-cell cell-value="Test value 3" cell-key="country"></sdds-body-cell>
+              <sdds-body-cell cell-value="Test value 4" cell-key="mileage"></sdds-body-cell>            
+          </sdds-table-body-row>
+          <sdds-table-body-row>
+              <sdds-body-cell cell-value="Test value 5" cell-key="truck"></sdds-body-cell>
+              <sdds-body-cell cell-value="Test value 6" cell-key="driver"></sdds-body-cell>
+              <sdds-body-cell cell-value="Test value 7" cell-key="country"></sdds-body-cell>
+              <sdds-body-cell cell-value="Test value 8" cell-key="mileage"></sdds-body-cell>            
+          </sdds-table-body-row>   
+          <sdds-table-body-row>
+              <sdds-body-cell cell-value="Test value 1" cell-key="truck"></sdds-body-cell>
+              <sdds-body-cell cell-value="Test value 2" cell-key="driver"></sdds-body-cell>
+              <sdds-body-cell cell-value="Test value 3" cell-key="country"></sdds-body-cell>
+              <sdds-body-cell cell-value="Test value 4" cell-key="mileage"></sdds-body-cell>            
+          </sdds-table-body-row>
+          <sdds-table-body-row>
+              <sdds-body-cell cell-value="Test value 5" cell-key="truck"></sdds-body-cell>
+              <sdds-body-cell cell-value="Test value 6" cell-key="driver"></sdds-body-cell>
+              <sdds-body-cell cell-value="Test value 7" cell-key="country"></sdds-body-cell>
+              <sdds-body-cell cell-value="Test value 8" cell-key="mileage"></sdds-body-cell>            
+          </sdds-table-body-row> 
+          <sdds-table-body-row>
+              <sdds-body-cell cell-value="Test value 1" cell-key="truck"></sdds-body-cell>
+              <sdds-body-cell cell-value="Test value 2" cell-key="driver"></sdds-body-cell>
+              <sdds-body-cell cell-value="Test value 3" cell-key="country"></sdds-body-cell>
+              <sdds-body-cell cell-value="Test value 4" cell-key="mileage"></sdds-body-cell>            
+          </sdds-table-body-row>
+          <sdds-table-body-row>
+              <sdds-body-cell cell-value="Test value 5" cell-key="truck"></sdds-body-cell>
+              <sdds-body-cell cell-value="Test value 6" cell-key="driver"></sdds-body-cell>
+              <sdds-body-cell cell-value="Test value 7" cell-key="country"></sdds-body-cell>
+              <sdds-body-cell cell-value="Test value 8" cell-key="mileage"></sdds-body-cell>            
+          </sdds-table-body-row>            
+      </sdds-table-body>
+  </sdds-table>`;
+
+export const Basic = BasicTemplate.bind({});
+Basic.args = {};
+
+const MultiselectTemplate = () => `
   <h3>Multiselect</h3>
    <sdds-table id="multiselect-table" enable-multiselect>
           <sdds-table-toolbar table-title="Multiselect" ></sdds-table-toolbar>
@@ -142,11 +67,12 @@ const Template = () => `
           </sdds-table-header>       
           <sdds-table-body enable-dummy-data>                      
           </sdds-table-body>
-  </sdds-table> 
-  
-  <br>
-  <br>
-  
+  </sdds-table>`;
+
+export const Multiselect = MultiselectTemplate.bind({});
+Multiselect.args = {};
+
+const BatchActionTemplate = () => `
   <h3>Batch action</h3>
    <sdds-table id="actionbar-table" enable-multiselect>
           <sdds-table-toolbar table-title="Users buttons - actionbar">          
@@ -183,11 +109,29 @@ const Template = () => `
           </sdds-table-header>       
           <sdds-table-body enable-dummy-data>                      
           </sdds-table-body>
-  </sdds-table> 
-  
-  <br>
-  <br>
-  
+  </sdds-table>`;
+
+export const BatchAction = BatchActionTemplate.bind({});
+BatchAction.args = {};
+
+const FilteringTemplate = () => `
+  <h3>Filtering and sorting example</h3>
+   <sdds-table id="filtering-table">
+          <sdds-table-toolbar table-title="Filter & Sort" enable-filtering></sdds-table-toolbar>
+          <sdds-table-header>
+              <sdds-header-cell column-key='truck' column-title='Truck type'></sdds-header-cell>
+              <sdds-header-cell column-key='driver' column-title='Driver name' sortable></sdds-header-cell>
+              <sdds-header-cell column-key='country' column-title='Country' sortable></sdds-header-cell>
+              <sdds-header-cell column-key='mileage' column-title='Mileage' sortable text-align='right'></sdds-header-cell> 
+          </sdds-table-header>       
+          <sdds-table-body enable-dummy-data>                      
+          </sdds-table-body>
+  </sdds-table>`;
+
+export const Filtering = FilteringTemplate.bind({});
+Filtering.args = {};
+
+const PaginationTemplate = () => `
   <h3>Pagination</h3>
    <sdds-table id="pagination-table">
           <sdds-table-toolbar table-title="Pagination"></sdds-table-toolbar>
@@ -200,18 +144,68 @@ const Template = () => `
           <sdds-table-body enable-dummy-data>                      
           </sdds-table-body>
           <sdds-table-footer enable-pagination rows-per-page="4"></sdds-table-footer>
-  </sdds-table> 
-  
-  <br>
-  <br>  
-  <br>
-  <br>
-  <br>
-  <br>
-  
-  
- 
-  `;
+  </sdds-table>`;
 
-export const WebComponentNEW = Template.bind({});
-WebComponentNEW.args = {};
+export const Pagination = PaginationTemplate.bind({});
+Pagination.args = {};
+
+const ExpandableRowTemplate = () => `
+  <h3>Expandable rows</h3>
+  <sdds-table id="expendable-rows-table" enable-expandable-rows>
+      <sdds-table-header>         
+          <sdds-header-cell column-key='truck' column-title='Truck type'></sdds-header-cell>
+          <sdds-header-cell column-key='driver' column-title='Driver name'></sdds-header-cell>
+          <sdds-header-cell column-key='country' column-title='Country'></sdds-header-cell>
+          <sdds-header-cell column-key='mileage' column-title='Mileage'  text-align='right'></sdds-header-cell> 
+      </sdds-table-header>                 
+      <sdds-table-body>           
+        <sdds-table-body-row-expandable>  
+          <sdds-body-cell cell-value="Test value 1" cell-key="truck"></sdds-body-cell>
+          <sdds-body-cell cell-value="Test value 2" cell-key="driver"></sdds-body-cell>
+          <sdds-body-cell cell-value="Test value 3" cell-key="country"></sdds-body-cell>
+          <sdds-body-cell cell-value="Test value 4" cell-key="mileage"></sdds-body-cell> 
+          <div slot="expand-row">Hello world 1</div>
+        </sdds-table-body-row-expandable>            
+         <sdds-table-body-row-expandable>  
+          <sdds-body-cell cell-value="Test value 5" cell-key="truck"></sdds-body-cell>
+          <sdds-body-cell cell-value="Test value 6" cell-key="driver"></sdds-body-cell>
+          <sdds-body-cell cell-value="Test value 7" cell-key="country"></sdds-body-cell>
+          <sdds-body-cell cell-value="Test value 8" cell-key="mileage"></sdds-body-cell> 
+          <div slot="expand-row">Hello to you too</div>
+        </sdds-table-body-row-expandable>            
+        <sdds-table-body-row-expandable>  
+          <sdds-body-cell cell-value="Test value 9" cell-key="truck"></sdds-body-cell>
+          <sdds-body-cell cell-value="Test value 10" cell-key="driver"></sdds-body-cell>
+          <sdds-body-cell cell-value="Test value 11" cell-key="country"></sdds-body-cell>
+          <sdds-body-cell cell-value="Test value 12" cell-key="mileage"></sdds-body-cell> 
+          <div slot="expand-row"><sdds-button type="primary" text="Call to action"></sdds-button></div>
+        </sdds-table-body-row-expandable>            
+      </sdds-table-body>
+  </sdds-table>`;
+
+export const ExpandableRows = ExpandableRowTemplate.bind({});
+ExpandableRows.args = {};
+
+const EventListenersTemplate = () => `
+  <h3>Disabled filtering, pagination and sorting - left to the user to listen to events</h3>
+   <sdds-table id="disabled-functionality-table">
+          <sdds-table-toolbar table-title="Disabled functionalities table" enable-filtering></sdds-table-toolbar>
+          <sdds-table-header>
+              <sdds-header-cell column-key='truck' column-title='Truck type'></sdds-header-cell>
+              <sdds-header-cell column-key='driver' column-title='Driver name' sortable></sdds-header-cell>
+              <sdds-header-cell column-key='country' column-title='Country' sortable></sdds-header-cell>
+              <sdds-header-cell column-key='mileage' column-title='Mileage' sortable text-align='right'></sdds-header-cell> 
+          </sdds-table-header>       
+          <sdds-table-body disable-pagination-function disable-filtering-function disable-sorting-function enable-dummy-data>  
+              <sdds-table-body-row>
+                  <sdds-body-cell cell-value="Test value 5" cell-key="truck"></sdds-body-cell>
+                  <sdds-body-cell cell-value="Test value 6" cell-key="driver"></sdds-body-cell>
+                  <sdds-body-cell cell-value="Test value 7" cell-key="country"></sdds-body-cell>
+                  <sdds-body-cell cell-value="Test value 8" cell-key="mileage"></sdds-body-cell>           
+              </sdds-table-body-row>       
+          </sdds-table-body>
+        <sdds-table-footer enable-client-pagination client-max-pages="10"></sdds-table-footer>
+  </sdds-table>  `;
+
+export const EventListeners = EventListenersTemplate.bind({});
+EventListeners.args = {};
