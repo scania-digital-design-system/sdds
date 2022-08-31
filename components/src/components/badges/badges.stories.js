307abe1da38a37b663370f61e4f53a3fb32e7315
Emelie Litwin <emelie.litwin@scania.com>
2022-08-31 09:56:16 +0200

    fix(badge): add props for small size and visibility (#429)
    * fix: add new 'no value' story to badges in sb
    
    * fix: init empty badge version
    
    * feat(badge): add props for small size and hidden
    
    * chore: fix spelling in props description
    
    * style: change name on a function
    
    * docs(badge): control changes in storybook

--- components/src/components/badges/badges.stories.js
+++ components/src/components/badges/badges.stories.js
@@ -1,28 +1,37 @@
 export default {
   title: 'Component/Badges',
   argTypes: {
+    visible: {
+      name: 'Visible',
+      description: 'Toggle visibility of badge',
+      control: {
+        type: 'boolean',
+      },
+      defaultValue: true,
+    },
     value: {
       name: 'Value',
       description: 'Set a value to show on the badge',
       control: {
         type: 'number',
       },
+      if: { arg: 'value' || 'visible', truthy: true },
     },
-  },
-  args: {
-    value: 0,
   },
 };
 
-const basicTemplate = ({ value }) => {
+const basicTemplate = ({ value, visible }) => {
   const valueString = value != null ? value.toString() : ''; // convert to string
   return ` 
-      <sdds-badges value=${valueString}>       
+      <sdds-badges value=${valueString} is-visible = ${visible}>       
       </sdds-badges>
     `;
 };
 export const Basic = basicTemplate.bind({});
-Basic.args = {};
+Basic.args = {
+  visible: true,
+  value: 1,
+};
 
 const style = `<style>
                     .demo {
@@ -38,21 +47,52 @@ const style = `<style>
                       top: -5px;
                     }
               </style>`;
-const badgesTemplate = ({ value }) => {
+const badgesTemplate = ({ value, visible }) => {
   const valueString = value != null ? value.toString() : ''; // convert to string
   return `
     ${style}
       <div class="demo">
-      <sdds-badges class="demo-badges" value=${valueString}>       
+      <sdds-badges class="demo-badges" value='${valueString}' is-visible = ${visible}>       
       </sdds-badges> 
       </div>
     `;
 };
+
+const smallBadgeStyle = `<style>
+                    .demo {
+                      margin:5px;
+                      height: 32px;
+                      width: 32px;
+                      position: relative;
+                      background-color: #C4C4C4;
+                    }
+                    .demo-badges{
+                      position: absolute;
+                      left: 26px;
+                      top: -2px;
+                    }
+              </style>`;
+const smallBadgeTemplate = ({ visible }) => {
+  return `
+    ${smallBadgeStyle}
+      <div class="demo">
+      <sdds-badges class="demo-badges" is-small is-visible = ${visible}>       
+      </sdds-badges> 
+      </div>
+    `;
+};
+
 export const Rounded = badgesTemplate.bind({});
 Rounded.args = {
+  visible: true,
   value: 2,
 };
 export const Pill = badgesTemplate.bind({});
 Pill.args = {
+  visible: true,
   value: 100,
+};
+export const Small = smallBadgeTemplate.bind({});
+Small.args = {
+  visible: true,
 };
