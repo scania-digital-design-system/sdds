b9d2d6bd61347158d09acea9507f7b7cccda292f
Emelie Litwin <emelie.litwin@scania.com>
2022-08-31 09:56:16 +0200

    fix(badge): add props for small size and visibility (#429)
    * fix: add new 'no value' story to badges in sb
    
    * fix: init empty badge version
    
    * feat(badge): add props for small size and hidden
    
    * chore: fix spelling in props description
    
    * style: change name on a function
    
    * docs(badge): control changes in storybook

--- components/src/components/badges/badges-component.tsx
+++ components/src/components/badges/badges-component.tsx
@@ -6,7 +6,14 @@ import { Component, h, Prop, Watch, State } from '@stencil/core';
   shadow: true,
 })
 export class SddsBadges {
+  /**Value shown in badge*/
   @Prop() value: string = '';
+
+  /**Changes visibility of badge*/
+  @Prop() isVisible: boolean = true;
+
+  /**Changes badge from default to small size*/
+  @Prop() isSmall: boolean = false;
 
   @State() shape: string = '';
 
@@ -14,35 +21,69 @@ export class SddsBadges {
 
   @State() text: string = '';
 
+  @State() smallVariation: boolean = false;
+
+  @State() defaultVariation: boolean = true;
+
   @Watch('value')
-  watchValue() {
-    // parse the value to number
-    const valueAsNumber = parseInt(this.value);
-    if (!isNaN(valueAsNumber)) {
-      this.shape = valueAsNumber.toString().length >= 2 ? 'pill' : '';
-      this.size = '';
-      this.text =
-        valueAsNumber.toString().length >= 3 ? '99+' : valueAsNumber.toString();
-    } else {
-      this.value !== ''
-        ? console.warn(
-            'The provided value is either empty or string, please provide value as number.'
-          )
-        : '';
-    }
+  @Watch('isVisible')
+  @Watch('isSmall')
+  watchProps() {
+    this.checkProps();
+  }
+  componentWillLoad() {
+    this.checkProps();
   }
 
-  componentWillLoad() {
-    this.watchValue();
+  checkProps() {
+    const valueAsNumber = parseInt(this.value);
+    if (!isNaN(valueAsNumber) && !this.isSmall) {
+      this.shape = valueAsNumber.toString().length >= 2 ? 'pill' : '';
+
+      if (!isNaN(valueAsNumber) && !this.isSmall) {
+        this.shape = this.value.toString().length >= 2 ? 'pill' : '';
+        this.size = '';
+        this.text =
+          valueAsNumber.toString().length >= 3
+            ? '99+'
+            : valueAsNumber.toString();
+      } else {
+        this.value !== ''
+          ? console.warn(
+              'The provided value is either empty or string, please provide value as number.'
+            )
+          : '';
+      }
+    }
+    if (this.isVisible) {
+      if (!this.isSmall) {
+        this.defaultVariation = true;
+        this.smallVariation = false;
+      }
+      if (this.isSmall) {
+        this.defaultVariation = false;
+        this.smallVariation = true;
+      }
+    } else {
+      this.defaultVariation = false;
+      this.smallVariation = false;
+    }
   }
 
   render() {
     return (
-      <div
-        class={`sdds-badges sdds-badges-${this.size} sdds-badges-${this.shape}`}
-      >
-        <div class="sdds-badges-text">{this.text}</div>
-      </div>
+      <host>
+        {this.defaultVariation && (
+          <div
+            class={`sdds-badges sdds-badges-${this.size} sdds-badges-${this.shape}`}
+          >
+            <div class="sdds-badges-text">{this.text}</div>
+          </div>
+        )}
+        {this.smallVariation && (
+          <div class={`sdds-badges sdds-badges-sm`}></div>
+        )}
+      </host>
     );
   }
 }
