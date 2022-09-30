208500f5ccf791b624dbcf22b75835c3afe02d3e
Emelie Litwin <emelie.litwin@scania.com>
2022-09-30 11:57:08 +0200

    refactor(spinner): test putting all values in scss

--- components/src/components/spinner/spinner.tsx
+++ components/src/components/spinner/spinner.tsx
@@ -3,7 +3,7 @@ import { Component, h, Prop, State } from '@stencil/core';
 @Component({
   tag: 'sdds-spinner',
   styleUrl: 'spinner.scss',
-  shadow: true,
+  shadow: false,
 })
 export class Spinner {
   /** Size of spinner. Accepted strings are: xs,sm, md, lg.*/
@@ -12,47 +12,44 @@ export class Spinner {
   /** Mode of spinner. Accepted strings are: standard, inverted*/
   @Prop() mode: 'standard' | 'inverted' = 'standard';
 
-  @State() width: number = 84;
+  /** ID of spinner. If no ID is passed it will be automatically generated */
+  @Prop() spinnerID: string = Math.random().toString(36).substring(2, 7);
+
+  @State() radius: number = 42;
 
   @State() strokeWidth: number = 8;
 
-  componentWillLoad() {
-    switch (this.size) {
-      case 'xs':
-        this.width = 16;
-        this.strokeWidth = 3;
-        break;
-      case 'sm':
-        this.width = 24;
-        this.strokeWidth = 4;
-        break;
-      case 'md':
-        this.width = 52;
-        this.strokeWidth = 6;
-        break;
-      case 'lg':
-        this.width = 84;
-        this.strokeWidth = 8;
-        break;
-      default:
-        break;
-    }
+  componentDidLoad() {
+    const circle = document.getElementById(
+      `sdds-spinner-circle-${this.spinnerID}`
+    );
+    const circleStyle = getComputedStyle(circle);
+    this.radius = parseInt(
+      circleStyle.getPropertyValue('--sdds-spinner-radius')
+    );
+    this.strokeWidth = parseInt(
+      circleStyle.getPropertyValue('--sdds-spinner-stroke-width')
+    );
+    console.log(this.spinnerID);
   }
 
   render() {
     return (
-      <host>
-        <svg class="sdds-spinner-svg" height={this.width} width={this.width}>
-          <circle
-            class={`sdds-spinner-circle sdds-spinner-circle-${this.size} sdds-spinner-circle-${this.mode}`}
-            cx={this.width / 2}
-            cy={this.width / 2}
-            r={this.width / 2 - this.strokeWidth / 2}
-            stroke-width={this.strokeWidth}
-            fill="none"
-          />
-        </svg>
-      </host>
+      <svg
+        class="sdds-spinner-svg"
+        height={this.radius * 2}
+        width={this.radius * 2}
+      >
+        <circle
+          id={`sdds-spinner-circle-${this.spinnerID}`}
+          class={`sdds-spinner-circle sdds-spinner-circle-${this.size} sdds-spinner-circle-${this.mode}`}
+          cx={this.radius}
+          cy={this.radius}
+          r={this.radius - this.strokeWidth / 2}
+          stroke-width={this.strokeWidth}
+          fill="none"
+        />
+      </svg>
     );
   }
 }
