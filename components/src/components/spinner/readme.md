243141a45c7902d7f5b1eeae83db0e18d82e2505
Emelie Litwin <emelie.litwin@scania.com>
2022-09-30 14:13:07 +0200

    refactor(spinner): move circle style to css

--- components/src/components/spinner/readme.md
+++ components/src/components/spinner/readme.md
@@ -5,11 +5,10 @@
 
 ## Properties
 
-| Property    | Attribute     | Description                                                          | Type                           | Default                                      |
-| ----------- | ------------- | -------------------------------------------------------------------- | ------------------------------ | -------------------------------------------- |
-| `mode`      | `mode`        | Mode of spinner. Accepted strings are: standard, inverted            | `"inverted" \| "standard"`     | `'standard'`                                 |
-| `size`      | `size`        | Size of spinner. Accepted strings are: xs,sm, md, lg.                | `"lg" \| "md" \| "sm" \| "xs"` | `'lg'`                                       |
-| `spinnerID` | `spinner-i-d` | ID of spinner. If no ID is passed it will be automatically generated | `string`                       | `Math.random().toString(36).substring(2, 7)` |
+| Property | Attribute | Description                                               | Type                           | Default      |
+| -------- | --------- | --------------------------------------------------------- | ------------------------------ | ------------ |
+| `mode`   | `mode`    | Mode of spinner. Accepted strings are: standard, inverted | `"inverted" \| "standard"`     | `'standard'` |
+| `size`   | `size`    | Size of spinner. Accepted strings are: xs,sm, md, lg.     | `"lg" \| "md" \| "sm" \| "xs"` | `'lg'`       |
 
 
 ----------------------------------------------
--- components/src/components/spinner/spinner.tsx
+++ components/src/components/spinner/spinner.tsx
@@ -1,4 +1,4 @@
-import { Component, h, Prop, State } from '@stencil/core';
+import { Component, h, Prop } from '@stencil/core';
 
 @Component({
   tag: 'sdds-spinner',
@@ -12,42 +12,11 @@ export class Spinner {
   /** Mode of spinner. Accepted strings are: standard, inverted*/
   @Prop() mode: 'standard' | 'inverted' = 'standard';
 
-  /** ID of spinner. If no ID is passed it will be automatically generated */
-  @Prop() spinnerID: string = Math.random().toString(36).substring(2, 7);
-
-  @State() radius: number = 42;
-
-  @State() strokeWidth: number = 8;
-
-  componentDidLoad() {
-    const circle = document.getElementById(
-      `sdds-spinner-circle-${this.spinnerID}`
-    );
-    const circleStyle = getComputedStyle(circle);
-    this.radius = parseInt(
-      circleStyle.getPropertyValue('--sdds-spinner-radius')
-    );
-    this.strokeWidth = parseInt(
-      circleStyle.getPropertyValue('--sdds-spinner-stroke-width')
-    );
-    console.log(this.spinnerID);
-  }
-
   render() {
     return (
-      <svg
-        class="sdds-spinner-svg"
-        height={this.radius * 2}
-        width={this.radius * 2}
-      >
+      <svg class={`sdds-spinner-svg sdds-spinner-svg-${this.size}`}>
         <circle
-          id={`sdds-spinner-circle-${this.spinnerID}`}
-          class={`sdds-spinner-circle sdds-spinner-circle-${this.size} sdds-spinner-circle-${this.mode}`}
-          cx={this.radius}
-          cy={this.radius}
-          r={this.radius - this.strokeWidth / 2}
-          stroke-width={this.strokeWidth}
-          fill="none"
+          class={`sdds-spinner-circle sdds-spinner-circle-${this.mode}`}
         />
       </svg>
     );
