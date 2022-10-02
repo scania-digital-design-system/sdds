95a166037d32daf103ceb19099da570545bcc8e0
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2022-10-02 13:49:23 +0200

    fix: add stencil readme under "Notes" in Storybook

--- components/.storybook/main.js
+++ components/.storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', '@storybook/addon-notes/register'],
   framework: '@storybook/html',
 };
