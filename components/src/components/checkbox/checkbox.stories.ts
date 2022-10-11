3c95acd54cf96fc47b51c94b72291097d92c36b5
mJarsater <martin.jarsater@hotmail.com>
2022-10-11 16:49:44 +0200

    feat(checkbox): migrated checkbox component

---
+++ components/src/components/checkbox/checkbox.stories.ts
@@ -0,0 +1,36 @@
+export default {
+  title: 'Components/Checkbox/Native',
+};
+
+const Template = () => `
+      <div class="sdds-theme-dark">
+        <div class="sdds-checkbox-item">
+          <input class="sdds-form-input" type="checkbox" name="cb-example" id="cb-option-1">
+          <label class="sdds-form-label" for="cb-option-1">
+            Label Text 1
+          </label>
+        </div>
+        <div class="sdds-checkbox-item">
+          <input class="sdds-form-input" type="checkbox" name="cb-example" id="cb-option-2">
+          <label class="sdds-form-label" for="cb-option-2">
+            Label Text 2
+          </label>
+        </div>
+        <div class="sdds-checkbox-item">
+          <input class="sdds-form-input" type="checkbox" name="cb-example" id="cb-option-3" checked="checked" disabled>
+          <label class="sdds-form-label" for="cb-option-3">
+            Label Text 3
+          </label>
+        </div>
+        <div class="sdds-checkbox-item">
+          <input class="sdds-form-input" type="checkbox" name="cb-example" id="cb-option-4" disabled>
+          <label class="sdds-form-label" for="cb-option-4">
+            Label Text 4
+          </label>
+        </div>
+      </div>
+      `;
+
+export const Default = Template.bind({});
+
+Default.args = {};
