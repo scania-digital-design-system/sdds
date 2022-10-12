9705e4f9c8c081b20f1da3df7b24712d69105319
mJarsater <martin.jarsater@hotmail.com>
2022-10-12 14:51:00 +0200

    feat(checkbox): added more stories

--- components/src/components/checkbox/checkbox.stories.ts
+++ components/src/components/checkbox/checkbox.stories.ts
@@ -3,37 +3,52 @@ export default {
   parameters: {
     layout: 'centered',
   },
+  argTypes: {
+    disabled: {
+      name: 'Disabled',
+      description: 'Disables the checkbox',
+      defaultValue: false,
+      control: {
+        type: 'boolean',
+      },
+    },
+    checked: {
+      name: 'Checked',
+      description: 'Checks the checkbox',
+      defaultValue: false,
+      control: {
+        type: 'boolean',
+      },
+    },
+    label: {
+      name: 'Label',
+      description: 'The label of the component',
+      defaultValue: 'Label',
+      type: 'string',
+    },
+  },
 };
 
-const Template = () => `
-      <div>
+const Template = args => `
         <div class="sdds-checkbox-item">
-          <input class="sdds-form-input" type="checkbox" name="cb-example" id="cb-option-1">
-          <label class="sdds-form-label" for="cb-option-1">
-            Label Text 1
-          </label>
+          ${args.label && `<label class="sdds-form-label" for="cb-option-2">  ${args.label} </label>`}
+          <input class="sdds-form-input" type="checkbox" ${args.checked && `checked="checked"`}  ${args.disabled && 'disabled'}>
         </div>
-        <div class="sdds-checkbox-item">
-          <input class="sdds-form-input" type="checkbox" name="cb-example" id="cb-option-2">
-          <label class="sdds-form-label" for="cb-option-2">
-            Label Text 2
-          </label>
-        </div>
-        <div class="sdds-checkbox-item">
-          <input class="sdds-form-input" type="checkbox" name="cb-example" id="cb-option-3" checked="checked" disabled>
-          <label class="sdds-form-label" for="cb-option-3">
-            Label Text 3
-          </label>
-        </div>
-        <div class="sdds-checkbox-item">
-          <input class="sdds-form-input" type="checkbox" name="cb-example" id="cb-option-4" disabled>
-          <label class="sdds-form-label" for="cb-option-4">
-            Label Text 4
-          </label>
-        </div>
-      </div>
       `;
 
 export const Default = Template.bind({});
-
 Default.args = {};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  disabled: true,
+};
+export const Checked = Template.bind({});
+Checked.args = {
+  checked: true,
+};
+
+export const WithLabel = Template.bind({});
+WithLabel.args = {
+  label: 'Label',
+};
