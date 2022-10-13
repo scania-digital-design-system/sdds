6729a1d61fa9243c2b5c9f853e28fa0923662258
mJarsater <martin.jarsater@hotmail.com>
2022-10-13 09:01:37 +0200

    feat(radio-button): migrated the radio-button component

---
+++ components/src/components/radio-button/radio-button.stories.js
@@ -0,0 +1,70 @@
+export default {
+  title: 'Components/Native/Radio Button',
+  parameters: {
+    layout: 'centered',
+  },
+  argTypes: {
+    label: {
+      name: 'Label',
+      description: 'The label for the radio button',
+      defaultValue: 'Label text',
+      controls: {
+        type: 'string',
+      },
+      type: { summary: 'string' },
+    },
+    disabled: {
+      name: 'Disabled',
+      description: 'Disables the radio button',
+      default: false,
+      controls: {
+        type: 'boolean',
+      },
+    },
+    checked: {
+      name: 'Checked',
+      description: 'Marks the radio button as checked',
+      default: false,
+      controls: {
+        type: 'boolean',
+      },
+    },
+  },
+};
+
+const Template = args => `
+    <div class="sdds-radio-button-group">
+      <div class="sdds-radio-item">
+        <input class="sdds-form-input" type="radio" name="rb-example" id="rb-option-1" ${args.disabled && 'disabled'}>
+        <label class="sdds-form-label" for="rb-option-1">
+          ${args.label} 1
+        </label>
+      </div>
+    </div>
+    <div class="sdds-radio-button-group">
+      <div class="sdds-radio-item">
+        <input class="sdds-form-input" type="radio" name="rb-example" id="rb-option-1" ${args.checked && 'checked="checked"'} >
+        <label class="sdds-form-label" for="rb-option-1">
+          ${args.label} 2
+        </label>
+      </div>
+    </div>
+    `;
+
+export const Default = Template.bind({});
+Default.args = {
+  label: 'Label Text',
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  label: 'Label Text',
+  disabled: true,
+};
+
+export const Checked = Template.bind({});
+Checked.args = {
+  label: 'Label Text',
+  disabled: false,
+  checked: true,
+};
