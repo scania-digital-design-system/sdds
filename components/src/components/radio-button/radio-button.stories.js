0e316bbc8fc90fdfb44551748f08e57e2c5e5560
mJarsater <martin.jarsater@hotmail.com>
2022-10-13 10:45:12 +0200

    fix(radio-button): added story to hightlight the disabled state

--- components/src/components/radio-button/radio-button.stories.js
+++ components/src/components/radio-button/radio-button.stories.js
@@ -49,17 +49,37 @@ const Template = args => `
       </label>
     </div>
   </div>
-    `;
+`;
+
+const DisabledTemplate = args => `
+    <div class="sdds-radio-button-group">
+    <div class="sdds-radio-item">
+      <input class="sdds-form-input" type="radio" name="rb-example" id="rb-option-1" ${args.disabled && 'disabled'}>
+      <label class="sdds-form-label" for="rb-option-1">
+        ${args.label} 1
+      </label>
+    </div>
+  </div>
+  <div class="sdds-radio-button-group">
+    <div class="sdds-radio-item">
+      <input class="sdds-form-input" type="radio" name="rb-example" id="rb-option-1" ${args.checked && 'checked="checked"'} ${args.disabled && 'disabled'} >
+      <label class="sdds-form-label" for="rb-option-1">
+        ${args.label} 2
+      </label>
+    </div>
+  </div>
+`;
 
 export const Default = Template.bind({});
 Default.args = {
   label: 'Label Text',
 };
 
-export const Disabled = Template.bind({});
+export const Disabled = DisabledTemplate.bind({});
 Disabled.args = {
   label: 'Label Text',
   disabled: true,
+  checked: true,
 };
 
 export const Checked = Template.bind({});
