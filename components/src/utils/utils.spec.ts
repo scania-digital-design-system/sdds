554c1be9208276278493984d7a155e11edf81c6f
Alexander Eneroth <alexandereneroth@users.noreply.github.com>
2022-10-14 11:49:18 +0200

    docs: add util to format story html (#507)

--- components/src/utils/utils.spec.ts
+++ components/src/utils/utils.spec.ts
@@ -1,19 +1,19 @@
-import { format } from './utils';
+import { demoFormat } from './utils';
 
 describe('format', () => {
   it('returns empty string for no names defined', () => {
-    expect(format(undefined, undefined, undefined)).toEqual('');
+    expect(demoFormat(undefined, undefined, undefined)).toEqual('');
   });
 
   it('formats just first names', () => {
-    expect(format('Joseph', undefined, undefined)).toEqual('Joseph');
+    expect(demoFormat('Joseph', undefined, undefined)).toEqual('Joseph');
   });
 
   it('formats first and last names', () => {
-    expect(format('Joseph', undefined, 'Publique')).toEqual('Joseph Publique');
+    expect(demoFormat('Joseph', undefined, 'Publique')).toEqual('Joseph Publique');
   });
 
   it('formats first, middle and last names', () => {
-    expect(format('Joseph', 'Quincy', 'Publique')).toEqual('Joseph Quincy Publique');
+    expect(demoFormat('Joseph', 'Quincy', 'Publique')).toEqual('Joseph Quincy Publique');
   });
 });
