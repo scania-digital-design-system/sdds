392170efbe963f97f1c3149684fa9e60875d9058
mJarsater <martin.jarsater@hotmail.com>
2022-10-19 16:56:43 +0200

    fix(radio button): cleaned up story

--- components/src/components/radio-button/radio-button.stories.js
+++ components/src/components/radio-button/radio-button.stories.js
@@ -37,7 +37,7 @@ const Template = args =>
   formatHtmlPreview(`
    <div class="sdds-radio-button-group">
    <div class="sdds-radio-item">
-     <input class="sdds-form-input" type="radio" name="rb-example" id="rb-option-1" ${args.disabled ? 'disabled' : ''}>
+     <input class="sdds-form-input" type="radio" name="rb-example" id="rb-option-1" checked="" ${args.disabled ? 'disabled' : ''}>
      <label class="sdds-form-label" for="rb-option-1">
        ${args.label} 1
      </label>
@@ -54,42 +54,7 @@ const Template = args =>
    </div>
 `);
 
-const DisabledTemplate = args =>
-  formatHtmlPreview(`
-
-    <div class="sdds-radio-button-group">
-    <div class="sdds-radio-item">
-      <input class="sdds-form-input" type="radio" name="rb-example" id="rb-option-1" ${args.disabled ? 'disabled' : ''}>
-      <label class="sdds-form-label" for="rb-option-1">
-        ${args.label} 1
-      </label>
-    </div>
-  </div>
-  <div class="sdds-radio-button-group">
-    <div class="sdds-radio-item">
-      <input class="sdds-form-input" type="radio" name="rb-example" id="rb-option-1" ${args.checked ? 'checked="checked"' : ''} ${args.disabled ? 'disabled' : ''} >
-      <label class="sdds-form-label" for="rb-option-1">
-        ${args.label} 2
-      </label>
-    </div>
-    </div>
-`);
-
 export const Default = Template.bind({});
 Default.args = {
   label: 'Label Text',
-};
-
-export const Disabled = DisabledTemplate.bind({});
-Disabled.args = {
-  label: 'Label Text',
-  disabled: true,
-  checked: true,
-};
-
-export const Checked = Template.bind({});
-Checked.args = {
-  label: 'Label Text',
-  disabled: false,
-  checked: true,
 };
