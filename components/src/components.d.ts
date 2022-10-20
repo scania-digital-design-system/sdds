32e3f1ca802cab8fdbaea6a25c599ce984e76cd0
Alexander Eneroth <alexandereneroth@users.noreply.github.com>
2022-10-20 16:25:44 +0200

    feat(accordion): migrate accordion component (#514)

--- components/src/components.d.ts
+++ components/src/components.d.ts
@@ -20,6 +20,34 @@ export namespace Components {
          */
         "middle": string;
     }
+    interface SddsAccordion {
+        /**
+          * Enable or disable divider lines between items
+         */
+        "divider": boolean;
+    }
+    interface SddsAccordionItem {
+        /**
+          * Icon can be placed after or before accordion header. Values accepted: `prefix` or `suffix` Default value is `suffix`
+         */
+        "affix": 'prefix' | 'suffix';
+        /**
+          * Disabled option in `boolean`.
+         */
+        "disabled": boolean;
+        /**
+          * Set to true to expand panel open
+         */
+        "expanded": boolean;
+        /**
+          * The header gives users the context about the additional information available inside the panel
+         */
+        "header": string;
+        /**
+          * When true 16px on right padding instead of 64px
+         */
+        "paddingReset": boolean;
+    }
     interface SddsBadges {
         /**
           * !!Deprecated!! Use size prop instead. Changes badge from default to small size
@@ -39,12 +67,28 @@ export namespace Components {
         "value": string;
     }
 }
+export interface SddsAccordionItemCustomEvent<T> extends CustomEvent<T> {
+    detail: T;
+    target: HTMLSddsAccordionItemElement;
+}
 declare global {
     interface HTMLMyComponentElement extends Components.MyComponent, HTMLStencilElement {
     }
     var HTMLMyComponentElement: {
         prototype: HTMLMyComponentElement;
         new (): HTMLMyComponentElement;
+    };
+    interface HTMLSddsAccordionElement extends Components.SddsAccordion, HTMLStencilElement {
+    }
+    var HTMLSddsAccordionElement: {
+        prototype: HTMLSddsAccordionElement;
+        new (): HTMLSddsAccordionElement;
+    };
+    interface HTMLSddsAccordionItemElement extends Components.SddsAccordionItem, HTMLStencilElement {
+    }
+    var HTMLSddsAccordionItemElement: {
+        prototype: HTMLSddsAccordionItemElement;
+        new (): HTMLSddsAccordionItemElement;
     };
     interface HTMLSddsBadgesElement extends Components.SddsBadges, HTMLStencilElement {
     }
@@ -54,6 +98,8 @@ declare global {
     };
     interface HTMLElementTagNameMap {
         "my-component": HTMLMyComponentElement;
+        "sdds-accordion": HTMLSddsAccordionElement;
+        "sdds-accordion-item": HTMLSddsAccordionItemElement;
         "sdds-badges": HTMLSddsBadgesElement;
     }
 }
@@ -71,6 +117,38 @@ declare namespace LocalJSX {
           * The middle name
          */
         "middle"?: string;
+    }
+    interface SddsAccordion {
+        /**
+          * Enable or disable divider lines between items
+         */
+        "divider"?: boolean;
+    }
+    interface SddsAccordionItem {
+        /**
+          * Icon can be placed after or before accordion header. Values accepted: `prefix` or `suffix` Default value is `suffix`
+         */
+        "affix"?: 'prefix' | 'suffix';
+        /**
+          * Disabled option in `boolean`.
+         */
+        "disabled"?: boolean;
+        /**
+          * Set to true to expand panel open
+         */
+        "expanded"?: boolean;
+        /**
+          * The header gives users the context about the additional information available inside the panel
+         */
+        "header"?: string;
+        /**
+          * Fires after the accordion item is closed or opened, emitting the value (as boolean) of the current state of the accordion
+         */
+        "onAccordionItemToggle"?: (event: SddsAccordionItemCustomEvent<boolean>) => void;
+        /**
+          * When true 16px on right padding instead of 64px
+         */
+        "paddingReset"?: boolean;
     }
     interface SddsBadges {
         /**
@@ -92,6 +170,8 @@ declare namespace LocalJSX {
     }
     interface IntrinsicElements {
         "my-component": MyComponent;
+        "sdds-accordion": SddsAccordion;
+        "sdds-accordion-item": SddsAccordionItem;
         "sdds-badges": SddsBadges;
     }
 }
@@ -100,6 +180,8 @@ declare module "@stencil/core" {
     export namespace JSX {
         interface IntrinsicElements {
             "my-component": LocalJSX.MyComponent & JSXBase.HTMLAttributes<HTMLMyComponentElement>;
+            "sdds-accordion": LocalJSX.SddsAccordion & JSXBase.HTMLAttributes<HTMLSddsAccordionElement>;
+            "sdds-accordion-item": LocalJSX.SddsAccordionItem & JSXBase.HTMLAttributes<HTMLSddsAccordionItemElement>;
             "sdds-badges": LocalJSX.SddsBadges & JSXBase.HTMLAttributes<HTMLSddsBadgesElement>;
         }
     }
