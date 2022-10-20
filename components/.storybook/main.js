a569b425644e37f1f7ad7cf58070f97ceda7bc62
Alexander Eneroth <alexandereneroth@users.noreply.github.com>
2022-10-20 11:12:38 +0200

    chore: add darkmode toggle to storybook (#494)

--- components/.storybook/main.js
+++ components/.storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', '@storybook/addon-notes/register'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', '@storybook/addon-notes/register', 'storybook-dark-mode'],
   framework: '@storybook/html',
 };
