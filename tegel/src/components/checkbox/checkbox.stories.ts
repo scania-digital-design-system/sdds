7afbcff0fde95a6477fbd004d1f59ca169a50c4a
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2022-10-24 08:42:34 +0200

    fix: checkbox story cleanup + eslint rule for import resolution (#527)

--- tegel/src/components/checkbox/checkbox.stories.ts
+++ tegel/src/components/checkbox/checkbox.stories.ts
@@ -23,7 +23,7 @@ export default {
       },
     },
     label: {
-      name: 'Label',
+      name: 'Label text',
       description: 'The label of the component',
       defaultValue: 'Label',
       type: 'string',
@@ -37,27 +37,15 @@ export default {
   },
 };
 
-const Template = args => {
-  return formatHtmlPreview(`
+const Template = args =>
+  formatHtmlPreview(`
     <div class="sdds-checkbox-item">
       <input class="sdds-form-input" type="checkbox" id="${args.id}" ${args.checked ? 'checked="checked"' : ''}  ${args.disabled ? 'disabled' : ''}>
       ${args.label ? `<label class="sdds-form-label" for="${args.id}" ${args.disabled && 'disabled'}>  ${args.label} </label>` : ''}
     </div>
   `);
-};
 
 export const Default = Template.bind({});
 Default.args = {
   id: 'example-default',
-};
-
-export const Disabled = Template.bind({});
-Disabled.args = {
-  id: 'example-disabled',
-  disabled: true,
-};
-export const Checked = Template.bind({});
-Checked.args = {
-  id: 'example-checked',
-  checked: true,
 };
