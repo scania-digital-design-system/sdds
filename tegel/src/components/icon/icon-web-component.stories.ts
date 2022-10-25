feececf2a48e4d97b0b2281eafaf8e98d3ba8cd9
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2022-10-25 12:10:43 +0200

    fix(icon): separation of font and component stories

---
+++ tegel/src/components/icon/icon-web-component.stories.ts
@@ -0,0 +1,41 @@
+import { formatHtmlPreview } from '../../utils/utils';
+import { iconsNames } from './iconsArray';
+import readme from './readme.md';
+
+export default {
+  title: 'Foundations/Icons',
+  parameters: {
+    layout: 'centered',
+    notes: readme,
+  },
+  argTypes: {
+    icon: {
+      name: 'Icon name',
+      control: {
+        type: 'select',
+        options: iconsNames,
+      },
+    },
+    size: {
+      name: 'Size in pixels',
+      control: {
+        type: 'range',
+        min: 16,
+        max: 100,
+        step: 4,
+      },
+    },
+  },
+};
+
+const IconTemplate = args =>
+  formatHtmlPreview(`
+  <sdds-icon name="${args.icon}" size="${`${args.size.toString()}px`}" />
+  `);
+
+export const ComponentWay = IconTemplate.bind({});
+
+ComponentWay.args = {
+  size: 32,
+  icon: 'truck',
+};
