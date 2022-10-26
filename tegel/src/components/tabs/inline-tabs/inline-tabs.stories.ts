c5787bdf8d2bb2654fc120ba5a50555df70b9dde
Alexander Eneroth <alexandereneroth@users.noreply.github.com>
2022-10-26 17:32:08 +0200

    fix(tabs): prepare inline-tabs for theming, add missing variant prop, update story

--- tegel/src/components/tabs/inline-tabs/inline-tabs.stories.ts
+++ tegel/src/components/tabs/inline-tabs/inline-tabs.stories.ts
@@ -1,8 +1,12 @@
 import { formatHtmlPreview } from '../../../utils/utils';
+import readme from './readme.md';
 
 export default {
   title: 'Components/Tabs/Inline Tabs',
-  parameters: {},
+  parameters: {
+    layout: 'fullscreen',
+    notes: readme,
+  },
   argTypes: {
     autoHeight: {
       name: 'Same height',
@@ -12,33 +16,54 @@ export default {
         type: 'boolean',
       },
     },
+    altBgColor: {
+      name: 'Alternative background color',
+      description: 'Style for display on the alternative background color',
+      defaultValue: false,
+      control: {
+        type: 'boolean',
+      },
+    },
   },
 };
 
-const Template = ({ autoHeight = false }) =>
-  formatHtmlPreview(`
+// eslint-disable-next-line arrow-body-style
+const Template = ({ autoHeight = false, altBgColor = false }) => {
+  // TODO: change background color of stories globally with dark theme plugin instead
+  return formatHtmlPreview(`
   <style>
     /* .demo-wrapper is only added here for demonstration purposes. */
     .demo-wrapper {
+      padding: 30px 20px;
       font-size: 14px;
+      background-color: ${altBgColor ? getComputedStyle(document.documentElement).getPropertyValue('--sdds-grey-50') : 'white'};
+    }
+    .sdds-theme-dark .demo-wrapper {
+      color: white;
+      background-color: ${
+        altBgColor ? getComputedStyle(document.documentElement).getPropertyValue('--sdds-grey-958') : getComputedStyle(document.documentElement).getPropertyValue('--sdds-grey-900')
+      };
     }
   </style>
 
   <div class="demo-wrapper">
 
-    <sdds-inline-tabs ${autoHeight ? 'auto-height' : ''}>
-      <div name="Tab very long name">
+    <sdds-inline-tabs ${autoHeight ? 'auto-height' : ''} ${altBgColor ? 'color-variant="on-grey"' : ''}>
+      <div name="Tab with tall content">
         Tab panel 1
         <div style="width:200px; height:200px; background: linear-gradient(125deg,rgba(255, 0, 0, 1) 0%,rgba(255, 255, 0, 1) 33%,rgba(0, 192, 255, 1) 66%,rgba(192, 0, 255, 1) 100%);"></div>
       </div>
-
-      <div name="Tab 2" default>
+      <div name="Default tab" default>
         Tab panel 2
+      </div>
+      <div name="Disabled tab" disabled>
+        Tab panel 3
       </div>
 
     </sdds-inline-tabs>
   </div>
 `);
+};
 
 export const Default = Template.bind({});
 Default.args = {};
--- tegel/src/components/tabs/inline-tabs/readme.md
+++ tegel/src/components/tabs/inline-tabs/readme.md
@@ -7,10 +7,11 @@
 
 ## Properties
 
-| Property     | Attribute     | Description                                                                                       | Type      | Default |
-| ------------ | ------------- | ------------------------------------------------------------------------------------------------- | --------- | ------- |
-| `autoHeight` | `auto-height` | different height settings. right now only supports "auto", otherwise ignored                      | `boolean` | `false` |
-| `defaultTab` | `default-tab` | either use this (default-tab="...") or read attribute "default" from one of the slotted children. | `string`  | `''`    |
+| Property       | Attribute       | Description                                                                                       | Type        | Default |
+| -------------- | --------------- | ------------------------------------------------------------------------------------------------- | ----------- | ------- |
+| `autoHeight`   | `auto-height`   | different height settings. right now only supports "auto", otherwise ignored                      | `boolean`   | `false` |
+| `colorVariant` | `color-variant` | color variant. right now only supports "on-grey", otherwise ignored                               | `"on-grey"` | `null`  |
+| `defaultTab`   | `default-tab`   | either use this (default-tab="...") or read attribute "default" from one of the slotted children. | `string`    | `''`    |
 
 
 ## Methods
