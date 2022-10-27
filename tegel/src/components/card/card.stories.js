d17a0c31d8ef003432b793fabfe09d4215788d15
mJarsater <martin.jarsater@hotmail.com>
2022-10-27 10:51:47 +0200

    fix(card): clean up of story

--- tegel/src/components/card/card.stories.js
+++ tegel/src/components/card/card.stories.js
@@ -23,10 +23,28 @@ export default {
         type: 'text',
       },
     },
+    text: {
+      name: 'Body text',
+      description: 'The body text for the card',
+      defaultValue: '',
+      control: {
+        type: 'text',
+      },
+    },
+    divider: {
+      name: 'Divider',
+      description: 'Add a divider to the card',
+      defaultValue: false,
+      control: {
+        type: 'boolean',
+      },
+      table: {
+        defaultValue: { summary: false },
+      },
+    },
     footer: {
       name: 'Footer',
       description: 'The footer of the card',
-      // TODO - Is dependant on font component
       defaultValue: '<sdds-icon style="font-size: 20px;" name="scania-arrow"></sdds-icon>',
       control: {
         type: 'text',
@@ -35,15 +53,29 @@ export default {
     clickable: {
       name: 'Clickable',
       description: 'Toggles if the card is clickable or not.',
-      defaultValue: false,
       control: {
         type: 'boolean',
+      },
+      defaultValue: false,
+      table: {
+        defaultValue: { summary: false },
+      },
+    },
+    imageTop: {
+      name: 'Image on top',
+      description: 'Sets the image on top',
+      control: {
+        type: 'boolean',
+      },
+      defaultValue: false,
+      table: {
+        defaultValue: { summary: false },
       },
     },
   },
 };
 
-const CardTemplate = ({ headline, subheadline, footer, clickable, text, divider, imageTop }) =>
+const Template = ({ headline, subheadline, footer, clickable, text, divider, imageTop }) =>
   formatHtmlPreview(
     `
 <style> 
@@ -67,24 +99,24 @@ const CardTemplate = ({ headline, subheadline, footer, clickable, text, divider,
   `,
   );
 
-export const Default = CardTemplate.bind({});
+export const Default = Template.bind({});
 
 Default.args = {};
 
-export const SupportText = CardTemplate.bind({});
+export const SupportText = Template.bind({});
 
 SupportText.args = {
   text: 'This is a short and consist detail text describing for the user what this text is really about.',
 };
 
-export const Divider = CardTemplate.bind({});
+export const Divider = Template.bind({});
 
 Divider.args = {
   divider: true,
   text: 'This is a short and consist detail text describing for the user what this text is really about.',
 };
 
-export const link = CardTemplate.bind({});
+export const link = Template.bind({});
 
 link.args = {
   divider: true,
@@ -92,7 +124,7 @@ link.args = {
   footer: '<a class="sdds-link sdds-link--no-underline" href="#">Link text</a><a class="sdds-link sdds-link--no-underline" href="#">Link text</a>',
 };
 
-export const button = CardTemplate.bind({});
+export const button = Template.bind({});
 
 button.args = {
   divider: true,
@@ -100,7 +132,7 @@ button.args = {
   footer: '<button class="sdds-btn sdds-btn-sm sdds-btn-primary">Button text</button>',
 };
 
-export const Image = CardTemplate.bind({});
+export const Image = Template.bind({});
 
 Image.args = {
   divider: true,
@@ -120,7 +152,14 @@ const AvatarTemplate = ({ headline, subheadline, footer, clickable, text, divide
       <div class="sdds-card ${clickable ? 'sdds-clickable' : ''}">
       ${imageTop == true ? `<img class="sdds-card-img" src="${CardImage}" />` : ''}
       <div class="sdds-card-header-avatar">
-        ${avatar ? `<div class="sdds-card-avatar">${avatar}</div>` : ''}
+        ${
+          avatar
+            ? `
+          <div class="sdds-card-avatar">
+          <svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="18" cy="18" r="18" fill="#E2E2E4"/></svg>
+          </div>`
+            : ''
+        }
         <div class="sdds-card-headlines">
           ${headline ? `<h6 class="sdds-card-headline">${headline}</h6>` : ''}
           ${subheadline ? `<h6 class="sdds-card-sub-headline" >${subheadline}</h6>` : ''}
@@ -137,7 +176,14 @@ const AvatarTemplate = ({ headline, subheadline, footer, clickable, text, divide
 export const Avatar = AvatarTemplate.bind({});
 
 Avatar.args = {
-  avatar: '<svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="18" cy="18" r="18" fill="#E2E2E4"/></svg>',
+  avatar: {
+    name: 'Avatar',
+    description: 'An avatar image for the card',
+    control: {
+      type: 'boolean',
+    },
+    defaultValue: true,
+  },
   divider: false,
   text: 'This is a short and consist detail text describing for the user what this text is really about.',
   imageTop: false,
