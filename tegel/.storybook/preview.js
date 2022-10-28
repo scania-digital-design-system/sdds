f23d57b0c0efa4cde437caf32ef1c74a8505d190
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2022-10-28 11:50:52 +0200

    fix(storybook): adding grid viewports + padded layout as default

--- tegel/.storybook/preview.js
+++ tegel/.storybook/preview.js
@@ -10,6 +10,58 @@ import { useDarkMode } from 'storybook-dark-mode';
 // Test below one in Netlify/Amplify build
 import '../www/build/tegel.css';
 
+const customViewports = {
+  xs: {
+    name: 'grid-xs',
+    styles: {
+      width: '607px',
+      height: '963px',
+    },
+  },
+  sm: {
+    name: 'grid-sm',
+    styles: {
+      width: '608px',
+      height: '963px',
+    },
+  },
+  md: {
+    name: 'grid-md',
+    styles: {
+      width: '800px',
+      height: '801px',
+    },
+  },
+  lg: {
+    name: 'grid-lg',
+    styles: {
+      width: '992px',
+      height: '1200px',
+    },
+  },
+  xlg: {
+    name: 'grid-xlg',
+    styles: {
+      width: '1184px',
+      height: '1400px',
+    },
+  },
+  xxlg: {
+    name: 'grid-xxlg',
+    styles: {
+      width: '1367px',
+      height: '1600px',
+    },
+  },
+  max: {
+    name: 'grid-max',
+    styles: {
+      width: '1584px',
+      height: '1680px',
+    },
+  },
+};
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
@@ -25,6 +77,7 @@ export const parameters = {
     'storybook/docs/panel': { index: -1 },
     // https://storybook.js.org/addons/storybook-dark-mode#:~:text=%5D%0A%7D%3B-,Configuration,-Configure%20the%20dark
   },
+  viewport: { viewports: customViewports },
   docs: {
     // Fix for dark mode not working on docs tab
     container: props => {
@@ -58,6 +111,7 @@ export const parameters = {
       order: ['Intro', 'Foundations', 'Utilities', 'Components', 'Patterns'],
     },
   },
+  layout: 'padded',
 };
 
 defineCustomElements();
