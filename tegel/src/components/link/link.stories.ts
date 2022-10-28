75654dd95bcd2204b32caf419b1a796b3fd06ddc
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2022-10-28 11:34:14 +0200

    fix(link): remove demo styling

--- tegel/src/components/link/link.stories.ts
+++ tegel/src/components/link/link.stories.ts
@@ -27,22 +27,14 @@ export default {
   },
 } as Meta<Args>;
 
-const Template: StoryFn<Args> = args => {
-  return formatHtmlPreview(
+const Template: StoryFn<Args> = args =>
+  formatHtmlPreview(
     `
-      <style>
-        /* The demo-text class is only for demonstration purposes. */
-        .demo-text {
-          font-size: 36px;
-        }
-      </style>
       <a class="${clsx('demo-text sdds-link', args.disabled && 'disabled', args.noUnderline && 'sdds-link--no-underline')}" href="#">
         This is a link.
       </a>
-   
   `,
   );
-};
 export const Default = Template.bind({});
 
 Default.args = defaultArgs;
