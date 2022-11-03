c5ada40972c66d7a2fb0d2b0e5a052ea5cda02e6
mJarsater <62651103+mJarsater@users.noreply.github.com>
2022-11-03 13:41:57 +0100

    feat(button): migrate button component (#531)
    * feat(button): migrate button component
    
    * fix(button): corrected path for story file
    
    * fix(button): corrected the story file to include formatHtmlPreview and align with other story files
    
    * fix(button): moved on-white styling to the component rather than the theme
    
    * fix(button): added variables for on-white theme
    
    * fix(button): cleaned up story of the button
    
    * fix(button): corrected size prop on story
    
    * fix(button): size fixes and story celanup
    
    * fix(button): description of type on readme
    
    * feat(button): added variant prop tp control onwhite for wc and added types to type prop
    
    * feat(button): added support for xs button size
    
    * feat(button): added all icons to the story with sdds-icon, corrected margin between text and icon
    
    Added the margin only to the icon only if the button has a text + icon. Corrected the margin-left
    value for all sizes on buttons with text + icon
    
    * fix(button): added correct sizing to the icon and aligned story for both native and wc
    
    * fix(button): cleaned up story
    
    * fix(button): cleaned up the story for the component
    
    * fix(button): added rule to exclude icons choises for xs button
    
    * fix(button): corrected the defaultValue to be displayed
    
    * fix(button): added class name to button text span
    
    added a class "sdds-btn-text" to the span for the button component
    
    BREAKING CHANGE: added a class "sdds-btn-text" to the span for the button component
    
    * feat(button): added color/fill on icon based on button type
    
    * fix(button): corrected the size of the icon on the story
    
    * fix(button): corrected icon on focus/hover state on secondary
    
    * fix(button): corrected the color for danger icon
    
    * fix(button): corrected name of the webcomponent story
    
    * fix(button): fix the padding diff between icon and text
    
    * fix(button): size fix for the webcomponent version
    
    * fix(button): corrected padding with negative margin on webcomponent
    
    * fix(button): used px instead of rem for margin left on Webcomponent icon
    
    * fix(button): changed name of story to match with Webcomponent one
    
    * fix(button): seperated stories and removed defaultValue
    
    * fix(button): added check for onlyIcon to display/hide text on webcomponent
    
    * fix(button): added turnary to add onlyIcon prop
    
    * fix(button): added padding for only icon on sm button, corrected var name and selectors
    
    * fix(button): added class to target onlyIcon on native, fixed fullbleed
    
    Added a class to the native sdds-btn for onlyIcon buttons
    
    BREAKING CHANGE: Added a class for only icon buttons on native button, this will need to be added by
    the user
    
    * fix(button): conditional for hiding fullbleed if onlyIcon is true
    
    * fix(button): fix padding/font-size for xs btn, corrected margin-left for fullbleed btns
    
    * fix(button): corrected border-radius for xs buttons
    
    * fix(button): changed height vars from rem to px

---
+++ tegel/src/components/button/button-webcomponent.stories.ts
@@ -0,0 +1,141 @@
+import { formatHtmlPreview } from '../../utils/utils';
+import { iconsNames } from '../icon/iconsArray.js';
+
+export default {
+  title: 'Components/Button',
+  parameters: {
+    layout: 'padded',
+    chromatic: {
+      disableSnapshot: false, // enables snapshotting for the component
+    },
+  },
+  argTypes: {
+    text: {
+      name: 'Text',
+      description: 'The text to be displayed on the button',
+      control: {
+        type: 'text',
+      },
+    },
+    btnType: {
+      name: 'Type',
+      description: 'Four different button types to help the user to distinguish the level of importance of the task they represent',
+      control: {
+        type: 'radio',
+        options: { Primary: 'primary', Secondary: 'secondary', Ghost: 'ghost', Danger: 'danger' },
+      },
+      table: {
+        defaultValue: { summary: 'primary' },
+      },
+    },
+    size: {
+      name: 'Size',
+      control: {
+        type: 'radio',
+        options: { Large: 'lg', Medium: 'md', Small: 'sm', ExtraSmall: 'xs' },
+      },
+      table: {
+        defaultValue: { summary: 'lg' },
+      },
+      description: 'Size of the button',
+    },
+    variant: {
+      name: 'Variant',
+      control: {
+        type: 'radio',
+        options: { 'On light': 'on-light', 'On dark': 'on-dark' },
+      },
+      description: 'Button variant.',
+      table: {
+        defaultValue: { summary: 'on-dark' },
+      },
+    },
+    fullbleed: {
+      name: 'Fullbleed',
+      type: 'boolean',
+      description: 'Fluid width in certain components-old',
+      table: {
+        defaultValue: { summary: false },
+      },
+      if: { arg: 'onlyIcon', truthy: false },
+    },
+    disabled: {
+      name: 'Disabled',
+      type: 'boolean',
+      description: 'Choose to disable the button',
+      table: {
+        defaultValue: { summary: false },
+      },
+    },
+    onlyIcon: {
+      name: 'Only Icon',
+      description: 'Displays only the icon and excludes any text from the button',
+      control: {
+        type: 'boolean',
+      },
+      table: {
+        defaultValue: { summary: false },
+      },
+      if: { arg: 'size', neq: 'xs' },
+    },
+    icon: {
+      name: 'Icon',
+      description: 'Icon to display on the button. Choose "none" to exclude the icon.',
+      control: {
+        type: 'select',
+      },
+      options: ['none', ...iconsNames],
+      if: { arg: 'size', neq: 'xs' },
+    },
+  },
+};
+
+const WebComponentTemplate = ({ onlyIcon, size, variant, btnType, fullbleed, disabled, icon, text = 'Button' }) => {
+  return formatHtmlPreview(
+    `
+    <style>
+    .demo-wrapper{
+      width: 100%;
+    }
+  </style>
+  <div class="demo-wrapper">
+  <sdds-button ${onlyIcon ? 'onlyIcon' : ''} type="${btnType}" size="${size}" ${disabled ? 'disabled' : ''} ${fullbleed ? 'fullbleed' : ''} text="${
+      onlyIcon ? '' : text
+    }" variant="${variant}" >
+  ${onlyIcon || (icon && icon !== 'none') ? `<sdds-icon slot='icon' class='sdds-btn-icon'  size='${size == 'sm' ? '16px' : '20px'}' name='${icon}'></sdds-icon>` : ''}  
+</sdds-button>
+  </div>
+  `,
+  );
+};
+
+const defaultValues = {
+  text: 'Button',
+  btnType: 'primary',
+  size: 'lg',
+  variant: 'on-dark',
+  fullbleed: false,
+  disabled: false,
+  onlyIcon: false,
+  icon: 'none',
+};
+
+/** Button as web component */
+export const WebComponent = WebComponentTemplate.bind({});
+WebComponent.args = {
+  ...defaultValues,
+};
+
+export const WebComponentWithIcon = WebComponentTemplate.bind({});
+WebComponentWithIcon.args = {
+  ...defaultValues,
+  icon: 'truck',
+};
+
+export const WebComponentOnlyIcon = WebComponentTemplate.bind({});
+WebComponentOnlyIcon.args = {
+  ...defaultValues,
+  text: '',
+  onlyIcon: true,
+  icon: 'truck',
+};
