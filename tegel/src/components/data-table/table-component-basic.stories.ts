21fa836e8a17af5d593b256258d9cad598d4136a
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2022-11-08 16:22:46 +0100

    fix(data-table): default story improvements

--- tegel/src/components/data-table/table-component-basic.stories.ts
+++ tegel/src/components/data-table/table-component-basic.stories.ts
@@ -5,49 +5,130 @@ export default {
   argTypes: {
     verticalDivider: {
       name: 'Vertical dividers',
+      description: 'When enabled, table has vertical dividers between columns.',
       control: {
         type: 'boolean',
       },
-      defaultValue: false,
+      table: {
+        defaultValue: {
+          summary: false,
+        },
+      },
     },
     compactDesign: {
-      name: 'Compact Design',
+      name: 'Compact design',
+      description: 'Enables compact design of the table, rows with less height.',
       control: {
         type: 'boolean',
       },
-      defaultValue: false,
+      table: {
+        defaultValue: {
+          summary: false,
+        },
+      },
     },
     onWhiteBackground: {
       name: 'On white background',
+      description: 'Changes BG color of table element to grey variation for better visibility on white layouts',
       control: {
         type: 'boolean',
       },
-      defaultValue: false,
+      table: {
+        defaultValue: {
+          summary: false,
+        },
+      },
     },
     responsiveDesign: {
-      name: 'Responsive design',
+      name: 'Responsive table',
+      description: 'Table takes 100% of available width. For column values less then 192px, "No minimum width" has to be enabled too. ',
       control: {
         type: 'boolean',
       },
-      defaultValue: false,
+      table: {
+        defaultValue: {
+          summary: false,
+        },
+      },
+    },
+    noMinWidth: {
+      name: 'No minimum width',
+      description: 'Resets min-width rule and enabled setting column width value less then 192px which is default one. When enabled, controls for columns width will show here.',
+      control: {
+        type: 'boolean',
+      },
+      table: {
+        defaultValue: {
+          summary: false,
+        },
+      },
+    },
+    column1Width: {
+      name: 'Column 1 width',
+      description:
+        'Value of width for column 1. In order to work correctly "No minimum width" has to be enabled too. When editing please provide a unit too next tot the value, eg. 200px.',
+      type: 'string',
+      table: {
+        defaultValue: {
+          summary: '192px',
+        },
+      },
+      if: { arg: 'noMinWidth', eq: true },
+    },
+    column2Width: {
+      name: 'Column 2 width',
+      description:
+        'Value of width for column 2. In order to work correctly "No minimum width" has to be enabled too. When editing please provide a unit too next tot the value, eg. 200px.',
+      type: 'string',
+      table: {
+        defaultValue: {
+          summary: '192px',
+        },
+      },
+      if: { arg: 'noMinWidth', eq: true },
+    },
+    column3Width: {
+      name: 'Column 3 width',
+      description:
+        'Value of width for column 3. In order to work correctly "No minimum width" has to be enabled too. When editing please provide a unit too next tot the value, eg. 200px.',
+      type: 'string',
+      table: {
+        defaultValue: {
+          summary: '192px',
+        },
+      },
+      if: { arg: 'noMinWidth', eq: true },
+    },
+    column4Width: {
+      name: 'Column 4 width',
+      description:
+        'Value of width for column 4. In order to work correctly "No minimum width" has to be enabled too. When editing please provide a unit too next tot the value, eg. 200px.',
+      type: 'string',
+      table: {
+        defaultValue: {
+          summary: '192px',
+        },
+      },
+      if: { arg: 'noMinWidth', eq: true },
     },
   },
 };
 
-const BasicTemplate = ({ verticalDivider, compactDesign, onWhiteBackground, responsiveDesign }) =>
+const BasicTemplate = args =>
   formatHtmlPreview(`
   <sdds-table
       id="basic-table"
-      vertical-dividers="${verticalDivider}"
-      compact-design="${compactDesign}"
-      white-background="${onWhiteBackground}"
-      enable-responsive="${responsiveDesign}"
+      vertical-dividers="${args.verticalDivider}"
+      compact-design="${args.compactDesign}"
+      white-background="${args.onWhiteBackground}"
+      enable-responsive="${args.responsiveDesign}"
+      no-min-width="${args.noMinWidth}"
             >
       <sdds-table-header>
-          <sdds-header-cell column-key='truck' column-title='Truck type' ></sdds-header-cell>
-          <sdds-header-cell column-key='driver' column-title='Driver name'></sdds-header-cell>
-          <sdds-header-cell column-key='country' column-title='Country'></sdds-header-cell>
-          <sdds-header-cell column-key='mileage' column-title='Mileage' text-align='right'></sdds-header-cell>
+          <sdds-header-cell column-key='truck' column-title='Truck type' custom-width="${args.column1Width};" }></sdds-header-cell>
+          <sdds-header-cell column-key='driver' column-title='Driver name' custom-width="${args.column2Width};" }></sdds-header-cell>
+          <sdds-header-cell column-key='country' column-title='Country' custom-width="${args.column3Width};" }></sdds-header-cell>
+          <sdds-header-cell column-key='mileage' column-title='Mileage' custom-width="${args.column4Width};" text-align='right' }></sdds-header-cell>
       </sdds-table-header>
       <sdds-table-body>
           <sdds-table-body-row>
@@ -89,5 +170,15 @@ const BasicTemplate = ({ verticalDivider, compactDesign, onWhiteBackground, resp
       </sdds-table-body>
   </sdds-table>`);
 
-export const Basic = BasicTemplate.bind({});
-Basic.args = {};
+export const Default = BasicTemplate.bind({});
+Default.args = {
+  compactDesign: false,
+  onWhiteBackground: false,
+  verticalDivider: false,
+  responsiveDesign: false,
+  noMinWidth: false,
+  column1Width: '',
+  column2Width: '',
+  column3Width: '',
+  column4Width: '',
+};
