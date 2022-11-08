8cfa18cae5b01c9fe3f9200784f3520ccbd24682
Emelie Litwin <emelie.litwin@scania.com>
2022-11-08 16:14:10 +0100

    feat(dropdown): migrate dropdown component (#544)
    Co-authored-by: theJohnnyMe <nikola.johnny.mirkovic@scania.com>
    Co-authored-by: Alexander Eneroth <alexandereneroth@users.noreply.github.com>

---
+++ tegel/src/components/dropdown/dropdown-native.stories.js
@@ -0,0 +1,73 @@
+import { formatHtmlPreview } from '../../utils/utils';
+
+export default {
+  title: 'Components/Dropdown',
+  parameters: {
+    layout: 'centered',
+  },
+  argTypes: {
+    label: {
+      name: 'Label',
+      type: 'string',
+      description: 'Label text helps to describe what the dropdown contains',
+      defaultValue: '',
+    },
+    size: {
+      name: 'Size',
+      control: {
+        type: 'radio',
+        options: { Large: 'lg', Medium: 'md', Small: 'sm' },
+      },
+      defaultValue: 'lg',
+      description: 'Size of the dropdown',
+    },
+    disabled: {
+      name: 'Disabled',
+      description: 'Disables the component',
+      control: {
+        type: 'boolean',
+      },
+      defaultValue: false,
+    },
+    state: {
+      name: 'State',
+      control: {
+        type: 'radio',
+      },
+      options: {
+        Default: 'default',
+        Error: 'error',
+      },
+      defaultValue: 'default',
+      description: 'Support error state',
+    },
+    helper: {
+      name: 'Helper text',
+      control: {
+        type: 'text',
+      },
+      defaultValue: '',
+      description: 'Helper text assists the user with additional information about the dropdown',
+    },
+  },
+};
+
+const NativeTemplate = ({ disabled, size, helper, label, state }) =>
+  formatHtmlPreview(`
+    <div style="width:300px">
+        <div class="sdds-dropdown ${size !== 'lg' ? `sdds-dropdown-${size}` : ''} ${state === 'error' ? 'sdds-dropdown--error' : ''}" >
+        ${label !== '' ? `<span class="sdds-dropdown-label-outside">${label}</span> ` : ''}
+        <select 
+        name="nativeDropdown" 
+        id="mySelect"
+        ${disabled ? `disabled` : ''} >
+            <option value="truck">Truck</option>
+            <option value="bus">Bus</option>
+            <option value="car">Car</option>
+          </select>
+          ${helper !== '' ? `<span class="sdds-dropdown-helper">${helper}</span>` : ''}
+        </div>
+      </div>
+  `);
+
+export const Native = NativeTemplate.bind({});
---
+++ tegel/src/components/dropdown/dropdown-option/readme.md
@@ -0,0 +1,38 @@
+# Dropdown option
+
+
+
+<!-- Auto Generated Below -->
+
+
+## Properties
+
+| Property   | Attribute  | Description                                                      | Type      | Default     |
+| ---------- | ---------- | ---------------------------------------------------------------- | --------- | ----------- |
+| `selected` | `selected` | Selected set to true if selected                                 | `boolean` | `false`     |
+| `value`    | `value`    | Value is a unique string that will be used for application logic | `string`  | `undefined` |
+
+
+## Events
+
+| Event          | Description | Type               |
+| -------------- | ----------- | ------------------ |
+| `selectOption` |             | `CustomEvent<any>` |
+
+
+## Dependencies
+
+### Used by
+
+ - [sdds-dropdown-filter](../dropdown-filter)
+
+### Graph
+```mermaid
+graph TD;
+  sdds-dropdown-filter --> sdds-dropdown-option
+  style sdds-dropdown-option fill:#f9f,stroke:#333,stroke-width:4px
+```
+
+----------------------------------------------
+
+*Built with [StencilJS](https://stenciljs.com/)*
