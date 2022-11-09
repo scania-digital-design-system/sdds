bd508451fc666e15ec14882619fa026972008076
mJarsater <martin.jarsater@hotmail.com>
2022-11-09 11:16:29 +0100

    fix(block): standardised controls

--- tegel/src/components/block/block.stories.ts
+++ tegel/src/components/block/block.stories.ts
@@ -1,13 +1,9 @@
+import { formatHtmlPreview } from '../../utils/utils';
+
 export default {
   title: 'Components/Block',
   parameters: {
-    backgrounds: {
-      default: 'white',
-      values: [
-        { name: 'white', value: '#fff' },
-        { name: 'grey', value: '#f9fafb' },
-      ],
-    },
+    layout: 'padded',
   },
   argTypes: {
     type: {
@@ -16,29 +12,34 @@ export default {
       defaultValue: 'default',
       control: {
         type: 'radio',
-        options: ['default', 'variant'],
-        labels: {
-          default: 'Default',
-          variant: 'Variant',
-        },
       },
+      options: ['Default', 'Variant'],
     },
+  },
+  args: {
+    type: 'Default',
   },
 };
 
-const Template = args => `
+const Template = ({ type }) => {
+  let typeLookup = {
+    Default: 'default',
+    Variant: 'variant',
+  };
+
+  return formatHtmlPreview(
+    `
     <div class="sdds-demo-block"> 
-      <div class="${args.type === 'variant' ? 'sdds-block sdds-block__variant' : 'sdds-block'}">
+      <div class="${typeLookup[type] === 'variant' ? 'sdds-block sdds-block__variant' : 'sdds-block'}">
         <p>Content be here...</p>
         <div class="sdds-block">
           <p>Content be here...</p>
         </div>
       </div>
     </div>
-    `;
+    `,
+  );
+};
 
 export const Default = Template.bind({});
 Default.args = {};
-Default.parameters = {
-  backgrounds: { default: 'white' },
-};
