a768372e0ce7006c3931304e83eec339300fa9a8
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2022-11-11 09:05:19 +0100

    fix(data-table): migration to tegel (#571)
    * fix(data-table): migration to tegel
    
    * fix(data-table): typescript story files format
    
    * fix: package lock file update
    
    * fix(data-table): story path update
    
    * fix(data-table): adding formatHtmlPreview
    
    * fix(data-table): native story improvements
    
    * fix(data-table): default story improvements
    
    * fix(data-table): sorting batch story
    
    * fix(data-table): sorting custom width story
    
    * fix(data-table): sorting event listeners story
    
    * fix(data-table): sorting expendable rows story
    
    * fix(data-table): sorting filtering story
    
    * fix(data-table): sorting multiselect story
    
    * fix(data-table): sorting pagination story
    
    * fix(data-table): readme files import
    
    * chore: package lock file update
    
    * fix: code review changes
    
    * fix: formatting
    
    * chore: new lock file

---
+++ tegel/src/components/data-table/table-component-filtering.stories.ts
@@ -0,0 +1,113 @@
+import { formatHtmlPreview } from '../../utils/utils';
+import sddsTable from './table/readme.md';
+import sddsTableToolbar from './table-toolbar/readme.md';
+import sddsHeader from './table-header/readme.md';
+import sddsHeaderCell from './table-header-cell/readme.md';
+import sddsTableBody from './table-body/readme.md';
+import sddsBodyRow from './table-body-row/readme.md';
+import sddsBodyRowExpandable from './table-body-row-expandable/readme.md';
+import sddsBodyCell from './table-body-cell/readme.md';
+import sddsTableFooter from './table-footer/readme.md';
+
+export default {
+  title: 'Components/Data Table/Web Component',
+  parameters: {
+    notes: {
+      'sdds-table': sddsTable,
+      'sdds-table-toolbar': sddsTableToolbar,
+      'sdds-header': sddsHeader,
+      'sdds-header-cell': sddsHeaderCell,
+      'sdds-table-body': sddsTableBody,
+      'sdds-body-row': sddsBodyRow,
+      'sdds-body-row-expandable': sddsBodyRowExpandable,
+      'sdds-body-cell': sddsBodyCell,
+      'sdds-table-footer': sddsTableFooter,
+    },
+  },
+  argTypes: {
+    verticalDivider: {
+      name: 'Vertical dividers',
+      description: 'When enabled, table has vertical dividers between columns.',
+      control: {
+        type: 'boolean',
+      },
+      table: {
+        defaultValue: {
+          summary: false,
+        },
+      },
+    },
+    compactDesign: {
+      name: 'Compact design',
+      description: 'Enables compact design of the table, rows with less height.',
+      control: {
+        type: 'boolean',
+      },
+      table: {
+        defaultValue: {
+          summary: false,
+        },
+      },
+    },
+    onWhiteBackground: {
+      name: 'On white background',
+      description:
+        'Changes BG color of table element to grey variation for better visibility on white layouts',
+      control: {
+        type: 'boolean',
+      },
+      table: {
+        defaultValue: {
+          summary: false,
+        },
+      },
+    },
+    responsiveDesign: {
+      name: 'Responsive table',
+      description:
+        'Table takes 100% of available width. For column values less then 192px, "No minimum width" has to be enabled too. ',
+      control: {
+        type: 'boolean',
+      },
+      table: {
+        defaultValue: {
+          summary: false,
+        },
+      },
+    },
+  },
+};
+
+const FilteringTemplate = ({
+  verticalDivider,
+  compactDesign,
+  onWhiteBackground,
+  responsiveDesign,
+}) =>
+  formatHtmlPreview(`
+  <h3>Filtering example</h3>
+   <sdds-table
+      id="filtering-table"
+      vertical-dividers="${verticalDivider}"
+      compact-design="${compactDesign}"
+      white-background="${onWhiteBackground}"
+       enable-responsive="${responsiveDesign}"
+   >
+          <sdds-table-toolbar table-title="Filter" enable-filtering></sdds-table-toolbar>
+          <sdds-table-header>
+              <sdds-header-cell column-key='truck' column-title='Truck type'></sdds-header-cell>
+              <sdds-header-cell column-key='driver' column-title='Driver name'></sdds-header-cell>
+              <sdds-header-cell column-key='country' column-title='Country' ></sdds-header-cell>
+              <sdds-header-cell column-key='mileage' column-title='Mileage' text-align='right'></sdds-header-cell>
+          </sdds-table-header>
+          <sdds-table-body enable-dummy-data>
+          </sdds-table-body>
+  </sdds-table>`);
+
+export const Filtering = FilteringTemplate.bind({});
+Filtering.args = {
+  compactDesign: false,
+  onWhiteBackground: false,
+  verticalDivider: false,
+  responsiveDesign: false,
+};
---
+++ tegel/src/components/data-table/table/readme.md
@@ -0,0 +1,32 @@
+# sdds-table
+
+
+
+<!-- Auto Generated Below -->
+
+
+## Properties
+
+| Property               | Attribute                | Description                                                              | Type      | Default     |
+| ---------------------- | ------------------------ | ------------------------------------------------------------------------ | --------- | ----------- |
+| `compactDesign`        | `compact-design`         | Enables style where data-table toolbar, rows and footer are less high    | `boolean` | `false`     |
+| `enableExpandableRows` | `enable-expandable-rows` | Enables extended row feature of data-table                               | `boolean` | `false`     |
+| `enableMultiselect`    | `enable-multiselect`     | Enables multiselect feature of data-table                                | `boolean` | `false`     |
+| `enableResponsive`     | `enable-responsive`      | Enables table to take 100% available width with equal spacing of columns | `boolean` | `false`     |
+| `noMinWidth`           | `no-min-width`           | Enables to customise width on data-table columns                         | `boolean` | `undefined` |
+| `verticalDividers`     | `vertical-dividers`      | Enables style with vertical dividers between columns                     | `boolean` | `false`     |
+| `whiteBackground`      | `white-background`       | Changes a colors of data data-table when used on white background        | `boolean` | `false`     |
+
+
+## Events
+
+| Event                     | Description                                                             | Type               |
+| ------------------------- | ----------------------------------------------------------------------- | ------------------ |
+| `commonTableStylesEvent`  | Sends out status of different general styling changes to children tegel | `CustomEvent<any>` |
+| `enableExpandedRowsEvent` | Sends out status of multiselect feature to children tegel               | `CustomEvent<any>` |
+| `enableMultiselectEvent`  | Sends out status of multiselect feature to children tegel               | `CustomEvent<any>` |
+
+
+----------------------------------------------
+
+*Built with [StencilJS](https://stenciljs.com/)*
