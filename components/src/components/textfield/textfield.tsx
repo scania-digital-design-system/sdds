92fa43353df1ee480780cfe94171cbe917eed57b
mJarsater <62651103+mJarsater@users.noreply.github.com>
2022-11-16 13:29:33 +0100

    fix/textfield state count issues (#599)
    Co-authored-by: theJohnnyMe <nikola.johnny.mirkovic@scania.com>
    Co-authored-by: albinscania <98806906+albinscania@users.noreply.github.com>
    Co-authored-by: Albin Larsson <albin@farstun.com>

--- components/src/components/textfield/textfield.tsx
+++ components/src/components/textfield/textfield.tsx
@@ -215,7 +215,7 @@ export class Textfield {
 
           {this.maxlength > 0 && (
             <div class="sdds-textfield-textcounter">
-              {this.value?.length}
+              {this.value === null ? 0 : this.value?.length}
               <span class="sdds-textfield-textcounter-divider"> / </span>
               {this.maxlength}
             </div>
