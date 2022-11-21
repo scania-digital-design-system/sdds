562c96d6ab5308887f75ae68f9ebfb3c49be6694
Alexander Eneroth <alexandereneroth@users.noreply.github.com>
2022-11-21 13:26:36 +0100

    fix(dropdown): fix dropdown singleselect supporting multiple selection

--- tegel/src/components/dropdown/dropdown-option/dropdown-option.tsx
+++ tegel/src/components/dropdown/dropdown-option/dropdown-option.tsx
@@ -73,8 +73,15 @@ export class DropdownOption {
   }
 
   selectOptionHandler(value) {
+    const listOptions = value.parent.childNodes;
     this.selectOption.emit(value);
-
+    if (!this.isMultiSelectOption) {
+      listOptions.forEach((optionEl) => {
+        // TODO: fix and enable rule
+        // eslint-disable-next-line no-param-reassign
+        optionEl.selected = false;
+      });
+    }
     const optionCheckbox = this.host.shadowRoot.querySelector('input');
 
     if (this.selected) {
