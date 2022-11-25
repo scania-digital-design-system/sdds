ae8f211a5c0b211011c702da73e4d21f7e2c084d
mJarsater <62651103+mJarsater@users.noreply.github.com>
2022-11-25 11:25:19 +0100

    fix/remove inline svgs footer/header/side menu (#641)

--- tegel/src/components/header/header-search.stories.ts
+++ tegel/src/components/header/header-search.stories.ts
@@ -36,6 +36,11 @@ const SearchbarMenuTemplate = (args) => {
   return formatHtmlPreview(
     `
     <style>
+    @import url('https://cdn.digitaldesign.scania.com/icons/webfont/css/sdds-icons.css');
+    i {
+      font-size: 16px;
+    }
+
     .demo-wrapper {
       font-size: 14px;
     }
@@ -80,10 +85,8 @@ const SearchbarMenuTemplate = (args) => {
         <button class='sdds-nav__item-core' onclick='toggleInlineDropdown()'>
             <p class='sdds-nav__item-core-text'>Item 3</p>
             <span class='sdds-nav__dropdown-icon'>
-                  <svg class="sdds-nav__dropdown-icon-svg" viewBox='0 0 14 7' fill='none' xmlns='http://www.w3.org/2000/svg'>
-                    <path fill-rule='evenodd' clip-rule='evenodd' fill='currentColor' d='M1.13668 0.891564C1.33194 0.696302 1.64853 0.696302 1.84379 0.891564L6.78786 5.83563C6.90501 5.95278 7.09496 5.95278 7.21212 5.83563L12.1562 0.891564C12.3515 0.696302 12.668 0.696303 12.8633 0.891565C13.0586 1.08683 13.0586 1.40341 12.8633 1.59867L7.91923 6.54273C7.41155 7.05041 6.58843 7.05041 6.08075 6.54273L1.13668 1.59867C0.941419 1.40341 0.941419 1.08683 1.13668 0.891564Z'/>
-                  </svg>                                       
-              </span>
+              <sdds-icon class="sdds-nav__dropdown-icon-svg" name="chevron_down" size="16px" />                                                                
+            </span>
         </button>
         <ul class='sdds-nav__dropdown-menu'>
             <li class='sdds-nav__dropdown-item'><a class='sdds-nav__dropdown-item-core' href='#'>Sub item 3 long label...</a></li>
@@ -98,9 +101,7 @@ const SearchbarMenuTemplate = (args) => {
           <ul class='sdds-nav__searchbar-menu'>
               <li class='sdds-nav__searchbar-item--top' tabindex="0">
                     <p class='sdds-nav__searchbar-all-results'>
-                    <svg width="16" height="16" class="sdds-nav__app-searchbar-results-mgl-svg" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path fill-rule="evenodd" clip-rule="evenodd" d="M6.47098 0.99231C3.44526 0.99231 0.992432 3.44514 0.992432 6.47086C0.992432 9.49657 3.44526 11.9494 6.47098 11.9494C7.80373 11.9494 9.02533 11.4735 9.97512 10.6824L14.1407 14.848C14.336 15.0433 14.6526 15.0433 14.8479 14.848C15.0431 14.6528 15.0431 14.3362 14.8479 14.1409L10.6823 9.97531C11.4735 9.02547 11.9495 7.80375 11.9495 6.47086C11.9495 3.44514 9.4967 0.99231 6.47098 0.99231ZM1.99243 6.47086C1.99243 3.99742 3.99755 1.99231 6.47098 1.99231C8.94441 1.99231 10.9495 3.99742 10.9495 6.47086C10.9495 8.94429 8.94441 10.9494 6.47098 10.9494C3.99755 10.9494 1.99243 8.94429 1.99243 6.47086Z" fill="currentColor" fill-opacity="1"></path>
-                    </svg>
+                    <sdds-icon name="search" class="sdds-nav__app-searchbar-results-mgl-svg"></sdds-icon>
                     See all search results <span>(press enter)</span>
                     </p>
               </li>
@@ -109,20 +110,17 @@ const SearchbarMenuTemplate = (args) => {
             <li class='sdds-nav__searchbar-results-item'>
               <a href='' class='sdds-nav__searchbar-results-item-core'>User – Eric Mattsson – IXCD Visual designer </a>
               <button class="sdds-nav__app-searchbar-results-x-btn">
-                <svg width="16" height="16" class="sdds-nav__app-searchbar-results-x-btn-svg" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path fill-rule="evenodd" clip-rule="evenodd" d="M12.2499 3.75004C12.4452 3.9453 12.4452 4.26188 12.2499 4.45714L4.4571 12.25C4.26184 12.4452 3.94526 12.4452 3.75 12.25C3.55474 12.0547 3.55474 11.7381 3.75 11.5429L11.5428 3.75004C11.7381 3.55478 12.0547 3.55478 12.2499 3.75004Z" fill="currentColor" fill-opacity="1"/>
-                  <path fill-rule="evenodd" clip-rule="evenodd" d="M3.75004 3.74996C3.9453 3.5547 4.26188 3.5547 4.45714 3.74996L12.25 11.5428C12.4452 11.738 12.4452 12.0546 12.25 12.2499C12.0547 12.4451 11.7381 12.4451 11.5428 12.2499L3.75004 4.45707C3.55478 4.26181 3.55478 3.94522 3.75004 3.74996Z" fill="currentColor" fill-opacity="1"/>
-                </svg>
+                <div>
+                  <sdds-icon class="sdds-nav__app-searchbar-results-x-btn" name="cross" size="16px" />
+                </div>
               </button>
             </li>
             <li class='sdds-nav__searchbar-results-item'>
               <a href='' class='sdds-nav__searchbar-results-item-core'>User– Eric Mattsson – IXCD Visual designer </a>
               <button class="sdds-nav__app-searchbar-results-x-btn">
-                <svg width="16" height="16" class="sdds-nav__app-searchbar-results-x-btn-svg" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path fill-rule="evenodd" clip-rule="evenodd" d="M12.2499 3.75004C12.4452 3.9453 12.4452 4.26188 12.2499 4.45714L4.4571 12.25C4.26184 12.4452 3.94526 12.4452 3.75 12.25C3.55474 12.0547 3.55474 11.7381 3.75 11.5429L11.5428 3.75004C11.7381 3.55478 12.0547 3.55478 12.2499 3.75004Z" fill="currentColor" fill-opacity="1"/>
-                  <path fill-rule="evenodd" clip-rule="evenodd" d="M3.75004 3.74996C3.9453 3.5547 4.26188 3.5547 4.45714 3.74996L12.25 11.5428C12.4452 11.738 12.4452 12.0546 12.25 12.2499C12.0547 12.4451 11.7381 12.4451 11.5428 12.2499L3.75004 4.45707C3.55478 4.26181 3.55478 3.94522 3.75004 3.74996Z" fill="currentColor" fill-opacity="1"/>
-                </svg>
-              </button>
+              <div>
+              <sdds-icon class="sdds-nav__app-searchbar-results-x-btn" name="cross" size="16px" />
+            </div>              </button>
               </li>
             </ul>
             <ul class='sdds-nav__searchbar-results--category'>
@@ -131,27 +129,20 @@ const SearchbarMenuTemplate = (args) => {
             <li class='sdds-nav__searchbar-results-item'>
               <a href='' class='sdds-nav__searchbar-results-item-core'>Team– Eric Mattsson – IXCD Visual designer </a>
               <button class="sdds-nav__app-searchbar-results-x-btn">
-                <svg width="16" height="16" class="sdds-nav__app-searchbar-results-x-btn-svg" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path fill-rule="evenodd" clip-rule="evenodd" d="M12.2499 3.75004C12.4452 3.9453 12.4452 4.26188 12.2499 4.45714L4.4571 12.25C4.26184 12.4452 3.94526 12.4452 3.75 12.25C3.55474 12.0547 3.55474 11.7381 3.75 11.5429L11.5428 3.75004C11.7381 3.55478 12.0547 3.55478 12.2499 3.75004Z" fill="currentColor" fill-opacity="1"/>
-                  <path fill-rule="evenodd" clip-rule="evenodd" d="M3.75004 3.74996C3.9453 3.5547 4.26188 3.5547 4.45714 3.74996L12.25 11.5428C12.4452 11.738 12.4452 12.0546 12.25 12.2499C12.0547 12.4451 11.7381 12.4451 11.5428 12.2499L3.75004 4.45707C3.55478 4.26181 3.55478 3.94522 3.75004 3.74996Z" fill="currentColor" fill-opacity="1"/>
-                </svg>
-             </button>
+              <div>
+              <sdds-icon class="sdds-nav__app-searchbar-results-x-btn" name="cross" size="16px" />
+            </div>             </button>
               </li>
           </ul>
         </ul>
         <input class="sdds-nav__searchbar-input" type="text" placeholder="Search">
           <button class="sdds-nav__app-searchbar-btn sdds-nav__app-searchbar-x-btn" onclick="toggleSearchbar()">
-            <svg class="sdds-nav__app-searchbar-btn-svg" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path fill-rule="evenodd" clip-rule="evenodd" d="M12.2499 3.75004C12.4452 3.9453 12.4452 4.26188 12.2499 4.45714L4.4571 12.25C4.26184 12.4452 3.94526 12.4452 3.75 12.25C3.55474 12.0547 3.55474 11.7381 3.75 11.5429L11.5428 3.75004C11.7381 3.55478 12.0547 3.55478 12.2499 3.75004Z" fill="currentColor" fill-opacity="1"/>
-              <path fill-rule="evenodd" clip-rule="evenodd" d="M3.75004 3.74996C3.9453 3.5547 4.26188 3.5547 4.45714 3.74996L12.25 11.5428C12.4452 11.738 12.4452 12.0546 12.25 12.2499C12.0547 12.4451 11.7381 12.4451 11.5428 12.2499L3.75004 4.45707C3.55478 4.26181 3.55478 3.94522 3.75004 3.74996Z" fill="currentColor" fill-opacity="1"/>
-            </svg>
+            <sdds-icon class="sdds-nav__app-searchbar-btn-svg" name="cross" size="20px" />
           </button>
           
       </div>
        <button class="sdds-nav__app-searchbar-btn sdds-nav__app-searchbar-mgl-btn" onclick="toggleSearchbar()">
-          <svg class="sdds-nav__app-searchbar-btn-svg" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path fill-rule="evenodd" clip-rule="evenodd" d="M6.47098 0.99231C3.44526 0.99231 0.992432 3.44514 0.992432 6.47086C0.992432 9.49657 3.44526 11.9494 6.47098 11.9494C7.80373 11.9494 9.02533 11.4735 9.97512 10.6824L14.1407 14.848C14.336 15.0433 14.6526 15.0433 14.8479 14.848C15.0431 14.6528 15.0431 14.3362 14.8479 14.1409L10.6823 9.97531C11.4735 9.02547 11.9495 7.80375 11.9495 6.47086C11.9495 3.44514 9.4967 0.99231 6.47098 0.99231ZM1.99243 6.47086C1.99243 3.99742 3.99755 1.99231 6.47098 1.99231C8.94441 1.99231 10.9495 3.99742 10.9495 6.47086C10.9495 8.94429 8.94441 10.9494 6.47098 10.9494C3.99755 10.9494 1.99243 8.94429 1.99243 6.47086Z" fill="currentColor" fill-opacity="1"></path>
-          </svg>
+          <sdds-icon class="sdds-nav__app-searchbar-btn-svg" name="search" size="20px" />
       </button>
     </div>
   </div>
@@ -186,9 +177,7 @@ const SearchbarMenuTemplate = (args) => {
         <div class='sdds-nav__right'>
           <div class='sdds-nav__item sdds-nav__app-launcher'>
               <button class='sdds-nav__app-launcher-btn' onclick='toggleAppLauncher()'>
-                  <svg class="sdds-nav__app-launcher-btn-svg" viewBox='0 0 16 16' fill='none' xmlns='http://www.w3.org/2000/svg'>
-                      <path fill-rule='evenodd' clip-rule='evenodd' d='M1.33333 2.66667C2.06971 2.66667 2.66667 2.06971 2.66667 1.33333C2.66667 0.596954 2.06971 0 1.33333 0C0.596954 0 0 0.596954 0 1.33333C0 2.06971 0.596954 2.66667 1.33333 2.66667ZM9.33307 1.33333C9.33307 2.06971 8.73612 2.66667 7.99974 2.66667C7.26336 2.66667 6.66641 2.06971 6.66641 1.33333C6.66641 0.596954 7.26336 0 7.99974 0C8.73612 0 9.33307 0.596954 9.33307 1.33333ZM16.0003 1.33333C16.0003 2.06971 15.4033 2.66667 14.6669 2.66667C13.9305 2.66667 13.3336 2.06971 13.3336 1.33333C13.3336 0.596954 13.9305 0 14.6669 0C15.4033 0 16.0003 0.596954 16.0003 1.33333ZM16.0003 8C16.0003 8.73638 15.4033 9.33333 14.6669 9.33333C13.9305 9.33333 13.3336 8.73638 13.3336 8C13.3336 7.26362 13.9305 6.66667 14.6669 6.66667C15.4033 6.66667 16.0003 7.26362 16.0003 8ZM14.6669 16C15.4033 16 16.0003 15.403 16.0003 14.6667C16.0003 13.9303 15.4033 13.3333 14.6669 13.3333C13.9305 13.3333 13.3336 13.9303 13.3336 14.6667C13.3336 15.403 13.9305 16 14.6669 16ZM7.99974 9.33333C8.73612 9.33333 9.33307 8.73638 9.33307 8C9.33307 7.26362 8.73612 6.66667 7.99974 6.66667C7.26336 6.66667 6.66641 7.26362 6.66641 8C6.66641 8.73638 7.26336 9.33333 7.99974 9.33333ZM9.33307 14.6667C9.33307 15.403 8.73612 16 7.99974 16C7.26336 16 6.66641 15.403 6.66641 14.6667C6.66641 13.9303 7.26336 13.3333 7.99974 13.3333C8.73612 13.3333 9.33307 13.9303 9.33307 14.6667ZM2.66667 8C2.66667 8.73638 2.06971 9.33333 1.33333 9.33333C0.596954 9.33333 0 8.73638 0 8C0 7.26362 0.596954 6.66667 1.33333 6.66667C2.06971 6.66667 2.66667 7.26362 2.66667 8ZM1.33333 16C2.06971 16 2.66667 15.403 2.66667 14.6667C2.66667 13.9303 2.06971 13.3333 1.33333 13.3333C0.596954 13.3333 0 13.9303 0 14.6667C0 15.403 0.596954 16 1.33333 16Z' fill='currentColor' fill-opacity='1'/>
-                  </svg>
+                <sdds-icon class="sdds-nav__app-launcher-btn-svg" name="bento" size="20px" />
               </button>
               <ul class='sdds-nav__app-launcher-menu'>
                   <li class='sdds-nav__app-launcher-item sdds-nav__app-launcher-item--category'>
