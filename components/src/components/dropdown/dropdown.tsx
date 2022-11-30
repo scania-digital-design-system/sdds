e59788965ab7e70dfeb74111901beaa79bf463cb
mJarsater <martin.jarsater@hotmail.com>
2022-11-30 11:21:38 +0100

    fix(dropdown): label placement and padding - components

--- components/src/components/dropdown/dropdown.tsx
+++ components/src/components/dropdown/dropdown.tsx
@@ -315,8 +315,9 @@ export class Dropdown {
             class={`sdds-dropdown-toggle ${this.selectedValue === 'filter' ? 'is-filter' : ''} ${
               this.selectedValue !== '' || this.selectedLabelsArray.length > 0
                 ? 'sdds-dropdown-toggle--selected'
-                : ''
-            }`}
+                : ''}
+                ${this.labelPosition === 'inside' && this.selectedValue !== '' || this.selectedLabelsArray.length > 0 ? 'sdds-dropdown-toggle-label-inside': ''}
+                `}
             type="button"
             onClick={() => this.handleClick()}
             ref={(node) => (this.node = node)}
