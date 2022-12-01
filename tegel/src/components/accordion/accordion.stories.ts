a7b1d7dd218f89f12cef3c288f519ba02daac0ca
Alexander Eneroth <alexandereneroth@users.noreply.github.com>
2022-12-01 14:37:18 +0100

    feat(accordion): rename affix (#671)
    Co-authored-by: Default <alex.john.eneroth@gmail.om>

--- tegel/src/components/accordion/accordion.stories.ts
+++ tegel/src/components/accordion/accordion.stories.ts
@@ -6,20 +6,35 @@ export default {
   title: 'Components/Accordion',
   argTypes: {
     iconPosition: {
-      name: 'Icon position',
+      name: 'Expand icon position',
       control: {
         type: 'radio',
       },
       options: { End: 'end', Start: 'start' },
-      description: 'Icon position',
+      description: 'The horizontal position of the expand icon.',
+      table: {
+        defaultValue: { summary: 'end' },
+      },
     },
     disabled: {
-      name: 'Disabled',
-      description: 'Disabled',
+      name: 'Disable all items',
+      description: 'Disable all accordion items.',
+      control: {
+        type: 'boolean',
+      },
+      table: {
+        defaultValue: { summary: 'false' },
+      },
     },
     paddingReset: {
       name: 'Less padding right',
-      description: 'Less padding on the right in accordion items',
+      description: 'Less padding on the right inside accordion items.',
+      control: {
+        type: 'boolean',
+      },
+      table: {
+        defaultValue: { summary: 'false' },
+      },
     },
   },
   parameters: {
@@ -33,7 +48,7 @@ export default {
 };
 
 const Template = ({ disabled, iconPosition, paddingReset }) => {
-  const affixAttr = iconPosition === 'start' ? 'affix="prefix"' : '';
+  const affixAttr = iconPosition === 'start' ? 'expand-icon-position="start"' : '';
   const disabledAttr = disabled ? 'disabled' : '';
   const paddingResetAttr = paddingReset ? 'padding-reset' : '';
   const tabIndexAttr = 'tabindex="0"';
@@ -46,30 +61,11 @@ const Template = ({ disabled, iconPosition, paddingReset }) => {
       </sdds-accordion-item>
       <sdds-accordion-item header="Second item" ${tabIndexAttr} ${affixAttr} ${disabledAttr} ${paddingResetAttr} expanded>
         This is the panel, which contains associated information with the header. Usually it contains text, set in the same size as the header. 
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis laoreet vestibulum fermentum. Proin ac odio sed tellus fermentum placerat. Nam sit amet orci dui. Proin commodo tellus at mauris accumsan blandit. Donec eget suscipit lorem, sit amet ultrices tellus. Cras massa ligula, rhoncus non elementum non, molestie sed nibh.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis laoreet vestibulum fermentum. 
       </sdds-accordion-item>
-      <sdds-accordion-item header="Third item" ${tabIndexAttr} ${affixAttr} ${disabledAttr} ${paddingResetAttr}>
-         This is the panel, which contains associated information with the header. Usually it contains text, set in the same size as the header. 
-         Lorem ipsum doler sit amet.
-       </sdds-accordion-item>
     </sdds-accordion>
   `);
 };
 
 export const Default = Template.bind({});
 Default.args = {};
-
-export const Prefix = Template.bind({});
-Prefix.args = {
-  iconPosition: 'start',
-};
-
-export const Disabled = Template.bind({});
-Disabled.args = {
-  disabled: true,
-};
-
-export const PaddingReset = Template.bind({});
-PaddingReset.args = {
-  paddingReset: true, // Allows item text to expand past suffix icon
-};
