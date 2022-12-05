bf19b15cf9318a0271f0b92e53cb154663859d21
Alexander Eneroth <alexandereneroth@users.noreply.github.com>
2022-12-05 11:02:06 +0100

    docs: hide SB dev addons outside of dev (#688)
    Co-authored-by: Default <alex.john.eneroth@gmail.om>

--- tegel/.storybook/main.js
+++ tegel/.storybook/main.js
@@ -1,14 +1,22 @@
-module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    // '@storybook/addon-interactions',
-    '@storybook/addon-notes/register',
+let addons = [
+  '@storybook/addon-links',
+  '@storybook/addon-essentials',
+  // '@storybook/addon-interactions',
+  '@storybook/addon-notes/register',
+];
+
+if (process.env.STORYBOOK_ENV === 'development') {
+  addons = [
+    ...addons,
     'storybook-dark-mode',
     'storybook-addon-designs',
     '@storybook/addon-a11y',
     'addon-screen-reader',
-  ],
+  ];
+}
+
+module.exports = {
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: addons,
   framework: '@storybook/html',
 };
