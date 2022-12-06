3bed174e3c4bd80d7feec64033a5bcd51e6b8fb7
mJarsater <martin.jarsater@hotmail.com>
2022-12-06 13:21:59 +0100

    feat(modal): refactored how we open the modal and removed prop selector
    refactored how we open the modal and removed prop selector,updated the story and added to migration
    docs
    
    BREAKING CHANGE: Removed the selector prop and added prop for open

--- tegel/src/components/modal/modal.tsx
+++ tegel/src/components/modal/modal.tsx
@@ -1,4 +1,4 @@
-import { Component, h, Listen, Host, Prop, State, Element, Watch, Method } from '@stencil/core';
+import { Component, h, Host, Prop, Element, Method } from '@stencil/core';
 
 @Component({
   tag: 'sdds-modal',
@@ -6,9 +6,6 @@ import { Component, h, Listen, Host, Prop, State, Element, Watch, Method } from 
   shadow: true,
 })
 export class Modal {
-  /** Target selector that triggers opening of modal, for example button with id="btn1", then selector is "#btn1" */
-  @Prop() selector: string = '';
-
   /** Disables closing modal on clicking on overlay area. */
   @Prop() prevent: boolean = false;
 
@@ -18,69 +15,51 @@ export class Modal {
   /** Sticky or Static Actions  */
   @Prop() actions: 'sticky' | 'static' = 'static';
 
-  @Element() el: HTMLElement;
+  /* Opens or closes the modal. */
+  @Prop() open: boolean = false;
 
-  /** State when modal should be shown */
-  @State() show: boolean = false;
+  @Element() el: HTMLElement;
 
   /** Shows the modal  */
   @Method()
   async openModal() {
-    this.show = true;
-  }
-
-  componentDidLoad() {
-    const targets = document.querySelectorAll(this.selector);
-    this.dismissModal();
-
-    // If the modal doesn't have a selector to be triggered
-    if (!targets) {
-      console.warn('No prop for modal targeted, please add selector attribute');
-      return;
-    }
-    // Find all buttons with selector (id/class) and add onclick event on it
-    targets.forEach(el => {
-      el.addEventListener('click', () => {
-        this.show = true;
-      });
-    });
+    this.open = true;
   }
 
   dismissModal() {
     const nodes = this.el.querySelectorAll('[data-dismiss-modal]');
 
-    nodes.forEach(el => {
+    nodes.forEach((el) => {
       el.addEventListener('click', () => {
-        this.show = false;
+        this.open = false;
       });
     });
   }
 
-  @Watch('show')
-  showToggled() {
-    const root = document.querySelector('html');
-    if (this.show === true) {
-      root.classList.add('sdds-modal-overflow');
-    } else {
-      root.classList.remove('sdds-modal-overflow');
-    }
-  }
-
-  // Click event on valid targets to dismiss the modal
-  @Listen('click')
   handleClick(e) {
     const targetList = e.composedPath();
     const target = targetList[0];
-    if (target.classList[0] === 'sdds-modal-btn' || (target.classList[0] === 'sdds-modal-backdrop' && this.prevent === false)) {
-      this.show = false;
+    if (
+      target.classList[0] === 'sdds-modal-btn' ||
+      (target.classList[0] === 'sdds-modal-backdrop' && this.prevent === false)
+    ) {
+      this.open = false;
     }
-    e.stopPropagation();
   }
 
   render() {
     return (
-      <Host class={`sdds-modal-backdrop ${this.show ? 'show' : 'hide'}`}>
-        <div class={`sdds-modal ${this.actions ? `sdds-modal__actions-${this.actions}` : ''} ${this.size ? `sdds-modal-${this.size}` : ''} `}>
+      <Host
+        onClick={(event) => {
+          this.handleClick(event);
+        }}
+        class={`sdds-modal-backdrop ${this.open ? 'show' : 'hide'}`}
+      >
+        <div
+          class={`sdds-modal ${this.actions ? `sdds-modal__actions-${this.actions}` : ''} ${
+            this.size ? `sdds-modal-${this.size}` : ''
+          } `}
+        >
           <div class="sdds-modal-header">
             <slot name="sdds-modal-headline"></slot>
             <button class="sdds-modal-btn"></button>
