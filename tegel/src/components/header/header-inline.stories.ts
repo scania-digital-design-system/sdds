d13e3b47bb61950ff842b3a8807d94c72111df82
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2022-12-09 16:13:08 +0100

    Update tegel/src/components/header/header-inline.stories.ts
    Co-authored-by: mJarsater <62651103+mJarsater@users.noreply.github.com>

--- tegel/src/components/header/header-inline.stories.ts
+++ tegel/src/components/header/header-inline.stories.ts
@@ -89,7 +89,7 @@ const InlineMenuTemplate = ({ siteName }) =>
     </nav>
 
     <script>
-    /* Note: Code below is used only for inspiration and presentation purposes in Stroybook */
+    /* Note: Code below is used only for inspiration and presentation purposes in Storybook */
     toggleMobileMenu = () => {
         document.getElementsByClassName("sdds-nav")[0].classList.toggle("sdds-nav__mob-menu--opened");
         document.getElementsByClassName("sdds-nav")[0].classList.remove("sdds-nav__avatar--opened");
