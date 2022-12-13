3e6e117c6e36bdbe11a8c5a990c48a412ee40274
Nathalie Lindqvist <nathalie.lindqvist@scania.com>
2022-12-13 11:39:33 +0100

    fix(): Change "Webcomponent" to "Web Component" in relevant components (#722)

--- tegel/src/components/button/button-native.stories.ts
+++ tegel/src/components/button/button-native.stories.ts
@@ -105,11 +105,11 @@ export default {
     },
     iconType: {
       name: 'Icon type',
-      description: 'Native/Webcomponent',
+      description: 'Native/Web Component',
       control: {
         type: 'radio',
       },
-      options: ['Native', 'Webcomponent'],
+      options: ['Native', 'Web Component'],
       if: { arg: 'icon', neq: 'none' },
     },
   },
