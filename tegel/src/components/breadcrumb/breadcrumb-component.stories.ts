ed4e03abe3254ecf6a6c94180887da55c2b854b2
mJarsater <62651103+mJarsater@users.noreply.github.com>
2023-01-05 11:40:07 +0100

    feat(breadcrumb): webcomponent (#730)

---
+++ tegel/src/components/breadcrumb/breadcrumb-component.stories.ts
@@ -0,0 +1,37 @@
+import readme from './readme.md';
+import itemReadme from './breadcrumb-item/readme.md';
+import { formatHtmlPreview } from '../../utils/utils';
+
+export default {
+  title: 'Components/Breadcrumb',
+  parameters: {
+    notes: { 'Breadcrumb': readme, 'Breadcumb Item': itemReadme },
+    design: [
+      {
+        name: 'Figma',
+        type: 'figma',
+        url: 'https://www.figma.com/file/d8bTgEx7h694MSesi2CTLF/Tegel-UI-Library?node-id=2703%3A4725&t=rVXuTOgTmXPauyHd-1',
+      },
+      {
+        name: 'Link',
+        type: 'link',
+        url: 'https://www.figma.com/file/d8bTgEx7h694MSesi2CTLF/Tegel-UI-Library?node-id=2703%3A4725&t=rVXuTOgTmXPauyHd-1',
+      },
+    ],
+  },
+};
+
+const Template = () =>
+  formatHtmlPreview(
+    `   
+      <sdds-breadcrumb>
+        <sdds-breadcrumb-item href="#">Page 1</sdds-breadcrumb-item>
+        <sdds-breadcrumb-item href="#">Page 2</sdds-breadcrumb-item>
+        <sdds-breadcrumb-item current>Page 3</sdds-breadcrumb-item>
+      </sdds-breadcrumb>
+      `,
+  );
+
+export const WebComponent = Template.bind({});
+
+WebComponent.args = {};
---
+++ tegel/src/components/breadcrumb/breadcrumb.tsx
@@ -0,0 +1,24 @@
+import { Component, h, Element } from '@stencil/core';
+
+@Component({
+  tag: 'sdds-breadcrumb',
+  styleUrl: 'breadcrumb-component.scss',
+  shadow: true,
+})
+export class SddsBreadcrumb {
+  @Element() el: HTMLElement;
+
+  connectedCallback() {
+    this.el.children[this.el.children.length - 1].classList.add('sdds-breadcrumb-item-last');
+  }
+
+  render() {
+    return (
+      <nav>
+        <ol class={'sdds-breadcrumb'}>
+          <slot></slot>
+        </ol>
+      </nav>
+    );
+  }
+}
