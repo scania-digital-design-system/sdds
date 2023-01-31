717fef97a5c14a9545c51fafef2db49264c9563d
Lindqvist Nathalie <nathalie.lindqvist@scania.com>
2023-01-31 09:46:46 +0100

    fix(spinner): Update descriptions and add default values to controls

--- tegel/src/components/spinner/spinner.stories.tsx
+++ tegel/src/components/spinner/spinner.stories.tsx
@@ -22,19 +22,29 @@ export default {
   argTypes: {
     variant: {
       name: 'Variant',
+      description: 'Sets the variant of the spinner.',
       control: {
         type: 'radio',
       },
       options: ['Standard', 'Inverted'],
-      description: 'Variant of the spinner',
+      table: {
+        defaultValue: {
+          summary: 'Standard',
+        },
+      },
     },
     size: {
       name: 'Size',
+      description: 'Sets the size of the spinner.',
       control: {
         type: 'radio',
       },
       options: ['Large', 'Medium', 'Small', 'Extra small'],
-      description: 'Size of the spinner',
+      table: {
+        defaultValue: {
+          summary: 'Large',
+        },
+      },
     },
   },
   args: {
