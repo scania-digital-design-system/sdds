017c556de78f8de4c479507147c0762621bb6c1a
mJarsater <62651103+mJarsater@users.noreply.github.com>
2023-02-13 15:41:06 +0100

    feat(card): webcomponent (#794)

--- tegel/src/components/card/card.stories.tsx
+++ tegel/src/components/card/card.stories.tsx
@@ -1,4 +1,4 @@
-import CardImage from '../../stories/assets/image/card-img.png';
+import CardPlaceholder from '../../stories/assets/image/card-placeholder.png';
 import { formatHtmlPreview } from '../../utils/utils';
 
 export default {
@@ -19,29 +19,29 @@ export default {
     ],
   },
   argTypes: {
-    headline: {
+    header: {
       name: 'Header text',
       description: 'The header text',
       control: {
         type: 'text',
       },
     },
-    subheadline: {
+    subheader: {
       name: 'Subheader text',
       description: 'The subheader text',
       control: {
         type: 'text',
       },
     },
-    text: {
+    bodyText: {
       name: 'Body text',
       description: 'The body text for the card',
       control: {
         type: 'text',
       },
     },
-    divider: {
-      name: 'Divider',
+    bodyDivider: {
+      name: 'Body divider',
       description: 'Add a divider to the card',
       control: {
         type: 'boolean',
@@ -49,10 +49,10 @@ export default {
       table: {
         defaultValue: { summary: false },
       },
+      if: { arg: 'bodyImg', eq: false },
     },
-    footer: {
-      name: 'Footer',
-      description: 'The footer of the card',
+    cardBottom: {
+      name: 'Content of the bottom of the card',
       control: {
         type: 'text',
       },
@@ -67,9 +67,9 @@ export default {
         defaultValue: { summary: false },
       },
     },
-    image: {
-      name: 'Image',
-      description: 'Image on card',
+    bodyImg: {
+      name: 'Body image',
+      description: 'Image in body on card.',
       control: {
         type: 'boolean',
       },
@@ -85,11 +85,22 @@ export default {
       },
       if: { arg: 'image', eq: true },
     },
-    avatar: {
-      name: 'Avatar',
-      description: 'An avatar image for the card',
+    headerImg: {
+      name: 'Header image',
+      description: 'Image for the header',
       control: {
         type: 'boolean',
+      },
+    },
+    headerPlacement: {
+      name: 'Header placement',
+      description: 'Placement of the header',
+      control: {
+        type: 'radio',
+      },
+      options: ['Above', 'Below'],
+      table: {
+        defaultValue: { summary: 'above' },
       },
     },
     modeVariant: {
@@ -98,45 +109,58 @@ export default {
       control: {
         type: 'radio',
       },
-      options: ['Primary', 'Secondary'],
+      options: ['Inherit from parent', 'Primary', 'Secondary'],
       table: {
-        defaultValue: { summary: 'primary' },
+        defaultValue: { summary: 'Inherit from parent' },
       },
     },
   },
   args: {
-    headline: 'Header text',
-    subheadline: 'Subheader text',
-    text: '',
-    divider: false,
-    footer: '<sdds-icon style="font-size: 20px;" name="arrow_left"></sdds-icon>',
+    header: 'Header text',
+    subheader: 'Subheader text',
+    headerImg: true,
+    headerPlacement: 'Above',
+    bodyImg: false,
+    bodyText: '',
+    bodyDivider: false,
+    cardBottom: '<sdds-icon style="font-size: 20px;" name="arrow_right"></sdds-icon>',
+    modeVariant: 'Inherit from parent',
     clickable: false,
-    image: false,
-    imageTop: false,
-    avatar: false,
-    modeVariant: 'Primary',
   },
 };
 
-const Template = ({ headline, subheadline, footer, clickable, text, divider, imageTop, avatar, modeVariant }) =>
+const Template = ({
+  header,
+  subheader,
+  cardBottom,
+  clickable,
+  bodyText,
+  bodyDivider,
+  headerPlacement,
+  headerImg,
+  modeVariant,
+  bodyImg,
+}) =>
   formatHtmlPreview(
     `
 <style>
 /* demo-wrapper is for demonstration purposes only*/
   .demo-wrapper {
-    width: 300px;
+    width: 368px;
   }
 </style>
     <div class="demo-wrapper">
-          <div class="sdds-card${clickable ? ' sdds-clickable' : ''} ${modeVariant === 'Secondary' ? 'sdds-mode-variant-secondary' : '.sdds-mode-variant-primary'}">
+          <div class="sdds-card${clickable ? ' sdds-clickable' : ''} ${
+      modeVariant !== 'Inherit from parent' ? `sdds-mode-variant-${modeVariant}` : ''
+    }">
             ${
-              imageTop === true
-                ? `<img class="sdds-card-img" src="${CardImage}" alt="Add description to image"/>`
+              bodyImg === true && headerPlacement === 'Below'
+                ? `<img class="sdds-card-img" src="${CardPlaceholder}" alt="Add description to image"/>`
                 : ''
             }
-            <div class="${avatar ? 'sdds-card-header-avatar' : 'sdds-card-header'}">
+            <div class="${headerImg ? 'sdds-card-header-avatar' : 'sdds-card-header'}">
             ${
-              avatar
+              headerImg
                 ? `
               <div class="sdds-card-avatar">
                 <svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="18" cy="18" r="18" fill="#E2E2E4"/></svg>
@@ -144,76 +168,27 @@ const Template = ({ headline, subheadline, footer, clickable, text, divider, ima
                 : ''
             }
                 ${
-                  avatar
+                  headerImg
                     ? `<div class="sdds-card-headlines">
-                  ${headline ? `<h6 class="sdds-card-headline">${headline}</h6>` : ''}
-                  ${subheadline ? `<h6 class="sdds-card-sub-headline" >${subheadline}</h6>` : ''}
+                  ${header ? `<h6 class="sdds-card-headline">${header}</h6>` : ''}
+                  ${subheader ? `<h6 class="sdds-card-sub-headline" >${subheader}</h6>` : ''}
                   </div>`
-                    : `${headline ? `<h6 class="sdds-card-headline">${headline}</h6>` : ''}
-                 ${subheadline ? `<h6 class="sdds-card-sub-headline" >${subheadline}</h6>` : ''}
+                    : `${header ? `<h6 class="sdds-card-headline">${header}</h6>` : ''}
+                 ${subheader ? `<h6 class="sdds-card-sub-headline" >${subheader}</h6>` : ''}
                 `
                 }
             </div>
             ${
-              imageTop === false
-                ? `<img class="sdds-card-img" src="${CardImage}" alt="Add description to image"/>`
+              bodyImg === true && headerPlacement === 'Above'
+                ? `<img class="sdds-card-img" src="${CardPlaceholder}" alt="Add description to image"/>`
                 : ''
             }
-            ${divider ? '<div class="sdds-divider-border-top"></div>' : ''}
-            ${text ? `<div class="sdds-card-body">${text}</div>` : ''}
-            ${footer ? `<div class="sdds-card-footer">${footer}</div>` : ''}
+            ${bodyDivider ? '<div class="sdds-divider-border-top"></div>' : ''}
+            ${bodyText ? `<div class="sdds-card-body">${bodyText}</div>` : ''}
+            ${cardBottom ? `<div class="sdds-card-footer">${cardBottom}</div>` : ''}
           </div>
     </div>
   `,
   );
 
-export const Default = Template.bind({});
-Default.args = {};
-
-export const SupportText = Template.bind({});
-
-SupportText.args = {
-  text: 'This is a short and consist detail text describing for the user what this text is really about.',
-};
-
-export const Divider = Template.bind({});
-
-Divider.args = {
-  divider: true,
-  text: 'This is a short and consist detail text describing for the user what this text is really about.',
-};
-
-export const Link = Template.bind({});
-
-Link.args = {
-  divider: true,
-  text: 'This is a short and consist detail text describing for the user what this text is really about.',
-  footer:
-    '<a class="sdds-link sdds-link--no-underline" href="#">Link text</a><a class="sdds-link sdds-link--no-underline" href="#">Link text</a>',
-};
-
-export const Button = Template.bind({});
-
-Button.args = {
-  divider: true,
-  text: 'This is a short and consist detail text describing for the user what this text is really about.',
-  footer: '<button class="sdds-btn sdds-btn-sm sdds-btn-primary">Button text</button>',
-};
-
-export const Image = Template.bind({});
-
-Image.args = {
-  divider: true,
-  text: 'This is a short and consist detail text describing for the user what this text is really about.',
-  image: true,
-  imageTop: true,
-};
-
-export const Avatar = Template.bind({});
-
-Avatar.args = {
-  avatar: true,
-  divider: false,
-  text: 'This is a short and consist detail text describing for the user what this text is really about.',
-  imageTop: false,
-};
+export const Native = Template.bind({});
