ecbcb28ae2b547ff665b426777e1dcd8afd0c63a
mJarsater <62651103+mJarsater@users.noreply.github.com>
2023-02-27 10:25:16 +0100

    feat(stepper): webcomponent (#762)

---
+++ tegel/src/components/stepper/readme.md
@@ -0,0 +1,20 @@
+# sdds-stepper
+
+
+
+<!-- Auto Generated Below -->
+
+
+## Properties
+
+| Property        | Attribute        | Description                                           | Type                         | Default        |
+| --------------- | ---------------- | ----------------------------------------------------- | ---------------------------- | -------------- |
+| `direction`     | `direction`      | The direction the children are layed out.             | `"horizontal" \| "vertical"` | `'horizontal'` |
+| `hideLabels`    | `hide-labels`    | Hides the label for the child components if true.     | `boolean`                    | `false`        |
+| `labelPosition` | `label-position` | Text position, only available on direction:horizontal | `"aside" \| "below"`         | `'below'`      |
+| `size`          | `size`           | Size of the component and it's children.              | `"lg" \| "sm"`               | `'lg'`         |
+
+
+----------------------------------------------
+
+*Built with [StencilJS](https://stenciljs.com/)*
---
+++ tegel/src/components/stepper/stepper-item/sdds-stepper-item.tsx
@@ -0,0 +1,71 @@
+import { Component, Host, h, Prop, Element, State, Method } from '@stencil/core';
+
+@Component({
+  tag: 'sdds-stepper-item',
+  styleUrl: 'sdds-stepper-item.scss',
+  shadow: true,
+})
+export class SddsStepper {
+  /** Label text for the stepper-item. */
+  @Prop() labelText: string = '';
+
+  /** State of the stepper-item */
+  @Prop() state: 'current' | 'error' | 'success' | 'upcoming' = 'upcoming';
+
+  @State() hideLabel: boolean;
+
+  @State() size: string;
+
+  @State() iconSize: string;
+
+  @State() direction: string;
+
+  @State() labelPosition: string;
+
+  @Element() el: HTMLElement;
+
+  stepperEl: HTMLSddsStepperElement;
+
+  componentWillLoad() {
+    this.stepperEl = this.el.closest('sdds-stepper');
+    this.direction = this.stepperEl.direction;
+    this.labelPosition = this.stepperEl.labelPosition;
+    this.size = this.stepperEl.size;
+    this.hideLabel = this.stepperEl.hideLabels;
+  }
+
+  /** Method to set the width if the stepper item based on its siblings widht, used by the parent element. */
+  @Method()
+  async setWidth(width) {
+    this.el.style.width = `${width}px`;
+  }
+
+  render() {
+    return (
+      <Host>
+        <div
+          role="listItem"
+          class={`${this.size} ${this.direction} text-${this.labelPosition} ${
+            this.hideLabel ? 'hide-labels' : ''
+          }`}
+        >
+          <div class={`${this.state} content-container`}>
+            {this.state === 'success' || this.state === 'error' ? (
+              <sdds-icon
+                name={this.state === 'success' ? 'tick' : 'warning'}
+                size={this.size === 'lg' ? '20px' : '16px'}
+              ></sdds-icon>
+            ) : (
+              <div class="number sdds-detail-05">
+                <slot></slot>
+              </div>
+            )}
+          </div>
+          {!this.hideLabel && (
+            <div class={`label ${this.size} ${this.state}`}>{this.labelText}</div>
+          )}
+        </div>
+      </Host>
+    );
+  }
+}
