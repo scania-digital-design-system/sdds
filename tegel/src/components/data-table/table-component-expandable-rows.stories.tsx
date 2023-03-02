c3920ee363314fa50c87afedede44f31945e025f
Lindqvist Nathalie <nathalie.lindqvist@scania.com>
2023-03-02 14:31:48 +0100

    fix(data table): update control descriptions

--- tegel/src/components/data-table/table-component-expandable-rows.stories.tsx
+++ tegel/src/components/data-table/table-component-expandable-rows.stories.tsx
@@ -27,7 +27,7 @@ export default {
   argTypes: {
     modeVariant: {
       name: 'Mode variant',
-      description: 'The mode variant of the component',
+      description: 'Mode variant adjusts component colors to have better visibility depending on global mode and background.',
       control: {
         type: 'radio',
       },
@@ -43,46 +43,37 @@ export default {
         type: 'boolean',
       },
       table: {
-        defaultValue: {
-          summary: false,
-        },
+        defaultValue: { summary: false },
       },
     },
     responsiveDesign: {
       name: 'Responsive table',
-      description:
-        'Table takes 100% of available width. For column values less then 192px, "No minimum width" has to be enabled too. ',
+      description: 'Enables table to take 100% of available width. For column values less than 192px, "No minimum width" has to be enabled too.',
       control: {
         type: 'boolean',
       },
       table: {
-        defaultValue: {
-          summary: false,
-        },
+        defaultValue: { summary: false },
       },
     },
     verticalDivider: {
       name: 'Vertical dividers',
-      description: 'When enabled, table has vertical dividers between columns.',
+      description: 'Enables vertical dividers between table columns.',
       control: {
         type: 'boolean',
       },
       table: {
-        defaultValue: {
-          summary: false,
-        },
+        defaultValue: { summary: false },
       },
     },
     noMinWidth: {
-      name: 'No column minimum width limitation',
-      description: 'Enable columns to shrink below width 192px.',
+      name: 'No minimum width',
+      description: 'Resets min-width rule and enables setting column width value to less than 192px which is the default. When enabled, controls for column width will show here.',
       control: {
         type: 'boolean',
       },
       table: {
-        defaultValue: {
-          summary: 'false',
-        },
+        defaultValue: { summary: false },
       },
     },
   },
