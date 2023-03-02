a1d1bb69eeb7bb883b5b831ca17815a337f0c52f
Lindqvist Nathalie <nathalie.lindqvist@scania.com>
2023-03-02 10:50:36 +0100

    fix(data table): enable custom width input when noMinWidth is true

--- tegel/src/components/data-table/table-component-basic.stories.tsx
+++ tegel/src/components/data-table/table-component-basic.stories.tsx
@@ -98,6 +98,38 @@ export default {
         },
       },
     },
+    column1Width: {
+      name: 'Column 1 width',
+      description:'Value of width for column 1. In order to work correctly "No minimum width" has to be enabled too. When editing please provide a unit next to the value, eg. 200px.',
+      control: {
+        type: 'text',
+      },
+      if: { arg: 'noMinWidth', eq: true },
+    },
+    column2Width: {
+      name: 'Column 2 width',
+      description: 'Value of width for column 2. In order to work correctly "No minimum width" has to be enabled too. When editing please provide a unit next to the value, eg. 200px.',
+      control: {
+        type: 'text',
+      },
+      if: { arg: 'noMinWidth', eq: true },
+    },
+    column3Width: {
+      name: 'Column 3 width',
+      description: 'Value of width for column 3. In order to work correctly "No minimum width" has to be enabled too. When editing please provide a unit next to the value, eg. 200px.',
+      control: {
+        type: 'text',
+      },
+      if: { arg: 'noMinWidth', eq: true },
+    },
+    column4Width: {
+      name: 'Column 4 width',
+      description: 'Value of width for column 4. In order to work correctly "No minimum width" has to be enabled too. When editing please provide a unit next to the value, eg. 200px.',
+      control: {
+        type: 'text',
+      },
+      if: { arg: 'noMinWidth', eq: true },
+    },
   },
   args: {
     modeVariant: 'Inherit from parent',
@@ -106,6 +138,10 @@ export default {
     disablePadding: false,
     verticalDivider: false,
     noMinWidth: false,
+    column1Width: '',
+    column2Width: '',
+    column3Width: '',
+    column4Width: '',
   },
 };
 
@@ -116,6 +152,10 @@ const BasicTemplate = ({
   disablePadding,
   verticalDivider,
   noMinWidth,
+  column1Width,
+  column2Width,
+  column3Width,
+  column4Width,
 }) =>
   formatHtmlPreview(`
   <sdds-table
@@ -125,10 +165,10 @@ const BasicTemplate = ({
       ${noMinWidth ? 'no-min-width' : ''}
       ${modeVariant !== 'Inherit from parent' ? `mode-variant="${modeVariant.toLowerCase()}"` : ''}>
       <sdds-table-header>
-          <sdds-header-cell column-key='truck' column-title='Truck type'></sdds-header-cell>
-          <sdds-header-cell column-key='driver' column-title='Driver name'></sdds-header-cell>
-          <sdds-header-cell column-key='country' column-title='Country'></sdds-header-cell>
-          <sdds-header-cell column-key='mileage' column-title='Mileage' text-align='right'></sdds-header-cell>
+          <sdds-header-cell ${column1Width ? `style="width: ${column1Width};"` : ''} column-key='truck' column-title='Truck type'></sdds-header-cell>
+          <sdds-header-cell ${column2Width ? `style="width: ${column2Width};"` : ''} column-key='driver' column-title='Driver name'></sdds-header-cell>
+          <sdds-header-cell ${column3Width ? `style="width: ${column3Width};"` : ''} column-key='country' column-title='Country'></sdds-header-cell>
+          <sdds-header-cell ${column4Width ? `style="width: ${column4Width};"` : ''} column-key='mileage' column-title='Mileage' text-align='right'></sdds-header-cell>
       </sdds-table-header>
       <sdds-table-body>
           <sdds-table-body-row>
