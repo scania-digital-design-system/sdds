202debd74b5d8eb0f679e7babf5d60db8d24b196
Lindqvist Nathalie <nathalie.lindqvist@scania.com>
2023-03-03 12:00:39 +0100

    fix(popover canvas): refactor default value

--- tegel/src/components/popover-canvas/popover-canvas.stories.tsx
+++ tegel/src/components/popover-canvas/popover-canvas.stories.tsx
@@ -29,7 +29,9 @@ export default {
         'Right end',
         'Auto',
       ],
-      defaultValue: { summary: 'Bottom' },
+      table: {
+        defaultValue: { summary: 'Bottom' },
+      },
     },
   },
   args: {
