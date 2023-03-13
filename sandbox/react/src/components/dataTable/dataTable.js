f72f9a2015162c7993858742f4bc5d86091b0faa
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2023-03-13 17:17:40 +0100

    fix(sandbox-react): data table responsive variant 1

--- sandbox/react/src/components/dataTable/dataTable.js
+++ sandbox/react/src/components/dataTable/dataTable.js
@@ -5,15 +5,27 @@ const DataTable = () => {
         <sdds-table
           no-min-width
           body-data='[{"truck":"L-series","driver":"Miki","country":"USA","mileage":123987},{"truck":"P-series","driver":"Guerra","country":"Sweden","mileage":2000852},{"truck":"G-series","driver":"Ferrell","country":"Germany","mileage":564},{"truck":"R-series","driver":"Cox","country":"Spain","mileage":1789357},{"truck":"S-series","driver":"Montgomery","country":"Croatia","mileage":65},{"truck":"L-series","driver":"Sheryl","country":"Greece","mileage":365784},{"truck":"G-series","driver":"Bojan","country":"Norway","mileage":80957}]'
-          style={{ width: '100%', display: 'grid' }}
         >
-          <sdds-header-cell column-key="truck" column-title="Truck type"></sdds-header-cell>
-          <sdds-header-cell column-key="driver" column-title="Driver name"></sdds-header-cell>
-          <sdds-header-cell column-key="country" column-title="Country"></sdds-header-cell>
+          <sdds-header-cell
+            column-key="truck"
+            column-title="Truck type"
+            custom-width="25vw"
+          ></sdds-header-cell>
+          <sdds-header-cell
+            column-key="driver"
+            column-title="Driver name"
+            custom-width="25vw"
+          ></sdds-header-cell>
+          <sdds-header-cell
+            column-key="country"
+            column-title="Country"
+            custom-width="25vw"
+          ></sdds-header-cell>
           <sdds-header-cell
             column-key="mileage"
             column-title="Mileage"
             text-align="right"
+            custom-width="25vw"
           ></sdds-header-cell>
         </sdds-table>
       </div>
