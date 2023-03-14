c3181304e234adcb4ab3d58117c52884b6ea89f2
Nathalie Lindqvist <nathalie.lindqvist@scania.com>
2023-03-14 07:38:47 +0100

    fix(toggle): standardise order of controls in Storybook (#929)

--- tegel/src/components/toggle/toggle.stories.tsx
+++ tegel/src/components/toggle/toggle.stories.tsx
@@ -20,46 +20,51 @@ export default {
   argTypes: {
     size: {
       name: 'Size',
+      description: 'Sets the size of the toggle.',
       control: {
         type: 'radio',
       },
-      options: ['Default', 'Small'],
-      description: 'Size of the toggle',
+      options: ['Large', 'Small'],
     },
     headline: {
       name: 'Headline',
-      description: 'Optional value to be used to clarify what the toggle is switching on / off',
+      description: 'Sets the headline, displayed above the toggle.',
       control: {
         type: 'text',
       },
     },
-    disabled: {
-      name: 'Disabled',
-      description: 'Should it be disabled?',
+    label: {
+      name: 'Label',
+      description: 'Sets the label for the toggles input element.',
       control: {
-        type: 'boolean',
+        type: 'text',
       },
     },
     checked: {
       name: 'Checked',
-      description: 'Sets the toggle as checked',
+      description: 'Sets the toggle as checked.',
       control: {
         type: 'boolean',
       },
-      table: {
-        defaultValue: { summary: 'false' },
+    },
+    disabled: {
+      name: 'Disabled',
+      description: 'Disables the toggle.',
+      control: {
+        type: 'boolean',
       },
     },
   },
   args: {
-    size: 'Default',
+    size: 'Large',
     headline: 'Headline',
-    disabled: false,
+    label: 'Label',
     checked: false,
+    disabled: false,
   },
 };
 
-const Template = ({ size, disabled, headline, checked }) => {
+const Template = ({ size, headline, label, checked, disabled }) => {
   const sizeValue = size === 'Small' ? 'sdds-toggle-sm' : '';
   const headlineDiv =
     headline.length > 0 ? `<div class="sdds-toggle-headline">${headline}</div>` : '';
@@ -71,7 +76,7 @@ const Template = ({ size, disabled, headline, checked }) => {
           disabled ? 'disabled' : ''
         } ${checked ? 'checked' : ''}>
         <span class="sdds-toggle-switch"></span>
-        <label class="sdds-toggle-label" for="customSwitch1">Toggle this switch element</label>
+        <label class="sdds-toggle-label" for="customSwitch1">${label}</label>
       </div>
   `);
 };
