273f5b4fd69657167ca7d55ccd36994eab48c846
mJarsater <martin.jarsater@hotmail.com>
2023-03-16 09:10:48 +0100

    fix(stepper-item): renamed slot to index and removed unused div/css

--- tegel/src/components/stepper/sdds-stepper.stories.ts
+++ tegel/src/components/stepper/sdds-stepper.stories.ts
@@ -84,10 +84,10 @@ const Template = ({ size, direction, labelPosition, hideLabels }) =>
     `<sdds-stepper ${hideLabels ? 'hide-labels' : ''} size="${sizeLookUp[size]}" ${
       direction === 'Horizontal' ? `label-position="${labelPosition?.toLowerCase()}"` : ''
     } direction="${direction.toLowerCase()}">
-    <sdds-stepper-item state="success" label-text="Step label"><div slot="number">1</div></sdds-stepper-item>
-    <sdds-stepper-item state="active" label-text="Step label"><div slot="number">2</div></sdds-stepper-item>
-    <sdds-stepper-item label-text="Step inactive with text"><div slot="number">3</div></sdds-stepper-item>
-    <sdds-stepper-item label-text="Step label"><div slot="number">4</div></sdds-stepper-item>
+    <sdds-stepper-item state="success" label-text="Step label"><div slot="index">1</div></sdds-stepper-item>
+    <sdds-stepper-item state="active" label-text="Step label"><div slot="index">2</div></sdds-stepper-item>
+    <sdds-stepper-item label-text="Step inactive with text"><div slot="index">3</div></sdds-stepper-item>
+    <sdds-stepper-item label-text="Step label"><div slot="index">4</div></sdds-stepper-item>
   </sdds-stepper>
         `,
   );
@@ -98,10 +98,10 @@ const TemplateWithError = ({ size, hideLabels, labelPosition, direction }) =>
     `<sdds-stepper ${hideLabels ? 'hide-labels' : ''} size="${sizeLookUp[size]}" ${
       direction === 'Horizontal' ? `label-position="${labelPosition?.toLowerCase()}"` : ''
     } direction="${direction.toLowerCase()}">
-  <sdds-stepper-item state="success" label-text="Step label"><div slot="number">1</div></sdds-stepper-item>
-  <sdds-stepper-item state="error" label-text="Step label"><div slot="number">2</div></sdds-stepper-item>
-  <sdds-stepper-item label-text="Step inactive with text"><div slot="number">3</div></sdds-stepper-item>
-  <sdds-stepper-item label-text="Step label"><div slot="number">4</div></sdds-stepper-item>
+  <sdds-stepper-item state="success" label-text="Step label"><div slot="index">1</div></sdds-stepper-item>
+  <sdds-stepper-item state="error" label-text="Step label"><div slot="index">2</div></sdds-stepper-item>
+  <sdds-stepper-item label-text="Step inactive with text"><div slot="index">3</div></sdds-stepper-item>
+  <sdds-stepper-item label-text="Step label"><div slot="index">4</div></sdds-stepper-item>
 </sdds-stepper>
       `,
   );
