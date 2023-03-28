84387b756a0dea4a538d57cea64c629168316050
Alexander Eneroth <alexandereneroth@users.noreply.github.com>
2023-03-28 10:34:44 +0200

    docs: fix navigation user menu story control
    Co-authored-by: Nathalie Lindqvist <nathalie.lindqvist@scania.com>

--- tegel/src/patterns-stories/navigation/navigation-user-menu.stories.tsx
+++ tegel/src/patterns-stories/navigation/navigation-user-menu.stories.tsx
@@ -30,7 +30,9 @@ export default {
     siteName: {
       name: 'Site name',
       description: 'Set a custom title for the header',
-      type: 'string',
+      control: {
+        type: 'text',
+      },
     },
   },
   args: {
