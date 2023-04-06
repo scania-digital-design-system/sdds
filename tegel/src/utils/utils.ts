5b4c359f4930548dd4ad597a1d53f003803c3c12
mJarsater <62651103+mJarsater@users.noreply.github.com>
2023-04-06 09:13:12 +0200

    feat(utils): appendHiddenInput (#990)

--- tegel/src/utils/utils.ts
+++ tegel/src/utils/utils.ts
@@ -19,3 +19,31 @@ export const formatHtmlPreview = (htmlStr) =>
 export function demoFormat(first: string, middle: string, last: string): string {
   return (first || '') + (middle ? ` ${middle}` : '') + (last ? ` ${last}` : '');
 }
+
+/** reference: https://github.com/ionic-team/ionic-framework/blob/main/core/src/utils/helpers.ts#L346
+ *
+ * Appends a hidden input element to allow the component
+ * work within and get picked up by a <form>.
+ * @param element The element on which the input with be appended.
+ * @param name Name of the input.
+ * @param value The value of the input.
+ * @param disabled Disables the input if true.
+ * @param additionalAttributes Additional attributes that should be passed to the input.
+ */
+export const appendHiddenInput = (
+  element: HTMLElement,
+  name: string,
+  value: string | undefined | null,
+  disabled: boolean,
+  additionalAttributes: Array<{ key: string; value: string }>,
+) => {
+  const input = element.ownerDocument!.createElement('input');
+  input.type = 'hidden';
+  element.appendChild(input);
+  input.disabled = disabled;
+  input.name = name;
+  input.value = value || '';
+  if (additionalAttributes) {
+    additionalAttributes.forEach((attr) => input.setAttribute(attr.key, attr.value));
+  }
+};
