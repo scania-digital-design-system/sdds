6c75311b7b6f188b7b461110d914f490396a11be
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2023-04-19 10:59:51 +0200

    chore: changes by martin.jarsater

--- tegel/src/components/button/button-native.stories.tsx
+++ tegel/src/components/button/button-native.stories.tsx
@@ -24,7 +24,8 @@ export default {
   argTypes: {
     modeVariant: {
       name: 'Mode variant',
-      description: 'Mode variant adjusts component colors to have better visibility depending on global mode and background.',
+      description:
+        'Mode variant adjusts component colors to have better visibility depending on global mode and background.',
       control: {
         type: 'radio',
       },
@@ -35,7 +36,8 @@ export default {
     },
     btnType: {
       name: 'Type',
-      description: 'Four different button types to help the user to distinguish the level of importance of the task they represent.',
+      description:
+        'Four different button types to help the user to distinguish the level of importance of the task they represent.',
       control: {
         type: 'radio',
       },
