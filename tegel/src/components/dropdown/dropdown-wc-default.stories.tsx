0eedec03ee4d55513b61ee7e55423c473e63ca5d
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2023-04-20 14:24:28 +0200

    feat(dropdown): all stories under one with controls for dropdown type

--- tegel/src/components/dropdown/dropdown-wc-default.stories.tsx
+++ tegel/src/components/dropdown/dropdown-wc-default.stories.tsx
@@ -34,6 +34,17 @@ export default {
         defaultValue: { summary: 'Inherit from parent' },
       },
     },
+    dropdownType: {
+      name: 'Dropdown Type',
+      description: 'Sets the type of the dropdown.',
+      control: {
+        type: 'radio',
+      },
+      options: ['Default', 'Filter', 'Multiselect'],
+      table: {
+        defaultValue: { summary: 'Default' },
+      },
+    },
     state: {
       name: 'Error state',
       description: 'Puts the component in error state.',
@@ -101,13 +112,22 @@ export default {
       },
     },
     defaultOption: {
-      if: { arg: 'type', neq: 'Multiselect' },
+      if: { arg: 'dropdownType', neq: 'Multiselect' },
       name: 'Default option',
       description: 'Sets a pre-selected option and replaces the placeholder.',
       control: {
         type: 'radio',
       },
       options: ['No default', 'Option 1', 'Option 2', 'Option 3'],
+    },
+    multiDefaultOption: {
+      name: 'Default options',
+      description: 'Sets a pre-selected option and replaces the placeholder.',
+      if: { arg: 'dropdownType', eq: 'Multiselect' },
+      control: {
+        type: 'check',
+      },
+      options: ['Option 1', 'Option 2', 'Option 3'],
     },
     disabled: {
       name: 'Disabled',
@@ -122,6 +142,7 @@ export default {
   },
   args: {
     modeVariant: 'Inherit from parent',
+    dropdownType: 'Default',
     state: false,
     size: 'Large',
     openDirection: 'Auto',
@@ -130,12 +151,14 @@ export default {
     placeholder: 'Placeholder',
     helper: '',
     defaultOption: 'Option 1',
+    multiDefaultOption: ['Option 1', 'Option 2'],
     disabled: false,
   },
 };
 
 const Template = ({
   modeVariant,
+  dropdownType,
   state = false,
   size,
   openDirection,
@@ -144,6 +167,7 @@ const Template = ({
   placeholder,
   helper,
   defaultOption,
+  multiDefaultOption,
   disabled = false,
 }) => {
   const sizeLookup = { Large: 'lg', Medium: 'md', Small: 'sm' };
@@ -155,6 +179,10 @@ const Template = ({
     'Option 3': 'option-3',
   };
 
+  const multiDefaultOptionValue =
+    dropdownType === 'Multiselect' &&
+    multiDefaultOption.map((value) => value.toLowerCase().replace(' ', '-'));
+
   return formatHtmlPreview(`
   <style>
   /* demo-wrapper is for demonstration purposes only*/
@@ -165,6 +193,9 @@ const Template = ({
   </style>
 
     <div class="demo-wrapper">
+    ${
+      dropdownType !== 'Filter'
+        ? `
         <sdds-dropdown
           ${
             modeVariant !== 'Inherit from parent'
@@ -181,12 +212,52 @@ const Template = ({
           ${labelPosLookup[labelPosition] !== 'no-label' ? `label="${labelText}"` : ''}
           ${helper !== '' ? `helper="${helper}"` : ''}
           state="${state}"
-          type="default"
-          default-option="${defaultOptionLookup[defaultOption]}" >
+          type="${dropdownType.toLowerCase()}"
+          default-option="${
+            dropdownType === 'Default'
+              ? defaultOptionLookup[defaultOption]
+              : multiDefaultOptionValue
+          }"
+        >
           <sdds-dropdown-option value="option-1" tabindex="0" disabled>Option 1</sdds-dropdown-option>
           <sdds-dropdown-option value="option-2" tabindex="0">Option 2</sdds-dropdown-option>
           <sdds-dropdown-option value="option-3" tabindex="0">Option 3</sdds-dropdown-option>
         </sdds-dropdown>
+    `
+        : `
+        <sdds-dropdown-filter
+          ${
+            modeVariant !== 'Inherit from parent'
+              ? `mode-variant="${modeVariant.toLowerCase()}".sdds-option-label {          `
+              : ''
+          }
+          id="sdds-dropdown-filter"
+          size="${sizeLookup[size]}"
+          placeholder="${placeholder}"
+          disabled="${disabled}"
+          open-direction="${openDirection.toLowerCase()}"
+          label-position="${labelPosLookup[labelPosition]}"
+          ${labelPosLookup[labelPosition] !== 'no-label' ? `label="${labelText}"` : ''}
+          ${helper !== '' ? `helper="${helper}"` : ''}
+          state="${state}"
+          data='[
+            {
+              "value": "option-1",
+              "label":"Jakarta"
+            },
+            {
+              "value":"option-2"
+              ,"label":"Stockholm"
+            },
+            {
+              "value":"option-3",
+              "label":"Barcelona"
+            }
+          ]'
+          default-option="${defaultOptionLookup[defaultOption]}">
+        </sdds-dropdown-filter>`
+    }
+
       </div>
   `);
 };
