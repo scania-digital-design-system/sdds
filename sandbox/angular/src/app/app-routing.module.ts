ad9c10fcf563d22f3fdb620f94a8a04172f31354
Alexander Eneroth <alexandereneroth@users.noreply.github.com>
2023-04-26 16:53:40 +0200

    fix(components/header): fix footer overlapping old header sidemenu

--- sandbox/angular/src/app/app-routing.module.ts
+++ sandbox/angular/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormComponent } from './components/form/form.component';
 import { HomeComponent } from './components/home/home.component';
+import { ShortComponent } from './components/short/short.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'form', component: FormComponent },
+  { path: 'short', component: ShortComponent, },
   { path: 'home', component: HomeComponent, pathMatch: 'full' },
 ];
 
