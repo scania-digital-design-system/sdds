278ebb882b20130456cda31b3e7b8a1e37bd3b2d
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2023-05-10 16:53:13 +0200

    feat: removing tegel from sdds

--- tegel/.storybook/manager.js
+++
@@ -1,8 +0,0 @@
-// .storybook/manager.js
-
-import { addons } from '@storybook/addons';
-import theme from './ScaniaThemeLight';
-
-addons.setConfig({
-  theme, // only this does not work with storybook-dark-mode plugin, put under 'darkmode' key inside preview.js file also. See https://github.com/hipstersmoothie/storybook-dark-mode/issues/157
-});
--- tegel/src/patterns-stories/navigation/readme.md
+++
@@ -1,3 +0,0 @@
-
-## API
-See the [header component story](/info/components-header-web-component--default) and [side menu component story](/info/components-side-menu-web-component--web-component), for more information on the components and their API.
