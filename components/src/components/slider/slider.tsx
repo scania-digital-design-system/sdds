f82ad8b44bf303b8f66679033165d780622d45e0
Lindqvist Nathalie <nathalie.lindqvist@scania.com>
2023-07-13 10:46:44 +0200

    fix(slider): remove comments

--- components/src/components/slider/slider.tsx
+++ components/src/components/slider/slider.tsx
@@ -432,7 +432,6 @@ export class Slider {
       this.tickValues.push(this.getMax());
     }
 
-
     if (this.readonly !== null) {
       this.readonlyState = true;
     } else {
@@ -484,9 +483,6 @@ export class Slider {
         ></input>
 
         <div
-          // class={`sdds-slider ${this.disabled ? 'disabled' : ''} ${
-          //   this.useSmall ? 'sdds-slider-small' : ''
-          // }`}
           class={{
             'sdds-slider': true,
             'disabled': this.disabled,
